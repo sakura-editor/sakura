a53ea88f2abd564265cfbcf8b541bbb47e04ca81
genta <genta@users.sourceforge.net>
2001-06-28 07:53:32 +0000

    Chg: Name of functionality
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk@111 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/CShareData.cpp
+++ sakura_core/CShareData.cpp
@@ -314,18 +314,18 @@ bool CShareData::Init( void )
 			{ VK_LEFT,"←",F_LEFT, F_LEFT_SEL/*F_GOLINETOP*/, F_WORDLEFT, F_WORDLEFT_SEL, F_BEGIN_BOX, 0, 0, 0 },
 			//Oct. 7, 2000 JEPRO	Shift+Ctrl+Alt+↑に「縦方向に最大化」を追加
 //			{ VK_UP,"↑",F_UP, F_UP_SEL, F_UP2, F_UP2_SEL, F_BEGIN_BOX, 0, 0, F_MAXIMIZE_V },
-			//Jun. 26, 2001 JEPRO
-			//	Ctrl+↑に割り当てられていた「カーソル上移動(２行ごと)」を「画面を上へ１行スクロール」に変更
-			{ VK_UP,"↑",F_UP, F_UP_SEL, F_WndScrollUp, F_UP2_SEL, F_BEGIN_BOX, 0, 0, F_MAXIMIZE_V },
+			//Jun. 27, 2001 JEPRO
+			//	Ctrl+↑に割り当てられていた「カーソル上移動(２行ごと)」を「テキストを１行下へスクロール」に変更
+			{ VK_UP,"↑",F_UP, F_UP_SEL, F_WndScrollDown, F_UP2_SEL, F_BEGIN_BOX, 0, 0, F_MAXIMIZE_V },
 			//2001.02.10 by MIK Shift+Ctrl+Alt+→に「横方向に最大化」を追加
 			{ VK_RIGHT,"→",F_RIGHT, F_RIGHT_SEL/*F_GOLINEEND*/, F_WORDRIGHT, F_WORDRIGHT_SEL, F_BEGIN_BOX, 0, 0, F_MAXIMIZE_H },
 			//Sept. 14, 2000 JEPRO
 			//	Ctrl+↓に割り当てられていた「右クリックメニュー」を「カーソル下移動(２行ごと)」に変更
 			//	それに付随してさらに「右クリックメニュー」をCtrl＋Alt＋↓に変更
 //			{ VK_DOWN,"↓",F_DOWN, F_DOWN_SEL, F_DOWN2, F_DOWN2_SEL, F_BEGIN_BOX, 0, F_MENU_RBUTTON, F_MINIMIZE_ALL },
-			//Jun. 26, 2001 JEPRO
-			//	Ctrl+↓に割り当てられていた「カーソル下移動(２行ごと)」を「画面を下へ１行スクロール」に変更
-			{ VK_DOWN,"↓",F_DOWN, F_DOWN_SEL, F_WndScrollDown, F_DOWN2_SEL, F_BEGIN_BOX, 0, F_MENU_RBUTTON, F_MINIMIZE_ALL },
+			//Jun. 27, 2001 JEPRO
+			//	Ctrl+↓に割り当てられていた「カーソル下移動(２行ごと)」を「テキストを１行上へスクロール」に変更
+			{ VK_DOWN,"↓",F_DOWN, F_DOWN_SEL, F_WndScrollUp, F_DOWN2_SEL, F_BEGIN_BOX, 0, F_MENU_RBUTTON, F_MINIMIZE_ALL },
 //			{ VK_PRIOR,"RollDown(PageUp)",F_ROLLDOWN, F_ROLLDOWN_SEL, 0, 0, 0, 0, 0, 0 },
 //			{ VK_NEXT,"RollUp(PageDown)",F_ROLLUP, F_ROLLUP_SEL, 0, 0, 0, 0, 0, 0 },
 			//Oct. 15, 2000 JEPRO Ctrl+PgUp, Shift+Ctrl+PgDn にそれぞれ「１ページダウン」, 「(選択)１ページダウン」を追加
