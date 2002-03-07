7299a108581f61014901b343879a1e938823c490
genta <genta@users.sourceforge.net>
2002-03-07 15:40:28 +0000

    (none)
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk@236 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

---
+++ sakura_core/COsVersionInfo.h
@@ -0,0 +1,77 @@
+/*!	@file
+	@brief COsVersionInfo
+
+	OSVERSIONINFOをラッピング
+
+	@author YAZAKI
+	@date 2002年3月3日
+*/
+/*
+	Copyright (C) 2001, YAZAKI
+
+	This software is provided 'as-is', without any express or implied
+	warranty. In no event will the authors be held liable for any damages
+	arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any purpose, 
+	including commercial applications, and to alter it and redistribute it 
+	freely, subject to the following restrictions:
+
+		1. The origin of this software must not be misrepresented;
+		   you must not claim that you wrote the original software.
+		   If you use this software in a product, an acknowledgment
+		   in the product documentation would be appreciated but is
+		   not required.
+
+		2. Altered source versions must be plainly marked as such, 
+		   and must not be misrepresented as being the original software.
+
+		3. This notice may not be removed or altered from any source
+		   distribution.
+*/
+
+#ifndef _COSVERSIONINFO_H_
+#define _COSVERSIONINFO_H_
+
+class COsVersionInfo {
+public:
+	COsVersionInfo(){
+		memset( (void *)&m_cOsVersionInfo, 0, sizeof( m_cOsVersionInfo ) );
+		m_cOsVersionInfo.dwOSVersionInfoSize = sizeof( m_cOsVersionInfo );
+		m_bSuccess = ::GetVersionEx( &m_cOsVersionInfo );
+	};
+	
+	/* OsVersionが取得できたか？ */
+	BOOL GetVersion(){
+		return m_bSuccess;
+	}
+	
+	/* 使用しているOS（Windows）が、動作対象か確認する */
+	BOOL OsIsEnableVersion(){
+		return !( m_cOsVersionInfo.dwMajorVersion < 4 );
+	};
+	
+	
+	// From Here Jul. 5, 2001 shoji masami
+	/*! NTプラットフォームかどうか調べる
+
+		@retval TRUE NT platform
+		@retval FALSE non-NT platform
+	*/
+	BOOL IsWin32NT(){
+		return (m_cOsVersionInfo.dwPlatformId == VER_PLATFORM_WIN32_NT);
+	}
+
+	/*	::WinHelp( hwnd, lpszHelp, HELP_COMMAND, (unsigned long)"CONTENTS()" );
+		が使用できないバージョンなら、TRUE
+		使用できるバージョンなら、FALSE
+	*/
+	BOOL HasWinHelpContentsProblem(){
+		return ( IsWin32NT() && (m_cOsVersionInfo.dwMajorVersion <= 4));
+	}
+protected:
+	BOOL m_bSuccess;
+	OSVERSIONINFO m_cOsVersionInfo;
+};
+
+#endif
