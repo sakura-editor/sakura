40b203b95a11c1c71348bb50e207dc8a4f9e53d0
Administrator <Administrator@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2002-09-27 04:39:40 +0000

    Refine Comment/Quote check
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk@302 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

---
+++ sakura_core/CBlockComment.h
@@ -0,0 +1,62 @@
+//	$Id$
+/*!	@file
+	@brief ブロックコメントデリミタを管理する
+
+	@author Yazaki
+	@date 2002/09/17 新規作成
+	$Revision$
+*/
+/*
+	Copyright (C) 2002, Yazaki
+
+	This source code is designed for sakura editor.
+	Please contact the copyright holder to use this code for other purpose.
+*/
+
+#ifndef _CBLOCKCOMMENT_H_
+#define _CBLOCKCOMMENT_H_
+
+//	sakura
+#include "global.h"
+
+/*-----------------------------------------------------------------------
+クラスの宣言
+-----------------------------------------------------------------------*/
+/*! ブロックコメントデリミタを管理する
+
+	@note CBlockCommentは、共有メモリTypesに含まれるので、メンバ変数は常に実体を持っていなければならない。
+*/
+#define BLOCKCOMMENT_NUM	2
+#define BLOCKCOMMENT_BUFFERSIZE	16
+
+class SAKURA_CORE_API CBlockComment
+{
+	char	m_szBlockCommentFrom[BLOCKCOMMENT_NUM][BLOCKCOMMENT_BUFFERSIZE];		/*!< ブロックコメントデリミタ(From) */
+	char	m_szBlockCommentTo[BLOCKCOMMENT_NUM][BLOCKCOMMENT_BUFFERSIZE];			/*!< ブロックコメントデリミタ(To) */	int		m_nBlockFromLen[BLOCKCOMMENT_NUM];
+	int		m_nBlockToLen[BLOCKCOMMENT_NUM];
+
+	friend class CShareData;
+public:
+	/*
+	||  Constructors：コンパイラ標準を使用。
+	*/
+	CBlockComment();
+
+	void CopyTo( const int n, const char* pszFrom, const char* pszTo );	//	行コメントデリミタをコピーする
+	bool Match_CommentFrom( int n, int nPos, int nLineLen, const char* pLine ) const;	//	行コメントに値するか確認する	
+	int Match_CommentTo( int n, int nPos, int nLineLen, const char* pLine ) const;	//	行コメントに値するか確認する
+
+	const char* getBlockCommentFrom( const int n ){
+		return m_szBlockCommentFrom[n];
+	};
+	const char* getBlockCommentTo( const int n ){
+		return m_szBlockCommentTo[n];
+	};
+};
+
+
+///////////////////////////////////////////////////////////////////////
+#endif /* _CBLOCKCOMMENT_H_ */
+
+
+/*[EOF]*/
---
+++ sakura_core/CLineComment.h
@@ -0,0 +1,61 @@
+//	$Id$
+/*!	@file
+	@brief 行コメントデリミタを管理する
+
+	@author Yazaki
+	@date 2002/09/17 新規作成
+	$Revision$
+*/
+/*
+	Copyright (C) 2002, Yazaki
+
+	This source code is designed for sakura editor.
+	Please contact the copyright holder to use this code for other purpose.
+*/
+
+#ifndef _CLINECOMMENT_H_
+#define _CLINECOMMENT_H_
+
+//	sakura
+#include "global.h"
+
+/*-----------------------------------------------------------------------
+クラスの宣言
+-----------------------------------------------------------------------*/
+#define COMMENT_DELIMITER_NUM	3
+#define COMMENT_DELIMITER_BUFFERSIZE	16
+
+/*! 行コメントデリミタを管理する
+
+	@note CLineCommentは、共有メモリTypesに含まれるので、メンバ変数は常に実体を持っていなければならない。
+*/
+class SAKURA_CORE_API CLineComment
+{
+	char	m_pszLineComment[COMMENT_DELIMITER_NUM][COMMENT_DELIMITER_BUFFERSIZE];	/* 行コメントデリミタ */
+	int		m_nLineCommentPos[COMMENT_DELIMITER_NUM];	//!< 行コメントの開始位置(負数は指定無し)
+	int		m_nLineCommentLen[COMMENT_DELIMITER_NUM];	//!< 行コメント文字列の長さ
+
+	friend class CShareData;
+public:
+	/*
+	||  Constructors：コンパイラ標準を使用。
+	*/
+	CLineComment();
+
+	void CopyTo( const int n, const char* buffer, int nCommentPos );	//	行コメントデリミタをコピーする
+	bool Match( int nPos, int nLineLen, const char* pLine ) const;	//	行コメントに値するか確認する
+
+	const char* getLineComment( const int n ){
+		return m_pszLineComment[n];
+	};
+	int getLineCommentPos( const int n ) const {
+		return m_nLineCommentPos[n];
+	};
+};
+
+
+///////////////////////////////////////////////////////////////////////
+#endif /* _CLINECOMMENT_H_ */
+
+
+/*[EOF]*/
