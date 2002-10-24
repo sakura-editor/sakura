01b9da5d3844ac13eb4bbc950e149424e64651f1
Administrator <Administrator@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2002-10-24 13:26:57 +0000

    Imp: Icon registration time
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk@336 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/CImageListMgr.cpp
+++ sakura_core/CImageListMgr.cpp
@@ -72,6 +72,7 @@ bool CImageListMgr::Create(HINSTANCE hInstance, HWND hWnd)
 
 		// (sakuraディレクトリ) my_icons.bmp の文字列を作成
 		_splitpath( szPath, szExeDrive, szExeDir, NULL, NULL );
+		//	@@TEST@@
 		wsprintf( szPath, "%s%s%s", szExeDrive, szExeDir, "my_icons.bmp" );	//Jul.04, 2001 JEPRO toolbar.bmp→my_icons.bmp に名前変更
 		hRscbmp = (HBITMAP)::LoadImage( NULL, szPath, IMAGE_BITMAP, 0, 0,
 			LR_LOADFROMFILE | LR_CREATEDIBSECTION | LR_LOADMAP3DCOLORS );
@@ -116,7 +117,11 @@ bool CImageListMgr::Create(HINSTANCE hInstance, HWND hWnd)
 		//	横長のBitmapにしたらアイコンがおかしくなったので，
 		//	1行分を少しずつ追加する方法に変更
 		//	1行分のBitmapを構築
-		hTmpbmp = ::CreateCompatibleBitmap(dcFrom, m_cx * MAX_X, m_cy);
+
+		//	Oct. 23, 2002 genta
+		//	横長のBitmapでも問題ないので，横長のBitmapを作って一括登録するように変更
+		//	DCのmap/unmapが速度に大きく影響するため，
+		hTmpbmp = ::CreateCompatibleBitmap(dcFrom, m_cx * MAX_X * MAX_Y, m_cy);
 		if( hTmpbmp == NULL ){
 			nRetPos = 3;
 			break;
@@ -127,7 +132,6 @@ bool CImageListMgr::Create(HINSTANCE hInstance, HWND hWnd)
 			nRetPos = 5;
 			break;
 		}
-
 		//	透明色の設定
 		cTransparent = GetPixel( dcFrom, 0, 0 );
 
@@ -138,31 +142,35 @@ bool CImageListMgr::Create(HINSTANCE hInstance, HWND hWnd)
 			break;
 		}
 
+		MY_TRACETIME( cRunningTimer, "List is created" );
 		int y, py, px;
 		for( y = py = 0, px = 0; y < MAX_Y;
 			y++, py += m_cy, px += m_cx * MAX_X ){
 			::BitBlt( dcTo,	//	Destination
-				0, 0,		//	転送先
+				px, 0,		//	転送先
 				m_cx * MAX_X, m_cy,		//	転送サイズ
 				dcFrom,		//	Source
 				0, py,		//	転送元
 				SRCCOPY		//	単純コピー
 			);
-			SelectObject( dcTo, hTOldbmp );	//	超重要！
-			//	BitmapがMemoryDCにAssignされている間はbitmapハンドルを
-			//	使っても正しいbitmapが取得できない．
-			//	つまり，DCへの描画命令を発行してもその場でBitmapに
-			//	反映されるわけではない．
-			//	BitmapをDCから取り外して初めて内容の保証ができる
-
-			//	Oct, 23, 2000 genta
-			//	横長のBitmapにしたらアイコンがおかしくなったので，
-			//	1行分を少しずつ追加する方法に変更
-			ImageList_AddMasked( m_hList, hTmpbmp, cTransparent);
-
-			//	MAPし直す
-			SelectObject( dcTo, hTmpbmp );
 		}
+		SelectObject( dcTo, hTOldbmp );	//	超重要！
+		//	BitmapがMemoryDCにAssignされている間はbitmapハンドルを
+		//	使っても正しいbitmapが取得できない．
+		//	つまり，DCへの描画命令を発行してもその場でBitmapに
+		//	反映されるわけではない．
+		//	BitmapをDCから取り外して初めて内容の保証ができる
+
+		//	Oct, 23, 2000 genta
+		//	横長のBitmapにしたらアイコンがおかしくなったので，
+		//	1行分を少しずつ追加する方法に変更
+		
+		//	Oct. 23, 2002 genta
+		//	横長のBitmapでも問題ない
+		//	DCのmap/unmapが速度に大きく影響するため，
+		//	横長のBitmapを作って一括登録するように変更
+		//	これによって250msecくらい速度が改善される．
+		ImageList_AddMasked( m_hList, hTmpbmp, cTransparent);
 	}while(0);	//	1回しか通らない
 
 	//	後処理
