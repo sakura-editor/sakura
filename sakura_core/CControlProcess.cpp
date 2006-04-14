2e426ed299e492bf60e6a1e74314c1186e4f314c
ryoji <ryoji@users.sourceforge.net>
2006-04-14 16:52:54 +0000

    Fix: Tabbar goes out the simultaneous starting time. contributed by ryoji
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk@961 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/CControlProcess.cpp
+++ sakura_core/CControlProcess.cpp
@@ -7,6 +7,7 @@
 /*
 	Copyright (C) 1998-2001, Norio Nakatani
 	Copyright (C) 2002, aroka CProcessより分離, YAZAKI
+	Copyright (C) 2006, ryoji
 
 	This source code is designed for sakura editor.
 	Please contact the copyright holder to use this code for other purpose.
@@ -55,12 +56,13 @@ INT_PTR CALLBACK CControlProcess::ExitingDlgProc(
 	@author aroka
 	@date 2002/01/07
 	@date 2002/02/17 YAZAKI 共有メモリを初期化するのはCProcessに移動。
+	@date 2006/04/10 ryoji 初期化完了イベントの処理を追加、異常時の後始末はデストラクタに任せる
 */
 bool CControlProcess::Initialize()
 {
 	MY_RUNNINGTIMER( cRunningTimer, "CControlProcess::Initialize" );
 
-	// 旧バージョンとの互換性：「異なるバージョン...」が二回出ないように
+	// 旧バージョン（1.2.104.1以前）との互換性：「異なるバージョン...」が二回出ないように
 	m_hMutex = ::CreateMutex( NULL, FALSE, GSTR_MUTEX_SAKURA );
 	if( NULL == m_hMutex ){
 		::MessageBeep( MB_ICONSTOP );
@@ -68,6 +70,17 @@ bool CControlProcess::Initialize()
 			_T("CreateMutex()失敗。\n終了します。") );
 		return false;
 	}
+
+	// 初期化完了イベントを作成する
+	m_hEventCPInitialized = ::CreateEvent( NULL, TRUE, FALSE, GSTR_EVENT_SAKURA_CP_INITIALIZED );
+	if( NULL == m_hEventCPInitialized )
+	{
+		::MessageBeep( MB_ICONSTOP );
+		::MYMESSAGEBOX( NULL, MB_OK | MB_ICONSTOP | MB_TOPMOST, GSTR_APPNAME,
+			_T("CreateEvent()失敗。\n終了します。") );
+		return false;
+	}
+
 	/* コントロールプロセスの目印 */
 	m_hMutexCP = ::CreateMutex( NULL, TRUE, GSTR_MUTEX_SAKURA_CP );
 	if( NULL == m_hMutexCP ){
@@ -77,18 +90,11 @@ bool CControlProcess::Initialize()
 		return false;
 	}
 	if( ERROR_ALREADY_EXISTS == ::GetLastError() ){
-		DWORD dwRet = ::WaitForSingleObject( m_hMutexCP, 0 );
-		if( WAIT_TIMEOUT == dwRet ){// すでにロックされてる
-			::CloseHandle( m_hMutexCP );
-			m_hMutexCP = NULL;
-			return false;
-		}
+		return false;
 	}
 	
 	/* 共有メモリを初期化 */
 	if ( CProcess::Initialize() == false ){
-		::ReleaseMutex( m_hMutexCP );
-		m_hMutexCP = NULL;
 		return false;
 	}
 
@@ -106,14 +112,20 @@ bool CControlProcess::Initialize()
 	MY_TRACETIME( cRunningTimer, "After new CEditApp" );
 
 	if( NULL == ( m_hWnd = m_pcEditApp->Create( m_hInstance ) ) ){
-		::ReleaseMutex( m_hMutexCP );
-		m_hMutexCP = NULL;
 		::MessageBeep( MB_ICONSTOP );
 		::MYMESSAGEBOX( NULL, MB_OK | MB_ICONSTOP | MB_TOPMOST,
 			GSTR_APPNAME, _T("ウィンドウの作成に失敗しました。\n起動できません。") );
 		return false;
 	}
 	m_pShareData->m_hwndTray = m_hWnd;
+
+	// 初期化完了イベントをシグナル状態にする
+	if( !::SetEvent( m_hEventCPInitialized ) ){
+		::MessageBeep( MB_ICONSTOP );
+		::MYMESSAGEBOX( NULL, MB_OK | MB_ICONSTOP | MB_TOPMOST, GSTR_APPNAME,
+			_T("SetEvent()失敗。\n終了します。") );
+		return false;
+	}
 
 	return true;
 }
@@ -177,11 +189,15 @@ CControlProcess::~CControlProcess()
 	if( m_pcEditApp ){
 		delete m_pcEditApp;
 	}
+	if( m_hEventCPInitialized ){
+		::ResetEvent( m_hEventCPInitialized );
+	}
+	::CloseHandle( m_hEventCPInitialized );
 	if( m_hMutexCP ){
 		::ReleaseMutex( m_hMutexCP );
 	}
 	::CloseHandle( m_hMutexCP );
-	// 旧バージョンとの互換性：「異なるバージョン...」が二回出ないように
+	// 旧バージョン（1.2.104.1以前）との互換性：「異なるバージョン...」が二回出ないように
 	if( m_hMutex ){
 		::ReleaseMutex( m_hMutex );
 	}
