e461bd23097cb84c3915848a3ebbb41d756a4407
ryoji <ryoji@users.sourceforge.net>
2007-11-11 12:39:18 +0000

    Fix: Portrait and Landscape radio buttons of the print page setup dialog box become both on.
    Patches#1829778
    
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk@1177 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/CDlgPrintSetting.cpp
+++ sakura_core/CDlgPrintSetting.cpp
@@ -554,7 +554,9 @@ void CDlgPrintSetting::OnChangeSettingType( BOOL bGetData )
 	// 2006.08.14 Moca 用紙方向コンボボックスを廃止し、ボタンを有効化
 	if( m_PrintSettingArr[m_nCurrentPrintSetting].m_nPrintPaperOrientation == DMORIENT_PORTRAIT ){
 		::CheckDlgButton( m_hWnd, IDC_RADIO_PORTRAIT, BST_CHECKED );
+		::CheckDlgButton( m_hWnd, IDC_RADIO_LANDSCAPE, BST_UNCHECKED );
 	}else{
+		::CheckDlgButton( m_hWnd, IDC_RADIO_PORTRAIT, BST_UNCHECKED );
 		::CheckDlgButton( m_hWnd, IDC_RADIO_LANDSCAPE, BST_CHECKED );
 	}
 
