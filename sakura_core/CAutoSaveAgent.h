2d8c62937d3a761b4e07ad5a496b3b99d6efc78e
kobake <kobake@users.sourceforge.net>
2008-03-27 16:18:09 +0000

    CAutoSaveファイルをCAutoSaveAgentに名前変更、CAutoSaveAgentクラスを作成。
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/branches/unicode2@1233 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/CAutoSaveAgent.h
+++ sakura_core/CAutoSaveAgent.h
@@ -31,9 +31,11 @@
 #define __CAUTOSAVE_H_
 #include <windows.h>
 #include "global.h"
+#include "CDocListener.h"
 
 //! 分→ミリ秒に変換するための係数
 const int MSec2Min = 1000 * 60;
+
 /*! @class CPassiveTimer CAutoSave.h
 	基準時刻からの経過時間が設定間隔を過ぎたかどうかを判定する。
 	頻繁に呼び出されるタイマーが既に別の場所にあるとき、それよりも間隔が広くて
@@ -48,29 +50,38 @@ public:
 	*/
 	CPassiveTimer() : nInterval(1), bEnabled(false){ Reset(); }
 
-	//!	時間間隔の設定
-	void SetInterval(int m);
+	//時間間隔
+	void SetInterval(int m);	//!	時間間隔の設定
+	int GetInterval(void) const {return nInterval / MSec2Min; }	//!< 時間間隔の取得
+	void Reset(void){ nLastTick = ::GetTickCount(); }			//!< 基準時刻のリセット
 
-	//!	時間間隔の取得
-	int GetInterval(void) const {return nInterval / MSec2Min; }
-
-	//!	基準時刻のリセット
-	void Reset(void){ nLastTick = ::GetTickCount(); }
-
-	//!	有効／無効の設定
-	void Enable(bool flag);
-	//!	有効／無効の読み出し
-	bool IsEnabled(void) const { return bEnabled; }
+	//有効／無効
+	void Enable(bool flag);							//!< 有効／無効の設定
+	bool IsEnabled(void) const { return bEnabled; }	//!< 有効／無効の読み出し
 
 	//!	規定時間に達したかどうかの判定
 	bool CheckAction(void);
 
 private:
-	DWORD nLastTick;	//!<	最後にチェックしたときの時刻 (GetTickCount()で取得したもの)
-	int  nInterval;		//!<	Action間隔 (分)
-	bool bEnabled;	//!<	有効かどうか
+	DWORD	nLastTick;	//!< 最後にチェックしたときの時刻 (GetTickCount()で取得したもの)
+	int		nInterval;	//!< Action間隔 (分)
+	bool	bEnabled;	//!< 有効かどうか
 };
+
+
+
+class CAutoSaveAgent : public CDocListenerEx{
+public:
+	void CheckAutoSave();
+	void ReloadAutoSaveParam();	//!< 設定をSharedAreaから読み出す
+
+private:
+	CPassiveTimer m_cPassiveTimer;
+};
+
+
+
+
+
 #endif
 
-
-/*[EOF]*/
