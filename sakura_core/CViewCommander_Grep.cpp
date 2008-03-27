f99a2a09b96fd98e37f9253e3f14349f119d746e
kobake <kobake@users.sourceforge.net>
2008-03-27 17:53:09 +0000

    UnderCommiting: 整理
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/branches/unicode2@1260 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/CViewCommander_Grep.cpp
+++ sakura_core/CViewCommander_Grep.cpp
@@ -27,7 +27,7 @@ void CViewCommander::Command_GREP_DIALOG( void )
 	wcscpy( GetEditWindow()->m_cDlgGrep.m_szText, cmemCurText.GetStringPtr() );
 
 	/* Grepダイアログの表示 */
-	int nRet = GetEditWindow()->m_cDlgGrep.DoModal( GetInstance(), m_pCommanderView->m_hWnd, GetDocument()->GetFilePath() );
+	int nRet = GetEditWindow()->m_cDlgGrep.DoModal( GetInstance(), m_pCommanderView->GetHwnd(), GetDocument()->m_cDocFile.GetFilePath() );
 //	MYTRACE_A( "nRet=%d\n", nRet );
 	if( FALSE == nRet ){
 		return;
@@ -49,7 +49,7 @@ void CViewCommander::Command_GREP( void )
 	if( GetShareData()->m_nEditArrNum >= MAX_EDITWINDOWS ){	//最大値修正	//@@@ 2003.05.31 MIK
 		TCHAR szMsg[512];
 		auto_sprintf( szMsg, _T("編集ウィンドウ数の上限は%dです。\nこれ以上は同時に開けません。"), MAX_EDITWINDOWS );
-		::MessageBox( m_pCommanderView->m_hWnd, szMsg, GSTR_APPNAME, MB_OK );
+		::MessageBox( m_pCommanderView->GetHwnd(), szMsg, GSTR_APPNAME, MB_OK );
 		return;
 	}
 	cmWork1.SetString( GetEditWindow()->m_cDlgGrep.m_szText );
@@ -59,11 +59,12 @@ void CViewCommander::Command_GREP( void )
 	/*	今のEditViewにGrep結果を表示する。
 		Grepモードのとき。または、変更フラグがオフで、ファイルを読み込んでいない場合。
 	*/
-	if( GetDocument()->m_bGrepMode ||
-		( !GetDocument()->IsModified() &&
-		  !GetDocument()->IsFilePathAvailable() )		/* 現在編集中のファイルのパス */
+	if( CEditApp::Instance()->m_pcGrepAgent->m_bGrepMode ||
+		( !GetDocument()->m_cDocEditor.IsModified() &&
+		  !GetDocument()->m_cDocFile.GetFilePathClass().IsValidPath() )		/* 現在編集中のファイルのパス */
 	){
-		m_pCommanderView->DoGrep(
+		CEditApp::Instance()->m_pcGrepAgent->DoGrep(
+			m_pCommanderView,
 			&cmWork1,
 			&cmWork2,
 			&cmWork3,
@@ -77,60 +78,41 @@ void CViewCommander::Command_GREP( void )
 	else{
 		/*======= Grepの実行 =============*/
 		/* Grep結果ウィンドウの表示 */
-		TCHAR*	pCmdLine = new TCHAR[1024];
-		TCHAR*	pOpt = new TCHAR[64];
-//		int		nDataLen;
 		cmWork1.Replace( L"\"", L"\"\"" );
 		cmWork2.Replace( _T("\""), _T("\"\"") );
 		cmWork3.Replace( _T("\""), _T("\"\"") );
-		/*
-		|| -GREPMODE -GKEY="1" -GFILE="*.*;*.c;*.h" -GFOLDER="c:\" -GCODE=0 -GOPT=S
-		*/
-		auto_sprintf(
-			pCmdLine, _T("-GREPMODE -GKEY=\"%ls\" -GFILE=\"%ls\" -GFOLDER=\"%ts\" -GCODE=%d"),
+
+		// -GREPMODE -GKEY="1" -GFILE="*.*;*.c;*.h" -GFOLDER="c:\" -GCODE=0 -GOPT=S
+		CCommandLineString cCmdLine;
+		cCmdLine.AppendF(
+			_T("-GREPMODE -GKEY=\"%ls\" -GFILE=\"%ls\" -GFOLDER=\"%ts\" -GCODE=%d"),
 			cmWork1.GetStringPtr(),
 			cmWork2.GetStringPtr(),
 			cmWork3.GetStringPtr(),
 			GetEditWindow()->m_cDlgGrep.m_nGrepCharSet
 		);
+
+		//GOPTオプション
+		TCHAR	pOpt[64];
 		pOpt[0] = _T('\0');
-		if( GetEditWindow()->m_cDlgGrep.m_bSubFolder ){	/* サブフォルダからも検索する */
-			_tcscat( pOpt, _T("S") );
-		}
-	//	if( m_bFromThisText ){	/* この編集中のテキストから検索する */
-	//
-	//	}
-		if( GetEditWindow()->m_cDlgGrep.m_sSearchOption.bWordOnly ){	/* 単語単位で探す */
-			_tcscat( pOpt, _T("W") );
-		}
-		if( GetEditWindow()->m_cDlgGrep.m_sSearchOption.bLoHiCase ){	/* 英大文字と英小文字を区別する */
-			_tcscat( pOpt, _T("L") );
-		}
-		if( GetEditWindow()->m_cDlgGrep.m_sSearchOption.bRegularExp ){	/* 正規表現 */
-			_tcscat( pOpt, _T("R") );
-		}
-//	2002/09/20 Moca 文字コードセットオプションに統合
-//		if( GetEditWindow()->m_cDlgGrep.m_KanjiCode_AutoDetect ){	/* 文字コード自動判別 */
-//			_tcscat( pOpt, _T("K") );
-//		}
-		if( GetEditWindow()->m_cDlgGrep.m_bGrepOutputLine ){	/* 行を出力するか該当部分だけ出力するか */
-			_tcscat( pOpt, _T("P") );
-		}
-		if( 1 == GetEditWindow()->m_cDlgGrep.m_nGrepOutputStyle ){	/* Grep: 出力形式 */
-			_tcscat( pOpt, _T("1") );
-		}
-		if( 2 == GetEditWindow()->m_cDlgGrep.m_nGrepOutputStyle ){	/* Grep: 出力形式 */
-			_tcscat( pOpt, _T("2") );
-		}
+		if( GetEditWindow()->m_cDlgGrep.m_bSubFolder				)_tcscat( pOpt, _T("S") );	// サブフォルダからも検索する
+		if( GetEditWindow()->m_cDlgGrep.m_sSearchOption.bWordOnly	)_tcscat( pOpt, _T("W") );	// 単語単位で探す
+		if( GetEditWindow()->m_cDlgGrep.m_sSearchOption.bLoHiCase	)_tcscat( pOpt, _T("L") );	// 英大文字と英小文字を区別する
+		if( GetEditWindow()->m_cDlgGrep.m_sSearchOption.bRegularExp	)_tcscat( pOpt, _T("R") );	// 正規表現
+		if( GetEditWindow()->m_cDlgGrep.m_bGrepOutputLine			)_tcscat( pOpt, _T("P") );	// 行を出力するか該当部分だけ出力するか
+		if( 1 == GetEditWindow()->m_cDlgGrep.m_nGrepOutputStyle		)_tcscat( pOpt, _T("1") );	// Grep: 出力形式
+		if( 2 == GetEditWindow()->m_cDlgGrep.m_nGrepOutputStyle		)_tcscat( pOpt, _T("2") );	// Grep: 出力形式
 		if( 0 < _tcslen( pOpt ) ){
-			_tcscat( pCmdLine, _T(" -GOPT=") );
-			_tcscat( pCmdLine, pOpt );
+			cCmdLine.AppendF( _T(" -GOPT="), pOpt );
 		}
+
 //		MYTRACE_A( "pCmdLine=[%ls]\n", pCmdLine );
 		/* 新規編集ウィンドウの追加 ver 0 */
-		CControlTray::OpenNewEditor( GetInstance(), m_pCommanderView->m_hWnd, pCmdLine, CODE_DEFAULT, FALSE );
-		delete [] pCmdLine;
-		delete [] pOpt;
+		SLoadInfo sLoadInfo;
+		sLoadInfo.cFilePath = _T("");
+		sLoadInfo.eCharCode = CODE_DEFAULT;
+		sLoadInfo.bViewMode = false;
+		CControlTray::OpenNewEditor( GetInstance(), m_pCommanderView->GetHwnd(), sLoadInfo, cCmdLine.c_str() );
 		/*======= Grepの実行 =============*/
 		/* Grep結果ウィンドウの表示 */
 	}
