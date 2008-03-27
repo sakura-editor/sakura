522a3462d25cbac71096922c6a7447d66fe8f8ab
kobake <kobake@users.sourceforge.net>
2008-03-27 17:57:29 +0000

    UnderCommiting: 文字列型
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/branches/unicode2@1263 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/basis/CMyString.h
+++ sakura_core/basis/CMyString.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include "util/string_ex.h"
 
 #define m_delete2(p) do{ if(p){ delete[] p; p=0; } }while(0)
 
@@ -62,6 +63,63 @@ private:
 //ã§í É}ÉNÉç
 #define _FT _T
 #include "util/StaticType.h"
+
 //ã§í å^
 typedef StaticString<TCHAR,_MAX_PATH> SFilePath;
+class CFilePath : public StaticString<TCHAR,_MAX_PATH>{
+private:
+	typedef StaticString<TCHAR,_MAX_PATH> Super;
+public:
+	CFilePath() : Super() { }
+	CFilePath(const TCHAR* rhs) : Super(rhs) { }
 
+	bool IsValidPath() const{ return At(0)!=_T('\0'); }
+	std::tstring GetDirPath() const
+	{
+		TCHAR	szDirPath[_MAX_PATH];
+		TCHAR	szDrive[_MAX_DRIVE];
+		TCHAR	szDir[_MAX_DIR];
+		_tsplitpath( this->c_str(), szDrive, szDir, NULL, NULL );
+		_tcscpy( szDirPath, szDrive);
+		_tcscat( szDirPath, szDir );
+		return szDirPath;
+	}
+	LPCTSTR GetExt() const
+	{
+		const TCHAR* head = c_str();
+		const TCHAR* p = auto_strchr(head,_T('\0')) - 1;
+		while(p>=head){
+			if(*p==_T('.'))break;
+			if(*p==_T('\\'))break;
+			if(*p==_T('/'))break;
+			p--;
+		}
+		if(p>=head && *p==_T('.'))return p;
+		else return _T("");
+	}
+};
+
+
+//$$ âº
+class CCommandLineString{
+public:
+	CCommandLineString()
+	{
+		m_szCmdLine[0] = _T('\0');
+		m_pHead = m_szCmdLine;
+	}
+	void AppendF(const TCHAR* szFormat, ...)
+	{
+		va_list v;
+		va_start(v,szFormat);
+		m_pHead+=auto_vsprintf_s(m_pHead,_countof(m_szCmdLine)-(m_pHead-m_szCmdLine),szFormat,v);
+		va_end(v);
+	}
+	const TCHAR* c_str() const
+	{
+		return m_szCmdLine;
+	}
+private:
+	TCHAR	m_szCmdLine[1024];
+	TCHAR*	m_pHead;
+};
