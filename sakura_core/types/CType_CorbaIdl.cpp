6f1e08f49d56f83a244163627dcb27ae6530f324
kobake <kobake@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2008-05-03 06:40:42 +0000

    rev1310での不足ファイルを追加
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/branches/unicode2@1311 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

---
+++ sakura_core/types/CType_CorbaIdl.cpp
@@ -0,0 +1,43 @@
+#include "stdafx.h"
+#include "types/CType.h"
+
+const wchar_t* g_ppszKeywordsCORBA_IDL[] = {
+	L"any",
+	L"attribute",
+	L"boolean",
+	L"case",
+	L"char",
+	L"const",
+	L"context",
+	L"default",
+	L"double",
+	L"enum",
+	L"exception",
+	L"FALSE",
+	L"fixed",
+	L"float",
+	L"in",
+	L"inout",
+	L"interface",
+	L"long",
+	L"module",
+	L"Object",
+	L"octet",
+	L"oneway",
+	L"out",
+	L"raises",
+	L"readonly",
+	L"sequence",
+	L"short",
+	L"string",
+	L"struct",
+	L"switch",
+	L"TRUE",
+	L"typedef",
+	L"unsigned",
+	L"union",
+	L"void",
+	L"wchar_t",
+	L"wstring"
+};
+int g_nKeywordsCORBA_IDL = _countof(g_ppszKeywordsCORBA_IDL);
