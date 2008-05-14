3cd3ddcc1282a622d85f1e4266014082b4c66249
ryoji <ryoji@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2008-05-14 16:41:30 +0000

    Fix: 改行コード指定の保存で画面がスクロールする
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/branches/unicode2@1340 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/doc/CDocVisitor.cpp
+++ sakura_core/doc/CDocVisitor.cpp
@@ -13,6 +13,12 @@ void CDocVisitor::SetAllEol(CEol cEol)
 		pcOpeBlk = new COpeBlk();
 	}
 
+	//カーソル位置記憶
+	CLayoutInt		nViewTopLine = pcView->GetTextArea().GetViewTopLine();
+	CLayoutInt		nViewLeftCol = pcView->GetTextArea().GetViewLeftCol();
+	CLayoutPoint	ptCaretPosXY = pcView->GetCaret().GetCaretLayoutPos();
+	CLayoutInt		nCaretPosX_Prev = pcView->GetCaret().m_nCaretPosX_Prev;
+
 	//改行コードを統一する
 	if(cEol.IsValid()){
 		CLogicInt	nLine = CLogicInt(0);
@@ -43,6 +49,18 @@ void CDocVisitor::SetAllEol(CEol cEol)
 	//アンドゥ記録
 	if(pcOpeBlk){
 		if(pcOpeBlk->GetNum()>0){
+			// カーソル位置復元
+			pcView->GetTextArea().SetViewTopLine(nViewTopLine);
+			pcView->GetTextArea().SetViewLeftCol(nViewLeftCol);
+			pcView->GetCaret().MoveCursor( ptCaretPosXY, TRUE );
+			pcView->GetCaret().m_nCaretPosX_Prev = nCaretPosX_Prev;
+			pcOpeBlk->AppendOpe(
+				new CMoveCaretOpe(
+					pcView->GetCaret().GetCaretLogicPos(),
+					pcView->GetCaret().GetCaretLogicPos()
+				)
+			);
+
 			m_pcDocRef->m_cDocEditor.m_cOpeBuf.AppendOpeBlk(pcOpeBlk);
 		}
 		else{
