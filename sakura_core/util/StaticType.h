119da20c818673d28fa759c42e3bf216d6caeb58
ryoji <ryoji@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2008-05-24 14:23:17 +0000

    Fix: 「閉じて開く」で異常終了する Fix: 画面が書き禁止状態のときに「閉じて(無題)」を実行すると上書き禁止の(無題)画面になる Fix: ファイル排他制御の設定を「排他制御しない」に変更しても現在開いているファイルのロックが解除されない
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/branches/unicode2@1354 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/util/StaticType.h
+++ sakura_core/util/StaticType.h
@@ -51,7 +51,7 @@ public:
 public:
 	//コンストラクタ・デストラクタ
 	StaticString(){ m_szData[0]=0; }
-	StaticString(const CHAR_TYPE* rhs){ auto_strcpy(m_szData,rhs); }
+	StaticString(const CHAR_TYPE* rhs){ if(!rhs) m_szData[0]=0; else auto_strcpy(m_szData,rhs); }
 
 	//クラス属性
 	size_t GetBufferCount() const{ return N_BUFFER_COUNT; }
@@ -67,7 +67,7 @@ public:
 	CHAR_TYPE At(int nIndex) const{ return m_szData[nIndex]; }
 
 	//簡易コピー
-	void Assign(const CHAR_TYPE* src){ auto_strcpy_s(m_szData,_countof(m_szData),src); }
+	void Assign(const CHAR_TYPE* src){ if(!src) m_szData[0]=0; else auto_strcpy_s(m_szData,_countof(m_szData),src); }
 	Me& operator = (const CHAR_TYPE* src){ Assign(src); return *this; }
 
 	//各種メソッド
