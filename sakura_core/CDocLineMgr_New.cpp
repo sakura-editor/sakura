ef31b040147c1ba23afc804cc1ac55d8f4709b74
ryoji <ryoji@users.sourceforge.net>
2008-06-06 14:54:31 +0000

    Fix: When the wide range (several thousand lines) it deletes, it ends abnormally.
    Patches#1977756
    
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk@1369 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/CDocLineMgr_New.cpp
+++ sakura_core/CDocLineMgr_New.cpp
@@ -11,6 +11,7 @@
 	Copyright (C) 2002, hor, aroka, MIK, Moca, genta
 	Copyright (C) 2003, Moca, genta
 	Copyright (C) 2005, かろと
+	Copyright (C) 2008, ryoji
 
 	This source code is designed for sakura editor.
 	Please contact the copyright holder to use this code for other purpose.
@@ -445,7 +446,11 @@ prev_line:;
 	pArg->nInsLineNum = m_nLines - nAllLinesOld;
 end_of_func:;
 	if( NULL != pCDlgCancel ){
-		delete pCDlgCancel;
+		// 進捗ダイアログを表示しない場合と同じ動きになるようにダイアログは遅延破棄する
+		// ここで pCDlgCancel を delete すると delete から戻るまでの間に
+		// ダイアログ破棄 -> 編集画面へフォーカス移動 -> キャレット位置調整
+		// まで一気に動くので無効なレイアウト情報参照で異常終了することがある
+		pCDlgCancel->DeleteAsync();	// 自動破棄を遅延実行する	// 2008.05.28 ryoji
 	}
 	return;
 }
