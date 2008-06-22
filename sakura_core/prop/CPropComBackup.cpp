be8135baeab8ff5160e32dca84f96332a6b03813
uchi5537 <uchi5537@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2008-06-22 01:25:53 +0000

    Imp: 『共通設定』タブ順変更
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/branches/unicode2@1399 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/prop/CPropComBackup.cpp
+++ sakura_core/prop/CPropComBackup.cpp
@@ -139,7 +139,7 @@ INT_PTR CPropCommon::DispatchEvent_PROP_BACKUP( HWND hwndDlg, UINT uMsg, WPARAM 
 				return TRUE;
 //@@@ 2002.01.03 YAZAKI 最後に表示していたシートを正しく覚えていないバグ修正
 			case PSN_SETACTIVE:
-				m_nPageNum = ID_PAGENUM_BACKUP;	//Oct. 25, 2000 JEPRO ZENPAN1→ZENPAN に変更(参照しているのはCPropCommon.cppのみの1箇所)
+				m_nPageNum = ID_PAGENUM_BACKUP;
 				return TRUE;
 			}
 			break;
@@ -664,4 +664,3 @@ void CPropCommon::UpdateBackupFile(HWND hwndDlg)	//	バックアップファイルの詳細設
 	}
 	return;
 }
-
