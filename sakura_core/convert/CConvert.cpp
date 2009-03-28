ad48a5edc29c951f30580f3beb3a6fccccb94dea
ryoji <ryoji@users.sourceforge.net>
2009-03-28 19:26:03 +0000

    Imp: 文字コード変換機能の実装
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@1562 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/convert/CConvert.cpp
+++ sakura_core/convert/CConvert.cpp
@@ -3,8 +3,13 @@
 #include "func/Funccode.h"
 #include "CEol.h"
 #include "charset/charcode.h"
+#include "charset/CCodeMediator.h"
 #include "charset/CShiftJis.h"
 #include "charset/CJis.h"
+#include "charset/CEuc.h"
+#include "charset/CUnicodeBe.h"
+#include "charset/CUtf8.h"
+#include "charset/CUtf7.h"
 #include "CConvert_ToLower.h"
 #include "CConvert_ToUpper.h"
 #include "CConvert_ToHankaku.h"
@@ -22,11 +27,41 @@
 /* 機能種別によるバッファの変換 */
 void CConvertMediator::ConvMemory( CNativeW* pCMemory, EFunctionCode nFuncCode, int nTabWidth )
 {
+	// コード変換はできるだけANSI版のsakuraと互換の結果が得られるように実装する	// 2009.03.26 ryoji
+	// xxx2SJIS:
+	//   1. バッファの内容がANSI版相当になるよう Unicode→SJIS 変換する
+	//   2. xxx→SJIS 変換後にバッファ内容をUNICODE版相当に戻す（SJIS→Unicode）のと等価な結果を得るために xxx→Unicode 変換する
+	// SJIS2xxx:
+	//   1. バッファ内容をANSI版相当に変換（Unicode→SJIS）後に SJIS→xxx 変換するのと等価な結果を得るために Unicode→xxx 変換する
+	//   2. バッファ内容をUNICODE版相当に戻すために SJIS→Unicode 変換する
+
 	switch( nFuncCode ){
-	//コード変換(2SJIS)
+	//コード変換(xxx2SJIS)
+	case F_CODECNV_AUTO2SJIS:
 	case F_CODECNV_EMAIL:
+	case F_CODECNV_EUC2SJIS:
+	case F_CODECNV_UNICODE2SJIS:
+	case F_CODECNV_UNICODEBE2SJIS:
+	case F_CODECNV_UTF82SJIS:
+	case F_CODECNV_UTF72SJIS:
 		CShiftJis::UnicodeToSJIS(pCMemory->_GetMemory());
 		break;
+	//コード変換(SJIS2xxx)
+	case F_CODECNV_SJIS2JIS:		CJis::UnicodeToJIS(pCMemory->_GetMemory());			break;
+	case F_CODECNV_SJIS2EUC:		CEuc::UnicodeToEUC(pCMemory->_GetMemory());			break;
+	case F_CODECNV_SJIS2UTF8:		CUtf8::UnicodeToUTF8(pCMemory->_GetMemory());		break;
+	case F_CODECNV_SJIS2UTF7:		CUtf7::UnicodeToUTF7(pCMemory->_GetMemory());		break;
+	}
+
+	if( nFuncCode == F_CODECNV_AUTO2SJIS ){
+		switch( CCodeMediator::CheckKanjiCode( reinterpret_cast<const char*>(pCMemory->_GetMemory()->GetRawPtr()), pCMemory->_GetMemory()->GetRawLength() ) ){
+		case CODE_JIS:			nFuncCode = F_CODECNV_EMAIL;			break;
+		case CODE_EUC:			nFuncCode = F_CODECNV_EUC2SJIS;			break;
+		case CODE_UNICODE:		nFuncCode = F_CODECNV_UNICODE2SJIS;		break;
+		case CODE_UNICODEBE:	nFuncCode = F_CODECNV_UNICODEBE2SJIS;	break;
+		case CODE_UTF8:			nFuncCode = F_CODECNV_UTF82SJIS;		break;
+		case CODE_UTF7:			nFuncCode = F_CODECNV_UTF72SJIS;		break;
+		}
 	}
 
 	switch( nFuncCode ){
@@ -39,16 +74,29 @@ void CConvertMediator::ConvMemory( CNativeW* pCMemory, EFunctionCode nFuncCode, 
 	case F_TOHANEI:					CConvert_ZeneisuToHaneisu().CallConvert(pCMemory);	break;	// 全角英数→半角英数
 	case F_TOZENKAKUKATA:			CConvert_ToZenkana().CallConvert(pCMemory);			break;	// 半角＋全ひら→全角・カタカナ
 	case F_TOZENKAKUHIRA:			CConvert_ToZenhira().CallConvert(pCMemory);			break;	// 半角＋全カタ→全角・ひらがな
-	case F_HANKATATOZENKATA:	CConvert_HankanaToZenkana().CallConvert(pCMemory);	break;	// 半角カタカナ→全角カタカナ
-	case F_HANKATATOZENHIRA:	CConvert_HankanaToZenhira().CallConvert(pCMemory);	break;	// 半角カタカナ→全角ひらがな
+	case F_HANKATATOZENKATA:		CConvert_HankanaToZenkana().CallConvert(pCMemory);	break;	// 半角カタカナ→全角カタカナ
+	case F_HANKATATOZENHIRA:		CConvert_HankanaToZenhira().CallConvert(pCMemory);	break;	// 半角カタカナ→全角ひらがな
 	//文字種変換、整形
 	case F_TABTOSPACE:				CConvert_TabToSpace(nTabWidth).CallConvert(pCMemory);break;	// TAB→空白
 	case F_SPACETOTAB:				CConvert_SpaceToTab(nTabWidth).CallConvert(pCMemory);break;	// 空白→TAB
 	case F_LTRIM:					CConvert_Trim(true).CallConvert(pCMemory);			break;	// 2001.12.03 hor
 	case F_RTRIM:					CConvert_Trim(false).CallConvert(pCMemory);			break;	// 2001.12.03 hor
-	//コード変換(2SJIS)
+	//コード変換(xxx2SJIS)
 	case F_CODECNV_EMAIL:			CJis::JISToUnicode(pCMemory->_GetMemory(), true);	break;
+	case F_CODECNV_EUC2SJIS:		CEuc::EUCToUnicode(pCMemory->_GetMemory());			break;
+	case F_CODECNV_UNICODE2SJIS:	/* 無変換 */										break;
+	case F_CODECNV_UNICODEBE2SJIS:	CUnicodeBe::UnicodeBEToUnicode(pCMemory->_GetMemory());	break;
+	case F_CODECNV_UTF82SJIS:		CUtf8::UTF8ToUnicode(pCMemory->_GetMemory());		break;
+	case F_CODECNV_UTF72SJIS:		CUtf7::UTF7ToUnicode(pCMemory->_GetMemory());		break;
+	//コード変換(SJIS2xxx)
+	case F_CODECNV_SJIS2JIS:
+	case F_CODECNV_SJIS2EUC:
+	case F_CODECNV_SJIS2UTF8:
+	case F_CODECNV_SJIS2UTF7:
+		CShiftJis::SJISToUnicode(pCMemory->_GetMemory());
+		break;
 	}
+
 	return;
 }
 
