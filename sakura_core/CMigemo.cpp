78b12daa75ad9efda77b2ac086dde8aa83da0abe
ryoji <ryoji@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2009-05-24 01:31:23 +0000

    Fix MIGEMOインクリメンタルサーチの不具合修正 Contributed by miau
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@1574 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/CMigemo.cpp
+++ sakura_core/CMigemo.cpp
@@ -7,6 +7,7 @@
 /*
 	Copyright (C) 2004, isearch
 	Copyright (C) 2007, ryoji
+	Copyright (C) 2009, miau
 
 	This source code is designed for sakura editor.
 	Please contact the copyright holder to use this code for other purpose.
@@ -104,6 +105,7 @@ long CMigemo::migemo_open(char* dict)
 	if (m_migemo == NULL)
 		return 0;
 	
+	migemo_setproc_int2char(pcre_int2char);	// 2009.04.30 miau
 	//if (!migemo_load(MIGEMO_DICTID_MIGEMO, path2))
 	//	return 0;
 	
@@ -226,4 +228,39 @@ CMigemo::~CMigemo()
 }
 
 
+// C/Migemo ソース中の rxgen.c:default_int2char を元に作成。	// 2009.04.30 miau
+static int __cdecl pcre_int2char(unsigned int in, unsigned char* out)
+{
+    /* outは最低でも16バイトはある、という仮定を置く */
+    if (in >= 0x100)
+    {
+	if (out)
+	{
+	    out[0] = (unsigned char)((in >> 8) & 0xFF);
+	    out[1] = (unsigned char)(in & 0xFF);
+	}
+	return 2;
+    }
+    else
+    {
+	int len = 0;
+	switch (in)
+	{
+	    case '\\':
+	    case '.': case '*': case '^': case '$': case '/':
+	    case '[': case ']': case '~':
+	    case '|': case '(': case ')':
+	    case '+': case '?': case '{': case '}':
+		if (out)
+		    out[len] = '\\';
+		++len;
+	    default:
+		if (out)
+		    out[len] = (unsigned char)(in & 0xFF);
+		++len;
+		break;
+	}
+	return len;
+    }
+}
 
