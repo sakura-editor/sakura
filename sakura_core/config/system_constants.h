e1922cc18cd3b1fccade5a3c73ff81644c14d61d
uchi5537 <uchi5537@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2010-06-20 11:43:47 +0000

    Chg:ICONをすべてのコマンドに割り当てる Chg:TabCloseOther、TabCloseLeft、TabCloseRightマクロ名追加
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@1779 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/config/system_constants.h
+++ sakura_core/config/system_constants.h
@@ -285,12 +285,15 @@
 	Version 99:
 	Backup Removable Media 対応  2010/5/27 Uchi
 
+	Version 100:
+	ICONをすべてのコマンドに割り当てる	2010/6/12 Uchi
+
 	-- 統合されたので元に戻す（1000～1023が使用済み） 	2008.11.16 nasukoji
 	-- Version 1000:
 	-- バージョン1000以降を本家統合までの間、使わせてください。かなり頻繁に構成が変更されると思われるので。by kobake 2008.03.02
 
 */
-#define N_SHAREDATA_VERSION		99
+#define N_SHAREDATA_VERSION		100
 #define STR_SHAREDATA_VERSION	NUM_TO_STR(N_SHAREDATA_VERSION)
 #define	GSTR_SHAREDATA	(_T("SakuraShareData") _T(_CODE_SUFFIX_) _T(_DEBUG_SUFFIX_) _T(STR_SHAREDATA_VERSION))
 
