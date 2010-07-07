0b949ed9f2630aafd22c0a275ba0a458e54c213e
moca_skr <moca_skr@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2010-07-07 16:50:21 +0000

    Keep: MAX_TYPESを変更してもコンパイルできるように
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@1784 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/types/CType_Others.cpp
+++ sakura_core/types/CType_Others.cpp
@@ -1,25 +1,10 @@
 #include "StdAfx.h"
 #include "types/CType.h"
 
-#define IMPLEMENT_CTYPE_OTHER(CLASS_NAME, TYPE_NAME) \
-void CLASS_NAME::InitTypeConfigImp(STypeConfig* pType) \
-{ \
-	_tcscpy( pType->m_szTypeName, _T(#TYPE_NAME) ); \
-	_tcscpy( pType->m_szTypeExts, _T("") ); \
+void CType_Other::InitTypeConfigImp(STypeConfig* pType)
+{
+	//ñºëOÇ∆ägí£éq
+	auto_sprintf( pType->m_szTypeName, _T("ê›íË%d"), pType->m_nIdx + 1 );
+	_tcscpy( pType->m_szTypeExts, _T("") );
+
 }
-
-IMPLEMENT_CTYPE_OTHER(CType_Other17, ê›íË17)
-IMPLEMENT_CTYPE_OTHER(CType_Other18, ê›íË18)
-IMPLEMENT_CTYPE_OTHER(CType_Other19, ê›íË19)
-IMPLEMENT_CTYPE_OTHER(CType_Other20, ê›íË20)
-IMPLEMENT_CTYPE_OTHER(CType_Other21, ê›íË21)
-IMPLEMENT_CTYPE_OTHER(CType_Other22, ê›íË22)
-IMPLEMENT_CTYPE_OTHER(CType_Other23, ê›íË23)
-IMPLEMENT_CTYPE_OTHER(CType_Other24, ê›íË24)
-IMPLEMENT_CTYPE_OTHER(CType_Other25, ê›íË25)
-IMPLEMENT_CTYPE_OTHER(CType_Other26, ê›íË26)
-IMPLEMENT_CTYPE_OTHER(CType_Other27, ê›íË27)
-IMPLEMENT_CTYPE_OTHER(CType_Other28, ê›íË28)
-IMPLEMENT_CTYPE_OTHER(CType_Other29, ê›íË29)
-IMPLEMENT_CTYPE_OTHER(CType_Other30, ê›íË30)
-
