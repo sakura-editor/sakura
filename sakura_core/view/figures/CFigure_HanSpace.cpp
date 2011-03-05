3e00c43724aecbbb452b100f9a9b3e33b1d0911c
ryoji <ryoji@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2011-03-05 12:56:46 +0000

    Fix: 行間隔を広くすると半角空白が描画されない Wiki BugReport/68
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@1901 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/view/figures/CFigure_HanSpace.cpp
+++ sakura_core/view/figures/CFigure_HanSpace.cpp
@@ -33,7 +33,7 @@ void CFigure_HanSpace::DispSpace(CGraphics& gr, DispPos* pDispPos, CEditView* pc
 	{
 		//小文字"o"の下半分を出力
 		CMyRect rcClipBottom=rcClip;
-		rcClipBottom.top=rcClip.top+rcClip.Height()/2;
+		rcClipBottom.top=rcClip.top+pcView->GetTextMetrics().GetHankakuHeight()/2;
 		::ExtTextOutW_AnyBuild(
 			gr,
 			pDispPos->GetDrawPos().x,
@@ -47,7 +47,7 @@ void CFigure_HanSpace::DispSpace(CGraphics& gr, DispPos* pDispPos, CEditView* pc
 		
 		//上半分は普通の空白で出力（"o"の上半分を消す）
 		CMyRect rcClipTop=rcClip;
-		rcClipTop.bottom=rcClip.top+rcClip.Height()/2;
+		rcClipTop.bottom=rcClip.top+pcView->GetTextMetrics().GetHankakuHeight()/2;
 		::ExtTextOutW_AnyBuild(
 			gr,
 			pDispPos->GetDrawPos().x,
