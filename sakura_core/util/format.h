65f963be40bc9d1279e8e21f920724e8439d326e
syat <syat@users.sourceforge.net>
2011-05-29 02:49:09 +0000

    Keep: rev1917の文字化けを修正
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@1919 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/util/format.h
+++ sakura_core/util/format.h
@@ -1,9 +1,9 @@
-// 2007.10.20 kobake Ö˜A
+// 2007.10.20 kobake ‘®ŠÖ˜A
 
 #pragma once
 
 
 // 20051121 aroka
 bool GetDateTimeFormat( TCHAR* szResult, int size, const TCHAR* format, const SYSTEMTIME& systime );
-UINT32 ParseVersion( const TCHAR* ver );	//o[WÔÌ‰
-int CompareVersion( const TCHAR* verA, const TCHAR* verB );	//o[WÔÌ”r
+UINT32 ParseVersion( const TCHAR* ver );	//ƒo[ƒWƒ‡ƒ“”Ô†‚Ì‰ğÍ
+int CompareVersion( const TCHAR* verA, const TCHAR* verB );	//ƒo[ƒWƒ‡ƒ“”Ô†‚Ì”äŠr
