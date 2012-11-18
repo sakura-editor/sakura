536fb13d7fc95caa5e4c516b3eae13a8eccb813d
uchi5537 <uchi5537@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2012-11-18 10:49:14 +0000

    keep:CViewCommander.cppにCSmartIndentIfObjが組み込んで有るので分離します Patchunicode#268
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@2472 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/CViewCommander.cpp
+++ sakura_core/CViewCommander.cpp
@@ -67,6 +67,7 @@
 #include "env/CSakuraEnvironment.h"
 #include "plugin/CJackManager.h"
 #include "plugin/COutlineIfObj.h"
+#include "plugin/CSmartIndentIfObj.h"
 #include "CPropertyManager.h"
 #include "CAppMode.h"
 #include "CGrepAgent.h"
@@ -108,76 +109,6 @@ CCaret& CViewCommander::GetCaret()
 	return m_pCommanderView->GetCaret();
 }
 
-// スマートインデント用WSHオブジェクト
-class CSmartIndentIfObj : public CWSHIfObj
-{
-	// 型定義
-	enum FuncId {
-		F_SI_COMMAND_FIRST = 0,					//↓コマンドは以下に追加する
-		F_SI_FUNCTION_FIRST = F_FUNCTION_FIRST,	//↓関数は以下に追加する
-		F_SI_GETCHAR							//押下したキーを取得する
-	};
-	typedef std::string string;
-	typedef std::wstring wstring;
-
-	// コンストラクタ
-public:
-	CSmartIndentIfObj( wchar_t ch )
-		: CWSHIfObj( L"Indent", false )
-		, m_wcChar( ch )
-	{
-	}
-
-	// デストラクタ
-public:
-	~CSmartIndentIfObj(){}
-
-	// 実装
-public:
-	//コマンド情報を取得する
-	MacroFuncInfoArray GetMacroCommandInfo() const{
-		static MacroFuncInfo macroFuncInfoArr[] = {
-			//	終端
-			{F_INVALID,	NULL, {VT_EMPTY, VT_EMPTY, VT_EMPTY, VT_EMPTY},	VT_EMPTY,	NULL}
-		};
-		return macroFuncInfoArr;
-	}
-	//関数情報を取得する
-	MacroFuncInfoArray GetMacroFuncInfo() const{
-		static MacroFuncInfo macroFuncInfoNotCommandArr[] = {
-			//ID									関数名							引数										戻り値の型	m_pszData
-			{EFunctionCode(F_SI_GETCHAR),			LTEXT("GetChar"),				{VT_EMPTY, VT_EMPTY, VT_EMPTY, VT_EMPTY},	VT_BSTR,	NULL }, //オプションファイルに値を書く
-			//	終端
-			{F_INVALID,	NULL, {VT_EMPTY, VT_EMPTY, VT_EMPTY, VT_EMPTY},	VT_EMPTY,	NULL}
-		};
-		return macroFuncInfoNotCommandArr;
-	}
-	//関数を処理する
-	bool HandleFunction(CEditView* View, EFunctionCode ID, const VARIANT *Arguments, const int ArgSize, VARIANT &Result)
-	{
-		switch ( LOWORD(ID) ) 
-		{
-		case F_SI_GETCHAR:						//押下したキーを取得する
-			{
-				wstring sValue;
-				sValue += m_wcChar;
-				SysString S(sValue.c_str(), sValue.size());
-				Wrap(&Result)->Receive(S);
-			}
-			return true;
-		}
-		return false;
-	}
-	//コマンドを処理する
-	void HandleCommand(CEditView* View, EFunctionCode ID, const WCHAR* Arguments[], const int ArgSize)
-	{
-		return;
-	}
-
-	// メンバ変数
-public:
-	wchar_t m_wcChar;
-};
 
 
 /* コマンドコードによる処理振り分け */
