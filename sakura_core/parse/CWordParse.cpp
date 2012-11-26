afce07d17417bc508b9a50d9c6c9784fcfc83bdc
novice123 <novice123@users.sourceforge.net>
2012-11-26 15:21:03 +0000

    Keep: typo修正
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@2489 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/parse/CWordParse.cpp
+++ sakura_core/parse/CWordParse.cpp
@@ -11,13 +11,13 @@
 	@retval false	失敗 現在位置のデータは「単語」とは言いきれない気がする。
 */
 bool CWordParse::WhereCurrentWord_2(
-	const wchar_t*	pLine,			//[in]  調べるメモリ全体の先頭アドレス
-	CLogicInt		nLineLen,		//[in]  調べるメモリ全体の有効長
-	CLogicInt		nIdx,			//[out] 調査開始地点:pLineからの相対的な位置
-	CLogicInt*		pnIdxFrom,		//[out] 単語が見つかった場合は、単語の先頭インデックスを返す。
-	CLogicInt*		pnIdxTo,		//[out] 単語が見つかった場合は、単語の終端の次のバイトの先頭インデックスを返す。
-	CNativeW*		pcmcmWord,		//[out] 単語が見つかった場合は、現在単語を切り出して指定されたCMemoryオブジェクトに格納する。情報が不要な場合はNULLを指定する。
-	CNativeW*		pcmcmWordLeft	//[out] 単語が見つかった場合は、現在単語の左に位置する単語を切り出して指定されたCMemoryオブジェクトに格納する。情報が不要な場合はNULLを指定する。
+	const wchar_t*	pLine,			//!< [in]  調べるメモリ全体の先頭アドレス
+	CLogicInt		nLineLen,		//!< [in]  調べるメモリ全体の有効長
+	CLogicInt		nIdx,			//!< [out] 調査開始地点:pLineからの相対的な位置
+	CLogicInt*		pnIdxFrom,		//!< [out] 単語が見つかった場合は、単語の先頭インデックスを返す。
+	CLogicInt*		pnIdxTo,		//!< [out] 単語が見つかった場合は、単語の終端の次のバイトの先頭インデックスを返す。
+	CNativeW*		pcmcmWord,		//!< [out] 単語が見つかった場合は、現在単語を切り出して指定されたCMemoryオブジェクトに格納する。情報が不要な場合はNULLを指定する。
+	CNativeW*		pcmcmWordLeft	//!< [out] 単語が見つかった場合は、現在単語の左に位置する単語を切り出して指定されたCMemoryオブジェクトに格納する。情報が不要な場合はNULLを指定する。
 )
 {
 	using namespace WCODE;
