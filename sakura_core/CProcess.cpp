7e6e3095eb1e63b8b8950ef5cab3515e91cb7d64
novice123 <novice123@users.sourceforge.net>
2013-04-06 04:41:06 +0000

    Imp: [U済] クラッシュダンプ機能 Patches#612
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk@2796 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/CProcess.cpp
+++ sakura_core/CProcess.cpp
@@ -8,6 +8,7 @@
 /*
 	Copyright (C) 2002, aroka 新規作成
 	Copyright (C) 2004, Moca
+	Copyright (C) 2009, ryoji
 
 	This source code is designed for sakura editor.
 	Please contact the copyright holder to use this code for other purpose.
@@ -31,6 +32,9 @@ CProcess::CProcess(
 )
 : m_hInstance( hInstance )
 , m_hWnd( 0 )
+#ifdef USE_CRASHDUMP
+, m_pfnMiniDumpWriteDump(NULL)
+#endif
 {
 }
 
@@ -67,10 +71,76 @@ bool CProcess::Run()
 {
 	if( InitializeProcess() )
 	{
-		MainLoop() ;
-		OnExitProcess();
+#ifdef USE_CRASHDUMP
+		HMODULE hDllDbgHelp = LoadLibraryExedir( _T("dbghelp.dll") );
+		m_pfnMiniDumpWriteDump = NULL;
+		if( hDllDbgHelp ){
+			*(FARPROC*)&m_pfnMiniDumpWriteDump = ::GetProcAddress( hDllDbgHelp, "MiniDumpWriteDump" );
+		}
+
+		__try {
+#endif
+			MainLoop() ;
+			OnExitProcess();
+#ifdef USE_CRASHDUMP
+		}
+		__except( WriteDump( GetExceptionInformation() ) ){
+		}
+
+		if( hDllDbgHelp ){
+			::FreeLibrary( hDllDbgHelp );
+			m_pfnMiniDumpWriteDump = NULL;
+		}
+#endif
 		return true;
 	}
 	return false;
 }
+
+#ifdef USE_CRASHDUMP
+/*!
+	@brief クラッシュダンプ
+	
+	@author ryoji
+	@date 2009.01.21
+*/
+int CProcess::WriteDump( PEXCEPTION_POINTERS pExceptPtrs )
+{
+	if( !m_pfnMiniDumpWriteDump )
+		return EXCEPTION_CONTINUE_SEARCH;
+
+	static TCHAR szFile[MAX_PATH];
+	GetInidirOrExedir( szFile, _T("sakura") _T(".dmp") );	// 出力先はiniと同じ（InitializeProcess()後に確定）
+
+	HANDLE hFile = ::CreateFile(
+		szFile,
+		GENERIC_WRITE,
+		0,
+		NULL,
+		CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL | FILE_FLAG_WRITE_THROUGH,
+		NULL);
+
+	if( hFile != INVALID_HANDLE_VALUE ){
+		MINIDUMP_EXCEPTION_INFORMATION eInfo;
+		eInfo.ThreadId = GetCurrentThreadId();
+		eInfo.ExceptionPointers = pExceptPtrs;
+		eInfo.ClientPointers = FALSE;
+
+		m_pfnMiniDumpWriteDump(
+			::GetCurrentProcess(),
+			::GetCurrentProcessId(),
+			hFile,
+			MiniDumpNormal,
+			pExceptPtrs ? &eInfo : NULL,
+			NULL,
+			NULL);
+
+		::CloseHandle(hFile);
+	}
+
+	return EXCEPTION_CONTINUE_SEARCH;
+}
+#endif
+
 /*[EOF]*/
--- sakura_core/CProcess.h
+++ sakura_core/CProcess.h
@@ -6,6 +6,7 @@
 */
 /*
 	Copyright (C) 2002, aroka 新規作成
+	Copyright (C) 2009, ryoji
 
 	This source code is designed for sakura editor.
 	Please contact the copyright holder to use this code for other purpose.
@@ -15,6 +16,7 @@
 #define _CPROCESS_H_
 
 #include "global.h"
+#include <DbgHelp.h> // MINIDUMP_TYPE
 #include "CShareData.h"
 
 /*-----------------------------------------------------------------------
@@ -34,8 +36,22 @@ protected:
 	virtual bool MainLoop() = 0;
 	virtual void OnExitProcess() = 0;
 
+#ifdef USE_CRASHDUMP
+	int				WriteDump( PEXCEPTION_POINTERS pExceptPtrs );
+#endif
 	HINSTANCE	m_hInstance;
 	HWND		m_hWnd;
+#ifdef USE_CRASHDUMP
+	BOOL (WINAPI *m_pfnMiniDumpWriteDump)(
+		HANDLE hProcess,
+		DWORD ProcessId,
+		HANDLE hFile,
+		MINIDUMP_TYPE DumpType,
+		PMINIDUMP_EXCEPTION_INFORMATION ExceptionParam,
+		PMINIDUMP_USER_STREAM_INFORMATION UserStreamParam,
+		PMINIDUMP_CALLBACK_INFORMATION CallbackParam
+		);
+#endif
 
 	//	唯一のCShareDateとする。（CProcessが責任を持ってnew/deleteする）
 	CShareData		m_cShareData;
