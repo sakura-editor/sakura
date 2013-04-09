32c6776b6f62ce19f3c87a9233ffa4550d98bd87
uchi5537 <uchi5537@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2013-04-09 10:04:18 +0000

    imp: Makefileを自動生成する Maikefaileのメンテナンスを手動でやるのは難しそうなので作成しました 384 Makefile修正 も一緒にコミットします Patchunicode#392 Patchunicode#384
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@2813 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura/preBuild.bat
+++ sakura/preBuild.bat
@@ -2,3 +2,4 @@
 
 HeaderMake -in=..\sakura_core\Funccode_x.hsrc -out=..\sakura_core\Funccode_define.h -mode=define
 HeaderMake -in=..\sakura_core\Funccode_x.hsrc -out=..\sakura_core\Funccode_enum.h -mode=enum -enum=EFunctionCode
+MakefileMake -file=..\sakura_core\Makefile -dir=..\sakura_core"
