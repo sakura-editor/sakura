02b49c30ada6e445cfbe5b6cd567f4d4dfb195ef
novice123 <novice123@users.sourceforge.net>
2013-04-23 17:09:08 +0000

    Keep: CCaretクラス整理 Patchunicode#443
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@2910 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/view/CCaret.cpp
+++ sakura_core/view/CCaret.cpp
@@ -1,3 +1,27 @@
+/*
+	Copyright (C) 2008, kobake
+
+	This software is provided 'as-is', without any express or implied
+	warranty. In no event will the authors be held liable for any damages
+	arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any purpose,
+	including commercial applications, and to alter it and redistribute it
+	freely, subject to the following restrictions:
+
+		1. The origin of this software must not be misrepresented;
+		   you must not claim that you wrote the original software.
+		   If you use this software in a product, an acknowledgment
+		   in the product documentation would be appreciated but is
+		   not required.
+
+		2. Altered source versions must be plainly marked as such,
+		   and must not be misrepresented as being the original software.
+
+		3. This notice may not be removed or altered from any source
+		   distribution.
+*/
+
 #include "StdAfx.h"
 #include <algorithm>
 #include "view/CCaret.h"
@@ -26,17 +50,17 @@ using namespace std;
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
 
 
-int CCaret::GetHankakuDx() const
+inline int CCaret::GetHankakuDx() const
 {
 	return m_pEditView->GetTextMetrics().GetHankakuDx();
 }
 
-int CCaret::GetHankakuHeight() const
+inline int CCaret::GetHankakuHeight() const
 {
 	return m_pEditView->GetTextMetrics().GetHankakuHeight();
 }
 
-int CCaret::GetHankakuDy() const
+inline int CCaret::GetHankakuDy() const
 {
 	return m_pEditView->GetTextMetrics().GetHankakuDy();
 }
