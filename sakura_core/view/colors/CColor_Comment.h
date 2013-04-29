2b647caeaf00a9dd431dd30d117aebec6291cb7b
novice123 <novice123@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2013-04-29 06:10:28 +0000

    Keep: CColor_Commentクラス整理(revert) Patchunicode#470
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@2945 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/view/colors/CColor_Comment.h
+++ sakura_core/view/colors/CColor_Comment.h
@@ -46,13 +46,13 @@ public:
 
 class CColor_BlockComment : public CColorStrategy{
 public:
-	CColor_BlockComment(EColorIndexType nType) : m_nType(nType), m_nCOMMENTEND(0) { }
-	virtual EColorIndexType GetStrategyColor() const{ return m_nType; }
+	CColor_BlockComment(int nType) : m_nType(nType), m_nCOMMENTEND(0) { }
+	virtual EColorIndexType GetStrategyColor() const{ return (EColorIndexType)(COLORIDX_BLOCK1 + m_nType); }
 	virtual void InitStrategyStatus(){ m_nCOMMENTEND = 0; }
 	virtual bool BeginColor(const CStringRef& cStr, int nPos);
 	virtual bool EndColor(const CStringRef& cStr, int nPos);
 private:
-	EColorIndexType m_nType;
+	int m_nType; //0 or 1 (ÉRÉÅÉìÉgéÌ)
 	int m_nCOMMENTEND;
 };
 
