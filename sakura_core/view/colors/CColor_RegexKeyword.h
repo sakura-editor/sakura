2106e3754c407d90722507bb05e76a1b1cebd88b
novice123 <novice123@users.sourceforge.net>
2013-05-10 05:01:32 +0000

    Keep: 色定数->色番号の変換処理 Patchunicode#458
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@2989 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/view/colors/CColor_RegexKeyword.h
+++ sakura_core/view/colors/CColor_RegexKeyword.h
@@ -30,9 +30,9 @@
 
 class CColor_RegexKeyword : public CColorStrategy{
 public:
-	CColor_RegexKeyword() : m_nCOMMENTEND(0), m_nCOMMENTMODE(EColorIndexType(COLORIDX_REGEX_FIRST + COLORIDX_DEFAULT)) { }
+	CColor_RegexKeyword() : m_nCOMMENTEND(0), m_nCOMMENTMODE(ToColorIndexType_RegularExpression(0)) { }
 	virtual EColorIndexType GetStrategyColor() const{ return m_nCOMMENTMODE; }
-	virtual void InitStrategyStatus(){ m_nCOMMENTEND = 0; m_nCOMMENTMODE = EColorIndexType(COLORIDX_REGEX_FIRST + COLORIDX_DEFAULT); }
+	virtual void InitStrategyStatus(){ m_nCOMMENTEND = 0; m_nCOMMENTMODE = ToColorIndexType_RegularExpression(0); }
 	virtual bool BeginColor(const CStringRef& cStr, int nPos);
 	virtual bool EndColor(const CStringRef& cStr, int nPos);
 private:
