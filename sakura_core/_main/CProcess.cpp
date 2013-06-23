e64be5898852dfd8ea85326eea5cb41110aba9b0
syat <syat@users.sourceforge.net>
2013-06-23 03:14:56 +0000

    Keep: ミニダンプのコメント追加
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@3174 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/_main/CProcess.cpp
+++ sakura_core/_main/CProcess.cpp
@@ -108,7 +108,9 @@ int CProcess::WriteDump( PEXCEPTION_POINTERS pExceptPtrs )
 		return EXCEPTION_CONTINUE_SEARCH;
 
 	static TCHAR szFile[MAX_PATH];
-	GetInidirOrExedir( szFile, _APP_NAME_(_T) _T(".dmp") );	// 出力先はiniと同じ（InitializeProcess()後に確定）
+	// 出力先はiniと同じ（InitializeProcess()後に確定）
+	// Vista以降では C:\Users\(ユーザ名)\AppData\Local\CrashDumps に出力
+	GetInidirOrExedir( szFile, _APP_NAME_(_T) _T(".dmp") );
 
 	HANDLE hFile = ::CreateFile(
 		szFile,
