55c36eab131e544e648f6a43795a5587dc6775f2
syat <syat@users.sourceforge.net>
2013-08-04 04:19:39 +0000

    Lang: 2.1.0.0のリソース変更を取り込み
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/branches/trunk2_i18n@3297 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

---
+++ sakura_lang_en_US/sakura_lang.h
@@ -0,0 +1,50 @@
+/*
+	Copyright (C) 2013, syat
+
+	This software is provided 'as-is', without any express or implied
+	warranty. In no event will the authors be held liable for any damages
+	arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any purpose,
+	including commercial applications, and to alter it and redistribute it
+	freely, subject to the following restrictions:
+
+		1. The origin of this software must not be misrepresented;
+		   you must not claim that you wrote the original software.
+		   If you use this software in a product, an acknowledgment
+		   in the product documentation would be appreciated but is
+		   not required.
+
+		2. Altered source versions must be plainly marked as such,
+		   and must not be misrepresented as being the original software.
+
+		3. This notice may not be removed or altered from any source
+		   distribution.
+*/
+#ifndef SAKURA_SAKURA_LANG_C627B14F_9909_441B_B510_9BBB2D3F078B_H_
+#define SAKURA_SAKURA_LANG_C627B14F_9909_441B_B510_9BBB2D3F078B_H_
+
+//#define USE_LOCAL_SAKURA_RC_H
+//#define USE_LOCAL_FUNCCODE_DEFINE_H
+//#define USE_LOCAL_STRING_DEFINE_H
+
+#ifdef USE_LOCAL_SAKURA_RC_H
+	#include "sakura_rc.h"
+#else
+	#include "../sakura_core/sakura_rc.h"
+#endif
+
+#ifdef USE_LOCAL_FUNCCODE_DEFINE_H
+	#include "Funccode_define.h"
+#else
+	#include "../sakura_core/Funccode_define.h"
+#endif
+
+#ifdef USE_LOCAL_STRING_DEFINE_H
+	#include "String_define.h"
+#else
+	#include "../sakura_core/String_define.h"
+#endif
+
+#endif /* SAKURA_SAKURA_LANG_C627B14F_9909_441B_B510_9BBB2D3F078B_H_ */
+/*[EOF]*/
