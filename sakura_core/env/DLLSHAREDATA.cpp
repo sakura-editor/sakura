e91d6543822f3dd10e7adb06108bb66bf30a3860
novice123 <novice123@users.sourceforge.net>
2013-09-23 15:02:44 +0000

    Keep: GetDllShareData()整理 Patchunicode#673
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@3362 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/env/DLLSHAREDATA.cpp
+++ sakura_core/env/DLLSHAREDATA.cpp
@@ -34,22 +34,6 @@
 //GetDllShareData用グローバル変数
 DLLSHAREDATA* g_theDLLSHAREDATA = NULL;
 
-//DLLSHAREDATAへの簡易アクセサ
-DLLSHAREDATA& GetDllShareData()
-{
-	assert(g_theDLLSHAREDATA);
-	return *g_theDLLSHAREDATA;
-}
-
-//DLLSHAREDATAを確保したら、まずこれを呼ぶ。
-void DLLSHAREDATA::OnInit()
-{
-	g_theDLLSHAREDATA = this;
-}
-
-
-
-
 static CMutex g_cKeywordMutex( FALSE, GSTR_MUTEX_SAKURA_KEYWORD );
 
 CShareDataLockCounter::CShareDataLockCounter(){
