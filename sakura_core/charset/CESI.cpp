2f07209c77c877c69a7ab18b16a7484ee82a97a8
moca_skr <moca_skr@users.sourceforge.net>
2014-03-28 09:12:54 +0000

    Fix: %sの修正 [patchunicode:#780]
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@3663 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/charset/CESI.cpp
+++ sakura_core/charset/CESI.cpp
@@ -876,7 +876,7 @@ void CESI::GetDebugInfo( const char* pS, const int nLen, CNativeT* pcmtxtOut )
 			cesi.m_apMbcInfo[i]->eCodeID = CODE_SJIS;
 		}
 		cesi.GetEvaluation( cesi.m_apMbcInfo[i]->eCodeID, &v1, &v2 );
-		auto_sprintf( szWork, LS(STR_ESI_OTHER_B_AND_P),	// "\t\t%d.%s\t固有バイト数 %d\tポイント数 %d\r\n"
+		auto_sprintf( szWork, LS(STR_ESI_OTHER_B_AND_P),	// "\t\t%d.%ts\t固有バイト数 %d\tポイント数 %d\r\n"
 			i+1, CCodeTypeName(cesi.m_apMbcInfo[i]->eCodeID).Normal(), v1, v2 );
 		pcmtxtOut->AppendString( szWork );
 	}
