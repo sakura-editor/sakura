3c870df4d14ba6222d42f963e5199a6ddd9ad990
novice123 <novice123@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2014-07-02 14:24:05 +0000

    Fix: 検索/置換/grepダイアログの検索履歴の初回ドロップダウン幅不正 Patchunicode#852
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@3757 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/dlg/CDlgFind.cpp
+++ sakura_core/dlg/CDlgFind.cpp
@@ -52,33 +52,23 @@ CDlgFind::CDlgFind()
 
 
 /*!
-	標準以外のメッセージを捕捉する
+	コンボボックスのドロップダウンメッセージを捕捉する
 
 	@date 2013.03.24 novice 新規作成
 */
-INT_PTR CDlgFind::DispatchEvent( HWND hWnd, UINT wMsg, WPARAM wParam, LPARAM lParam )
+BOOL CDlgFind::OnCbnDropDown( HWND hwndCtl, int wID )
 {
-	INT_PTR result;
-	result = CDialog::DispatchEvent( hWnd, wMsg, wParam, lParam );
-	switch( wMsg ){
-	case WM_COMMAND:
-		WORD wID = LOWORD(wParam);
-		switch( wID ){
-		case IDC_COMBO_TEXT:
-			if ( HIWORD(wParam) == CBN_DROPDOWN ) {
-				HWND hwndCombo = ::GetDlgItem( GetHwnd(), IDC_COMBO_TEXT );
-				if ( ::SendMessage(hwndCombo, CB_GETCOUNT, 0L, 0L) == 0) {
-					int nSize = m_pShareData->m_sSearchKeywords.m_aSearchKeys.size();
-					for (int i = 0; i < nSize; ++i) {
-						Combo_AddString( hwndCombo, m_pShareData->m_sSearchKeywords.m_aSearchKeys[i] );
-					}
-				}
+	switch( wID ){
+	case IDC_COMBO_TEXT:
+		if ( ::SendMessage(hwndCtl, CB_GETCOUNT, 0L, 0L) == 0) {
+			int nSize = m_pShareData->m_sSearchKeywords.m_aSearchKeys.size();
+			for (int i = 0; i < nSize; ++i) {
+				Combo_AddString( hwndCtl, m_pShareData->m_sSearchKeywords.m_aSearchKeys[i] );
 			}
-			break;
 		}
 		break;
 	}
-	return result;
+	return CDialog::OnCbnDropDown( hwndCtl, wID );
 }
 
 /* モードレスダイアログの表示 */
