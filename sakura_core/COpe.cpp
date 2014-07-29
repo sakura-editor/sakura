0d14dfc5f4f092ace0eb07047c2c55b5af728be4
novice123 <novice123@users.sourceforge.net>
2014-07-29 13:59:29 +0000

    Keep: COpeクラス整理 Patchunicode#870
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@3780 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/COpe.cpp
+++ sakura_core/COpe.cpp
@@ -41,8 +41,6 @@ void COpe::DUMP( void )
 	DEBUG_TRACE( _T("\t\tm_nOpe                  = [%d]\n"), m_nOpe               );
 	DEBUG_TRACE( _T("\t\tm_ptCaretPos_PHY_Before = [%d,%d]\n"), m_ptCaretPos_PHY_Before.x, m_ptCaretPos_PHY_Before.y   );
 	DEBUG_TRACE( _T("\t\tm_ptCaretPos_PHY_After  = [%d,%d]\n"), m_ptCaretPos_PHY_After.x, m_ptCaretPos_PHY_After.y   );
-//	DEBUG_TRACE( _T("\t\tm_nDataLen              = [%d]\n"),  m_nDataLen           );
-//	DEBUG_TRACE( _T("\t\tm_pcmemData             = [%ls]\n"), m_pcmemData.GetStringPtr() );
 	return;
 }
 
@@ -51,10 +49,10 @@ void CDeleteOpe::DUMP( void )
 {
 	COpe::DUMP();
 	DEBUG_TRACE( _T("\t\tm_ptCaretPos_PHY_To     = [%d,%d]\n"), m_ptCaretPos_PHY_To.x, m_ptCaretPos_PHY_To.y );
-	DEBUG_TRACE( _T("\t\tm_pcmemData.size         = [%d]\n"), m_pcmemData.size() );
-	for( size_t i = 0; i < m_pcmemData.size(); i++ ){
-		DEBUG_TRACE( _T("\t\tm_pcmemData[%d].nSeq         = [%d]\n"), m_pcmemData[i].nSeq );
-		DEBUG_TRACE( _T("\t\tm_pcmemData[%d].cmemLine     = [%ls]\n"), m_pcmemData[i].cmemLine.GetStringPtr() );		
+	DEBUG_TRACE( _T("\t\tm_cOpeLineData.size         = [%d]\n"), m_cOpeLineData.size() );
+	for( size_t i = 0; i < m_cOpeLineData.size(); i++ ){
+		DEBUG_TRACE( _T("\t\tm_cOpeLineData[%d].nSeq         = [%d]\n"), m_cOpeLineData[i].nSeq );
+		DEBUG_TRACE( _T("\t\tm_cOpeLineData[%d].cmemLine     = [%ls]\n"), m_cOpeLineData[i].cmemLine.GetStringPtr() );		
 	}
 	return;
 }
@@ -63,10 +61,10 @@ void CDeleteOpe::DUMP( void )
 void CInsertOpe::DUMP( void )
 {
 	COpe::DUMP();
-	DEBUG_TRACE( _T("\t\tm_pcmemData.size         = [%d]\n"), m_pcmemData.size() );
-	for( size_t i = 0; i < m_pcmemData.size(); i++ ){
-		DEBUG_TRACE( _T("\t\tm_pcmemData[%d].nSeq         = [%d]\n"), m_pcmemData[i].nSeq );
-		DEBUG_TRACE( _T("\t\tm_pcmemData[%d].cmemLine     = [%ls]\n"), m_pcmemData[i].cmemLine.GetStringPtr() );		
+	DEBUG_TRACE( _T("\t\tm_cOpeLineData.size         = [%d]\n"), m_cOpeLineData.size() );
+	for( size_t i = 0; i < m_cOpeLineData.size(); i++ ){
+		DEBUG_TRACE( _T("\t\tm_cOpeLineData[%d].nSeq         = [%d]\n"), m_cOpeLineData[i].nSeq );
+		DEBUG_TRACE( _T("\t\tm_cOpeLineData[%d].cmemLine     = [%ls]\n"), m_cOpeLineData[i].cmemLine.GetStringPtr() );		
 	}
 	return;
 }
