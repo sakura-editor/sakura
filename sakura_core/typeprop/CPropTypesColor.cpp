2ffdd0123a9642db3a9815409bf5e4cf0a965560
syat <syat@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2014-10-07 13:03:21 +0000

    Fix: タイプ別設定の強調キーワードダイアログを多言語化 upatchid:891
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@3823 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/typeprop/CPropTypesColor.cpp
+++ sakura_core/typeprop/CPropTypesColor.cpp
@@ -448,7 +448,7 @@ INT_PTR CPropTypesColor::DispatchEvent(
 					pCommon->InitData();
 					pCommon->m_nKeywordSet1 = m_nSet[0];
 					INT_PTR res = ::DialogBoxParam(
-						::GetModuleHandle(NULL),
+						CSelectLang::getLangRsrcInstance(),
 						MAKEINTRESOURCE( IDD_PROP_KEYWORD ),
 						hwndDlg,
 						CPropKeyword::DlgProc_dialog,
