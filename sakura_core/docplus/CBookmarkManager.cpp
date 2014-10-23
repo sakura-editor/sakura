837f3796e1993569513a340aa8f7fb9b62f54aa1
moca_skr <moca_skr@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2014-10-23 18:03:26 +0000

    Imp: ブックマークの記憶方法の変更
    [patchunicode:#803]
    差分32進数で記録して、従来より多く記録できるようにします。
    
    
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@3844 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/docplus/CBookmarkManager.cpp
+++ sakura_core/docplus/CBookmarkManager.cpp
@@ -68,6 +68,7 @@ bool CBookmarkManager::SearchBookMark(
 //! 物理行番号のリストからまとめて行マーク
 /*
 	@date 2002.01.16 hor
+	@date 2014.04.24 Moca ver2 差分32進数方式に変更
 */
 void CBookmarkManager::SetBookMarks( wchar_t* pMarkLines )
 {
@@ -75,11 +76,58 @@ void CBookmarkManager::SetBookMarks( wchar_t* pMarkLines )
 	wchar_t *p;
 	wchar_t delim[] = L", ";
 	p = pMarkLines;
-	while(wcstok(p, delim) != NULL) {
-		while(wcschr(delim, *p) != NULL)p++;
-		pCDocLine=m_pcDocLineMgr->GetLine( CLogicInt(_wtol(p)) );
-		if(pCDocLine)CBookmarkSetter(pCDocLine).SetBookmark(true);
-		p += wcslen(p) + 1;
+	if( p[0] == L':' ){
+		if( p[1] == L'0' ){
+			// ver2 形式 [0-9a-v] 0-31(終端バージョン) [w-zA-Z\+\-] 0-31
+			// 2番目以降は、数値+1+ひとつ前の値
+			// :00123x0 => 0,1,2,3,x0 => 0,(1+1),(2+2+1),(3+5+1),(32+9+1) => 0,2,5,9,42 => 1,3,6,10,43行目
+			// :0a => a, => 10, 11
+			p += 2;
+			int nLineNum = 0;
+			int nLineTemp = 0;
+			while( *p != L'\0' ){
+				bool bSeparete = false;
+				if( L'0' <= *p && *p <= L'9' ){
+					nLineTemp += (*p - L'0');
+					bSeparete = true;
+				}else if( L'a' <= *p && *p <= L'v' ){
+					nLineTemp += (*p - L'a') + 10;
+					bSeparete = true;
+				}else if( L'w' <= *p && *p <= L'z' ){
+					nLineTemp += (*p - L'w');
+				}else if( L'A' <= *p && *p <= L'Z' ){
+					nLineTemp += (*p - L'A') + 4;
+				}else if( *p == L'+' ){
+					nLineTemp += 30;
+				}else if( *p == L'-' ){
+					nLineTemp += 31;
+				}else{
+					break;
+				}
+				if( bSeparete ){
+					nLineNum += nLineTemp;
+					pCDocLine = m_pcDocLineMgr->GetLine( CLogicInt(nLineNum) );
+					if( pCDocLine ){
+						CBookmarkSetter(pCDocLine).SetBookmark(true);
+					}
+					nLineNum++;
+					nLineTemp = 0;
+				}else{
+					nLineTemp *= 32;
+				}
+				p++;
+			}
+		}else{
+			// 不明なバージョン
+		}
+	}else{
+		// 旧形式 行番号,区切り
+		while(wcstok(p, delim) != NULL) {
+			while(wcschr(delim, *p) != NULL)p++;
+			pCDocLine=m_pcDocLineMgr->GetLine( CLogicInt(_wtol(p)) );
+			if(pCDocLine)CBookmarkSetter(pCDocLine).SetBookmark(true);
+			p += wcslen(p) + 1;
+		}
 	}
 }
 
@@ -87,20 +135,62 @@ void CBookmarkManager::SetBookMarks( wchar_t* pMarkLines )
 //! 行マークされてる物理行番号のリストを作る
 /*
 	@date 2002.01.16 hor
+	@date 2014.04.24 Moca ver2 差分32進数方式に変更
 */
 LPCWSTR CBookmarkManager::GetBookMarks()
 {
 	const CDocLine*	pCDocLine;
 	static wchar_t szText[MAX_MARKLINES_LEN + 1];	//2002.01.17 // Feb. 17, 2003 genta staticに
 	wchar_t szBuff[10];
+	wchar_t szBuff2[10];
 	CLogicInt	nLinePos=CLogicInt(0);
+	CLogicInt	nLinePosOld=CLogicInt(-1);
+	int			nTextLen = 2;
 	pCDocLine = m_pcDocLineMgr->GetLine( nLinePos );
-	szText[0] = L'\0';
+	wcscpy( szText, L":0" );
 	while( pCDocLine ){
 		if(CBookmarkGetter(pCDocLine).IsBookmarked()){
-			auto_sprintf( szBuff, L"%d,",nLinePos );
-			if(wcslen(szBuff)+wcslen(szText)>MAX_MARKLINES_LEN)break;	//2002.01.17
-			wcscat( szText, szBuff);
+			CLogicInt nDiff = nLinePos - nLinePosOld - CLogicInt(1);
+			nLinePosOld = nLinePos;
+			if( nDiff == CLogicInt(0) ){
+				szBuff2[0] = L'0';
+				szBuff2[1] = L'\0';
+			}else{
+				int nColumn = 0;
+				while( nDiff ){
+					CLogicInt nKeta = nDiff % 32;
+					wchar_t c;
+					if( nColumn == 0 ){
+						if( nKeta <= 9 ){
+							c = (wchar_t)((Int)nKeta + L'0');
+						}else{
+							c = (wchar_t)((Int)nKeta - 10 + L'a');
+						}
+					}else{
+						if( nKeta <= 3 ){
+							c = (wchar_t)((Int)nKeta + L'w');
+						}else if( nKeta <= 29 ){
+							c = (wchar_t)((Int)nKeta - 4 + L'A');
+						}else if( nKeta == 30 ){
+							c = L'+';
+						}else{ // 31
+							c = L'-';
+						}
+					}
+					szBuff[nColumn] = c;
+					nColumn++;
+					nDiff /= 32;
+				}
+				int i = 0;
+				for(; i < nColumn; i++ ){
+					szBuff2[i] = szBuff[nColumn-1-i];
+				}
+				szBuff2[nColumn] = L'\0';
+			}
+			int nBuff2Len = wcslen(szBuff2);
+			if( nBuff2Len + nTextLen > MAX_MARKLINES_LEN ) break;	//2002.01.17
+			wcscpy( szText + nTextLen, szBuff2 );
+			nTextLen += nBuff2Len;
 		}
 		nLinePos++;
 		pCDocLine = pCDocLine->GetNextLine();
