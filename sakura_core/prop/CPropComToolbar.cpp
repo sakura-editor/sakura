b57ce4db27f1c99da6e668d6ebac85c70dd4396c
moca_skr <moca_skr@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2014-12-10 09:50:35 +0000

    Fix: ツールバー折返が英語にならない
    [patchunicode:#929]
    共通設定-ツールバーで、ツールバー折り返し(ツールバー折返)を追加すると、
    英語リソースに切り替えても、英語にならず日本語で表示される。
    
    
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@3895 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/prop/CPropComToolbar.cpp
+++ sakura_core/prop/CPropComToolbar.cpp
@@ -584,10 +584,9 @@ void CPropToolbar::DrawToolBarItemList( DRAWITEMSTRUCT* pDis )
 				auto_strncpy( szLabel, LSW(STR_PROPCOMTOOL_ITEM1), _countof(szLabel) - 1 );	// nLength 未使用 2003/01/09 Moca
 				szLabel[_countof(szLabel) - 1] = L'\0';
 			}else if( tbb.idCommand == F_MENU_NOT_USED_FIRST ){
-				if( ::LoadStringW_AnyBuild( G_AppInstance(), tbb.idCommand, szLabel, _countof( szLabel ) ) <= 0 ){
-					auto_strncpy( szLabel, LSW(STR_PROPCOMTOOL_ITEM2), _countof(szLabel) - 1 );
-					szLabel[_countof(szLabel) - 1] = L'\0';
-				}
+				// ツールバー折返
+				auto_strncpy( szLabel, LSW(STR_PROPCOMTOOL_ITEM2), _countof(szLabel) - 1 );
+				szLabel[_countof(szLabel) - 1] = L'\0';
 			}else{
 				auto_strncpy( szLabel, LSW(STR_PROPCOMTOOL_ITEM3), _countof(szLabel) - 1 );
 				szLabel[_countof(szLabel) - 1] = L'\0';
