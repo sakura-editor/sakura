6974d56cd3783e57b6f95379c26afe92099d9eb7
moca_skr <moca_skr@users.sourceforge.net>
2015-02-13 12:29:15 +0000

    Fix: 矩形選択ロックのマクロの記録が正しくない(r3949-)
    [patchunicode:#970]
    PageDown_BOX( 5, 1 );
    のようなマクロを実行した時のマクロの記録で、移動行数がデフォルト値に書き換わる不具合を修正します。
    この場合、5がそのときの1ページ分の行数に書き換わります。
    (Half)?Page(Up|Down)_BOXに影響します。
    
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@3976 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/macro/CMacro.cpp
+++ sakura_core/macro/CMacro.cpp
@@ -119,11 +119,19 @@ void CMacro::AddLParam( const LPARAM* lParams, const CEditView* pcEditView )
 				switch( m_nFuncID ){
 				case F_HalfPageUp_BOX:
 				case F_HalfPageDown_BOX:
-					AddIntParam( (Int)pcEditView->GetTextArea().m_nViewRowNum / 2 );
+					if( lParam == 0 ){
+						AddIntParam( (Int)pcEditView->GetTextArea().m_nViewRowNum / 2 );
+					}else{
+						AddIntParam( lParam );
+					}
 					break;
 				case F_1PageUp_BOX:
 				case F_1PageDown_BOX:
-					AddIntParam( (Int)pcEditView->GetTextArea().m_nViewRowNum - 1 );
+					if( lParam == 0 ){
+						AddIntParam( (Int)pcEditView->GetTextArea().m_nViewRowNum - 1 );
+					}else{
+						AddIntParam( lParam );
+					}
 					break;
 				default:
 					AddIntParam( lParam );
