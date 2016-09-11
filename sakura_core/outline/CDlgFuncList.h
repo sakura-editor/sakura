e20fe9df9e2772eb8d806eb0522d00ebd41c7448
novice123 <novice123@f7ce1907-e4c7-47ca-9f76-12c87ed2c91c>
2016-09-11 14:45:32 +0000

    Fix: アウトライン解析C++/Javaのソート順がおかしいバグの修正
    [patchunicode:#1031] Contributed by Moca
    
    
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@4133 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/outline/CDlgFuncList.h
+++ sakura_core/outline/CDlgFuncList.h
@@ -192,6 +192,8 @@ private:
 	// 0 デフォルト(ノードに関連づけれられた値順)
 	// 1 アルファベット順
 	int m_nSortType;
+	bool m_bDummyLParamMode;				//!< m_vecDummylParams有効/無効
+	std::vector<int> m_vecDummylParams;		//!< ダミー要素の識別値
 
 	// 選択中の関数情報
 	CFuncInfo* m_cFuncInfo;
