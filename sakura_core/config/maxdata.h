42f2a093a4350ecf5002174e01b7741574d5ecfd
novice123 <novice123@users.sourceforge.net>
2016-11-08 16:24:12 +0000

    Keep: maxdata定義追加
    [patchunicode:#1085]
    
    
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@4151 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/config/maxdata.h
+++ sakura_core/config/maxdata.h
@@ -31,8 +31,8 @@ enum maxdata{
 	MAX_EDITWINDOWS				= 256,	//!< 編集ウィンドウ数
 	MAX_SEARCHKEY				=  30,	//!< 検索キー
 	MAX_REPLACEKEY				=  30,	//!< 置換キー
-	MAX_GREPFILE				=  30,	//!< Gprepファイル
-	MAX_GREPFOLDER				=  30,	//!< Gprepフォルダ
+	MAX_GREPFILE				=  30,	//!< Grepファイル
+	MAX_GREPFOLDER				=  30,	//!< Grepフォルダ
 	MAX_TYPES					=  60,	//!< タイプ別設定
 	MAX_TYPES_NAME				=  64,	//!< タイプ属性：名称 バッファサイズ
 	MAX_TYPES_EXTS				=  64,	//!< タイプ属性：拡張子リスト バッファサイズ
