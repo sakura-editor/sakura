6d2b6cc790ed4b4e7da0721bbe0bc10d75b6eb7a
novice123 <novice123@users.sourceforge.net>
2017-01-27 14:05:09 +0000

    Fix: DragFinishがたりない
    [patchunicode:#1042] Contributed by Moca
    
    
    git-svn-id: https://svn.code.sf.net/p/sakura-editor/code/sakura/trunk2@4165 f7ce1907-e4c7-47ca-9f76-12c87ed2c91c

--- sakura_core/dlg/CDlgGrep.cpp
+++ sakura_core/dlg/CDlgGrep.cpp
@@ -215,15 +215,16 @@ BOOL CDlgGrep::OnInitDialog( HWND hwndDlg, WPARAM wParam, LPARAM lParam )
 */
 LRESULT CALLBACK OnFolderProc(HWND hwnd,UINT msg,WPARAM wparam,LPARAM lparam)
 {
-	if(msg == WM_DROPFILES) 
-	do {
+	if(msg == WM_DROPFILES){
 		//	From Here 2007.09.02 genta 
 		SFilePath sPath;
 		if( DragQueryFile((HDROP)wparam, 0, NULL, 0 ) > _countof2(sPath) - 1 ){
 			// skip if the length of the path exceeds buffer capacity
-			break;
+			::DragFinish((HDROP)wparam);
+			return 0;
 		}
 		DragQueryFile((HDROP)wparam, 0, sPath, _countof2(sPath) - 1);
+		::DragFinish((HDROP)wparam);
 
 		//ファイルパスの解決
 		CSakuraEnvironment::ResolvePath(sPath);
@@ -237,8 +238,8 @@ LRESULT CALLBACK OnFolderProc(HWND hwnd,UINT msg,WPARAM wparam,LPARAM lparam)
 		}
 
 		SetGrepFolder(hwnd, sPath);
+		return 0;
 	}
-	while(0);	//	1回しか通らない. breakでここまで飛ぶ
 
 	return  CallWindowProc(g_pOnFolderProc,hwnd,msg,wparam,lparam);
 }
