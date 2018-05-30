d353125e943a7e996547846a241ffa3604478419
kobake <pg.kobake@gmail.com>
2018-05-30 16:45:26 +0900

    VS2017 では postBuild.bat の処理内容は不要なので削除 (AppVeyor 文字化けメッセージ原因にもなるので削除が好ましい)
    これを残しておくと AppVeyor で以下のような文字化けメッセージが出て気持ち悪い。
    https://ci.appveyor.com/project/kobake/sakura-qiaye/build/1.0.4
    --------------------------------------------------------------------------------
      sakura_vc2017.vcxproj -> C:\projects\sakura-qiaye\sakura\Release_Unicode\sakura.exe
      '�̂�����邽�߁A' is not recognized as an internal or external command,
      operable program or batch file.
      'g�p�B' is not recognized as an internal or external command,
      operable program or batch file.
      'Ȃ̂ŃG���[�������Ă����I����' is not recognized as an internal or external command,
      operable program or batch file.
    --------------------------------------------------------------------------------

--- sakura/postBuild.bat
+++ sakura/postBuild.bat
@@ -1,13 +1,1 @@
 @echo off
-
-REM 指定ファイルのタイムスタンプを現在時刻に更新する
-REM VC++2005/2008 で不必要にビルドの問い合わせが出るのを回避するため、
-REM ビルド後のイベントで .manifest の時刻を更新するのに使用。
-REM ※問い合わせを減らしたいだけなのでエラーがあっても正常終了の 0 で終了
-
-if %1.==. goto end
-if not exist %1 goto end
-echo touch %1
-copy /b %1+,,%1 > nul
-:end
-exit 0
