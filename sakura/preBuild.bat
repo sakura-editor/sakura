6011cc6b024a932167e0c4a455efd0b763f9f312
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-05-30 22:48:22 +0900

    #18: リソースファイルには短い形式のハッシュを使う

--- sakura/preBuild.bat
+++ sakura/preBuild.bat
@@ -9,11 +9,20 @@ set GITREV_H=..\sakura_core\gitrev.h
 for /f "usebackq" %%s in (`git show -s --format^=%%H`) do (
     set COMMITID=%%s
 )
+for /f "usebackq" %%s in (`git show -s --format^=%%h`) do (
+    set SHORT_COMMITID=%%s
+)
 
+type nul                                  > %GITREV_H%
 if "%COMMITID%" == "" (
-	type nul                                  > %GITREV_H%
+	type nul                                  >> %GITREV_H%
 ) else (
-	echo #define GIT_COMMIT_HASH "%COMMITID%" > %GITREV_H%
+	echo #define GIT_COMMIT_HASH "%COMMITID%" >> %GITREV_H%
+)
+if "%SHORT_COMMITID%" == "" (
+	type nul                                              >> %GITREV_H%
+) else (
+	echo #define GIT_SHORT_COMMIT_HASH "%SHORT_COMMITID%" >> %GITREV_H%
 )
 
 SubWCRev.exe "..\\" "..\sakura_core\svnrev_template.h" "..\sakura_core\svnrev.h"
