aa753e96749341a3ea6b1738c1d7268e7f600764
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-06-03 15:02:52 +0900

    Debug/Release 構成をビルドする

--- build-by-cmake.bat
+++ build-by-cmake.bat
@@ -11,10 +11,31 @@ for /l %%n in (1,1,10) do (
 mkdir "%BUILDDIR%" || exit /b 1
 cd    "%BUILDDIR%"
 
+set RETCODE=0
 if %ERRORLEVEL% equ 0 (
 	cmake ..
+) else (
+	set RETCODE=1
 )
+
 if %ERRORLEVEL% equ 0 (
-	cmake --build .
+	cmake --build . --config Debug
+) else (
+	set RETCODE=1
+)
+
+if %ERRORLEVEL% equ 0 (
+	cmake --build . --config Release
+) else (
+	set RETCODE=1
 )
 cd ..
+
+if %ERRORLEVEL% equ 0 (
+	echo success
+	set RETCODE=0
+) else (
+	set RETCODE=1
+)
+
+exit /b %RETCODE%
