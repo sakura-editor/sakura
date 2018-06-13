d4b57b22b513043240a06adf22b34eebf167dfca
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-06-14 07:44:21 +0900

    #109: 内容を表示する

--- zipArtifacts.bat
+++ zipArtifacts.bat
@@ -5,3 +5,4 @@ set OUTFILE=sakura-%platform%-%configuration%.zip
 7z a %OUTFILE% %platform%\%configuration%\*.exe
 7z a %OUTFILE% %platform%\%configuration%\*.dll
 7z a %OUTFILE% %platform%\%configuration%\*.pdb
+7z l %OUTFILE%
