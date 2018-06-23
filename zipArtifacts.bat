db1aeaab34c2b37060df819a721761664d6ae865
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-06-23 23:25:25 +0900

    #164: msbuild のログをとって artifacts の中にいれる。

--- zipArtifacts.bat
+++ zipArtifacts.bat
@@ -1,6 +1,7 @@
 set platform=%1
 set configuration=%2
 set WORKDIR=sakura-%platform%-%configuration%
+set WORKDIR_LOG=%WORKDIR%\Log
 set WORKDIR_EXE=%WORKDIR%\EXE
 set WORKDIR_INST=%WORKDIR%\Installer
 set OUTFILE=sakura-%platform%-%configuration%.zip
@@ -14,6 +15,7 @@ if exist "%WORKDIR%" (
 )
 
 mkdir %WORKDIR%
+mkdir %WORKDIR_LOG%
 mkdir %WORKDIR_EXE%
 mkdir %WORKDIR_INST%
 copy %platform%\%configuration%\*.exe %WORKDIR_EXE%\
@@ -26,6 +28,7 @@ copy help\sakura\sakura.chm  %WORKDIR_EXE%\
 
 copy installer\warning.txt   %WORKDIR%\
 copy installer\Output\*.exe  %WORKDIR_INST%\
+copy msbuild-%platform%-%configuration%.log %WORKDIR_LOG%\
 
 7z a %OUTFILE%  -r %WORKDIR%
 7z l %OUTFILE%
