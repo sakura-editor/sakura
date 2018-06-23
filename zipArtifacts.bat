d6c42fb0476a612167c4f38cb2aaccb1d082fa83
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-06-23 19:32:52 +0900

    #166: 警告のドキュメントが埋もれてしまうのでサブフォルダにわける

--- zipArtifacts.bat
+++ zipArtifacts.bat
@@ -1,6 +1,8 @@
 set platform=%1
 set configuration=%2
 set WORKDIR=sakura-%platform%-%configuration%
+set WORKDIR_EXE=%WORKDIR%\EXE
+set WORKDIR_INST=%WORKDIR%\Installer
 set OUTFILE=sakura-%platform%-%configuration%.zip
 
 @rem cleanup for local testing
@@ -12,16 +14,18 @@ if exist "%WORKDIR%" (
 )
 
 mkdir %WORKDIR%
-copy %platform%\%configuration%\*.exe %WORKDIR%\
-copy %platform%\%configuration%\*.dll %WORKDIR%\
-copy %platform%\%configuration%\*.pdb %WORKDIR%\
+mkdir %WORKDIR_EXE%
+mkdir %WORKDIR_INST%
+copy %platform%\%configuration%\*.exe %WORKDIR_EXE%\
+copy %platform%\%configuration%\*.dll %WORKDIR_EXE%\
+copy %platform%\%configuration%\*.pdb %WORKDIR_EXE%\
 
-copy help\macro\macro.chm    %WORKDIR%\
-copy help\plugin\plugin.chm  %WORKDIR%\
-copy help\sakura\sakura.chm  %WORKDIR%\
+copy help\macro\macro.chm    %WORKDIR_EXE%\
+copy help\plugin\plugin.chm  %WORKDIR_EXE%\
+copy help\sakura\sakura.chm  %WORKDIR_EXE%\
 
 copy installer\warning.txt   %WORKDIR%\
-copy installer\Output\*.exe  %WORKDIR%\
+copy installer\Output\*.exe  %WORKDIR_INST%\
 
 7z a %OUTFILE%  -r %WORKDIR%
 7z l %OUTFILE%
