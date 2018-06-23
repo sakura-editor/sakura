e2327458a258053d6e262ac122a21f5bd363c1a6
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-06-23 23:25:25 +0900

    #164: msbuild のログをとって artifacts の中にいれる。

--- build-sln.bat
+++ build-sln.bat
@@ -10,10 +10,14 @@ if "%APPVEYOR%"=="True" (
     set EXTRA_CMD=
 )
 
+set LOG_FILE=msbuild-%platform%-%configuration%.log
+@rem https://msdn.microsoft.com/ja-jp/library/ms171470.aspx
+set LOG_OPTION=/flp:logfile=%LOG_FILE%
+
 set MSBUILD_EXE="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
-@echo %MSBUILD_EXE% %SLN_FILE% /p:Platform=%platform% /p:Configuration=%configuration%      /t:"Clean","Rebuild"  %EXTRA_CMD%
-      %MSBUILD_EXE% %SLN_FILE% /p:Platform=%platform% /p:Configuration=%configuration%      /t:"Clean","Rebuild"  %EXTRA_CMD%
+@echo %MSBUILD_EXE% %SLN_FILE% /p:Platform=%platform% /p:Configuration=%configuration%      /t:"Clean","Rebuild"  %EXTRA_CMD% %LOG_OPTION%
+      %MSBUILD_EXE% %SLN_FILE% /p:Platform=%platform% /p:Configuration=%configuration%      /t:"Clean","Rebuild"  %EXTRA_CMD% %LOG_OPTION%
 if %errorlevel% neq 0 (echo error && exit /b 1)
 
 exit /b 0
