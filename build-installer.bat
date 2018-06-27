866a121ca539ab3ca89c4ba03247854e7f44c1dc
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-06-27 21:45:20 +0900

    sinst_src.zip を展開して登録するのに対応

--- build-installer.bat
+++ build-installer.bat
@@ -4,17 +4,15 @@ set INSTALLER_RESOURCES=installer\temp
 set INSTALLER_WORK=installer\sakura
 set INSTALLER_OUTPUT=installer\Output
 
-set INSTALLER_RESOURCES_SINT=installer\temp\sint
+set INSTALLER_RESOURCES_SINT=installer\sinst_src
 set INSTALLER_RESOURCES_BRON=installer\temp\bron
 
 if exist "%INSTALLER_RESOURCES%" rmdir /s /q "%INSTALLER_RESOURCES%"
 if exist "%INSTALLER_WORK%"      rmdir /s /q "%INSTALLER_WORK%"
 if exist "%INSTALLER_OUTPUT%"    rmdir /s /q "%INSTALLER_OUTPUT%"
 
-if exist "%INSTALLER_RESOURCES_SINT%" rmdir /s /q "%INSTALLER_RESOURCES_SINT%"
 if exist "%INSTALLER_RESOURCES_BRON%" rmdir /s /q "%INSTALLER_RESOURCES_BRON%"
 
-7z x installer\sinst_src.zip                   -O"%INSTALLER_RESOURCES_SINT%" || (echo error && exit /b 1)
 7z x installer\externals\bregonig\bron412.zip  -O"%INSTALLER_RESOURCES_BRON%" || (echo error && exit /b 1)
 
 mkdir %INSTALLER_WORK%
