bbf0a66fdf6a5147936ab6ce76cef93de6b38223
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-06-28 21:40:18 +0900

    区切りと改行を出力

--- build-all.bat
+++ build-all.bat
@@ -3,21 +3,26 @@ set configuration=%2
 
 @echo PLATFORM      %PLATFORM%
 @echo CONFIGURATION %CONFIGURATION%
+@echo.
 
-@echo start build-sln.bat
+@echo ---- start build-sln.bat ----
 call build-sln.bat       %PLATFORM% %CONFIGURATION% || (echo error build-sln.bat       && exit /b 1)
-@echo end   build-sln.bat
+@echo ---- end   build-sln.bat ----
+@echo.
 
-@echo start build-chm.bat
+@echo ---- start build-chm.bat ----
 call build-chm.bat                                  || (echo error build-chm.bat       && exit /b 1)
-@echo end   build-chm.bat
+@echo ---- end   build-chm.bat ----
+@echo.
 
-@echo start build-installer.bat
+@echo ---- start build-installer.bat ----
 call build-installer.bat %PLATFORM% %CONFIGURATION% || (echo error build-installer.bat && exit /b 1)
-@echo end   build-installer.bat
+@echo ---- end   build-installer.bat ----
+@echo.
 
-@echo start zipArtifacts.bat
+@echo ---- start zipArtifacts.bat ----
 call zipArtifacts.bat    %PLATFORM% %CONFIGURATION% || (echo error zipArtifacts.bat    && exit /b 1)
-@echo end   zipArtifacts.bat
+@echo ---- end   zipArtifacts.bat ----
+@echo.
 
 exit /b 0
