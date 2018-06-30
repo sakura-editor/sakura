5d717a505a89529e6fbc5408dfd412767c97d0f3
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-06-30 09:43:18 +0900

    build ログの解析を追加

--- build-sln.bat
+++ build-sln.bat
@@ -20,4 +20,8 @@ set MSBUILD_EXE="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\M
       %MSBUILD_EXE% %SLN_FILE% /p:Platform=%platform% /p:Configuration=%configuration%      /t:"Clean","Rebuild"  %EXTRA_CMD% %LOG_OPTION%
 if %errorlevel% neq 0 (echo error && exit /b 1)
 
+@echo call parse-buildlog.bat %LOG_FILE%
+      call parse-buildlog.bat %LOG_FILE%
+if %errorlevel% neq 0 (echo error && exit /b 1)
+
 exit /b 0
