713a6e8d27328e3e73cae20e6e86c8956e49af1f
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-07-04 21:04:10 +0900

    zip ファイル名の構成要素を並び替える
    - "sakura"
    - platform
    - configuration
    - PR番号  (オプション)
    - ビルド番号 (オプション、appveyor の場合は常に存在)
    - commitHash (オプション、appveyor の場合は常に存在)
    - alpha (オプション、x64 のみ)

--- zipArtifacts.bat
+++ zipArtifacts.bat
@@ -7,26 +7,31 @@ if "%platform%" == "x64" (
 	set ALPHA=0
 )
 
-@echo checking APPVEYOR_BUILD_NUMBER
+@rem ----------------------------------------------------------------
+@rem prepare environment variable
+@rem ----------------------------------------------------------------
+@echo checking APPVEYOR_BUILD_NUMBER %APPVEYOR_BUILD_NUMBER%
 if not "%APPVEYOR_BUILD_NUMBER%" == "" (
 	set BUILD_NUMBER=build%APPVEYOR_BUILD_NUMBER%
 )
 
-@echo checking APPVEYOR_REPO_TAG_NAME
+@echo checking APPVEYOR_REPO_TAG_NAME %APPVEYOR_REPO_TAG_NAME%
 if not "%APPVEYOR_REPO_TAG_NAME%" == "" (
 	@rem replace '/' with '_'
-	set TAG_NAME=tag_%APPVEYOR_REPO_TAG_NAME:/=_%
+	set TEMP_NAME=%APPVEYOR_REPO_TAG_NAME:/=_%
+
+	@rem replace ' ' with '_'
+	set TAG_NAME=tag_%TEMP_NAME: =_%
 )
 
-@echo checking APPVEYOR_PULL_REQUEST_NUMBER
+@echo checking APPVEYOR_PULL_REQUEST_NUMBER %APPVEYOR_PULL_REQUEST_NUMBER%
 if not "%APPVEYOR_PULL_REQUEST_NUMBER%" == "" (
 	set PR_NAME=PR%APPVEYOR_PULL_REQUEST_NUMBER%
 )
 
-@echo checking APPVEYOR_REPO_COMMIT
-set SHORTHASH=%APPVEYOR_REPO_COMMIT:~0,8%
-if not "%SHORTHASH%" == "" (
-	set COMMITHASH=%SHORTHASH%
+@echo checking APPVEYOR_REPO_COMMIT %APPVEYOR_REPO_COMMIT%
+if not "%APPVEYOR_REPO_COMMIT%" == "" (
+	set SHORTHASH=%APPVEYOR_REPO_COMMIT:~0,8%
 )
 
 if "%ALPHA%" == "1" (
@@ -35,40 +40,50 @@ if "%ALPHA%" == "1" (
 	set RELEASE_PHASE=
 )
 
-set SUFFIX=
+@rem ----------------------------------------------------------------
+@rem build BASENAME
+@rem ----------------------------------------------------------------
+set BASENAME=sakura
 
-@echo adding BUILD_NUMBER
-if not "%BUILD_NUMBER%" == "" (
-	set SUFFIX=%SUFFIX%-%BUILD_NUMBER%
-)
-@echo SUFFIX = %SUFFIX%
+@echo adding platform and configuration
+set BASENAME=%BASENAME%-%platform%-%configuration%
+@echo BASENAME = %BASENAME%
 
 @echo adding TAG_NAME
 if not "%TAG_NAME%" == "" (
-	set SUFFIX=%SUFFIX%-%TAG_NAME%
+	set BASENAME=%BASENAME%-%TAG_NAME%
 )
-@echo SUFFIX = %SUFFIX%
+@echo BASENAME = %BASENAME%
 
 @echo adding PR_NAME
 if not "%PR_NAME%" == "" (
-	set SUFFIX=%SUFFIX%-%PR_NAME%
+	set BASENAME=%BASENAME%-%PR_NAME%
 )
-@echo SUFFIX = %SUFFIX%
+@echo BASENAME = %BASENAME%
 
-@echo adding COMMITHASH
-if not "%COMMITHASH%" == "" (
-	set SUFFIX=%SUFFIX%-%COMMITHASH%
+@echo adding BUILD_NUMBER
+if not "%BUILD_NUMBER%" == "" (
+	set BASENAME=%BASENAME%-%BUILD_NUMBER%
 )
-@echo SUFFIX = %SUFFIX%
+@echo BASENAME = %BASENAME%
+
+@echo adding SHORTHASH
+if not "%SHORTHASH%" == "" (
+	set BASENAME=%BASENAME%-%SHORTHASH%
+)
+@echo BASENAME = %BASENAME%
 
 @echo adding RELEASE_PHASE
 if not "%RELEASE_PHASE%" == "" (
-	set SUFFIX=%SUFFIX%-%RELEASE_PHASE%
+	set BASENAME=%BASENAME%-%RELEASE_PHASE%
 )
-@echo SUFFIX = %SUFFIX%
+@echo BASENAME = %BASENAME%
 
 @echo on
-set BASENAME=sakura-%platform%-%configuration%%SUFFIX%
+
+@rem ----------------------------------------------------------------
+@rem build WORKDIR
+@rem ----------------------------------------------------------------
 set WORKDIR=%BASENAME%
 set WORKDIR_LOG=%WORKDIR%\Log
 set WORKDIR_EXE=%WORKDIR%\EXE
