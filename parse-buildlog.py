1b3791ef1700f0c9fc9646df4fd5388fc0d657b0
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-07-05 20:12:36 +0900

    コメントを追加 (日本語コメントを書くために UTF-8 BOM 付きで保存)

--- parse-buildlog.py
+++ parse-buildlog.py
@@ -1,3 +1,4 @@
+﻿# -*- coding: utf-8 -*-
 import sys
 import re
 import os
@@ -6,11 +7,24 @@ import csv
 infile = sys.argv[1]
 outfile = infile + '.csv'
 
+# ファイル名に対する正規表現: 例 c:\hogehoge\hoge.c
 regLilePath  = r'(?P<filePath>[a-zA-Z]:([^(]+))'
+
+# 行番号に対する正規表現: 例 (100)
 regLineNumer = r'\((?P<lineNumber>\d+)\)'
+
+# エラーコードに対する正規表現: 例 warning C4267
 regError     = r'\s*(?P<type>\w+)\s+(?P<code>\w+)\s*'
+
+# エラーメッセージ: 例 'argument': conversion from 'size_t' to 'int', possible loss of data [C:\projects\sakura\sakura\sakura.vcxproj]
 regMessage   = r'(?P<message>.+)$'
+
+# 解析対象の正規表現
+# 例
+#   c:\projects\sakura\sakura_core\basis\cmystring.h(39): warning C4267: 'argument': conversion from 'size_t' to 'int', possible loss of data [C:\projects\sakura\sakura\sakura.vcxproj]
 regEx        = regLilePath + regLineNumer + r':' + regError + r':' + regMessage
+
+# 型変換に対する警告メッセージ部分に対する正規表現: 例 'size_t' to 'int'
 regFromTo    = r"from '(?P<source>[^']+)' to '(?P<dest>[^']+)'"
 
 data = []
@@ -62,9 +76,11 @@ with open(infile, "r") as fin:
 			entry['key'] = ' '.join(temp)
 			data.append(entry)
 
+# ソート対象のハッシュ配列で 'key' というキーを元にソートする。
 from operator import itemgetter
 data = sorted(data, key=itemgetter('key'))
 
+# 解析結果を CSV ファイルに出力する
 with open(outfile, "w") as fout:
 	fieldnames = [
 		'type',
