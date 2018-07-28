7c1f89a656e70090bedb09aced4dbb9cec079f4e
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-07-28 21:57:21 +0900

    ログを追加

--- tests/build-and-test.bat
+++ tests/build-and-test.bat
@@ -2,19 +2,26 @@ set platform=%1
 set configuration=%2
 set ERROR_RESULT=0
 
+@echo ---- start create-project.bat ----
 call %~dp0create-project.bat %platform% %configuration%
 if %ERRORLEVEL% neq 0 (
 	@echo ERROR in create-project.bat
 	exit /b 1
 )
+@echo ---- end   create-project.bat ----
+
+@echo ---- start build-project.bat ----
 call %~dp0build-project.bat %platform% %configuration%
 if %ERRORLEVEL% neq 0 (
 	@echo ERROR in build-project.bat
 	exit /b 1
 )
+@echo ---- end   build-project.bat ----
 
+@echo ---- start run-tests.bat ----
 call %~dp0run-tests.bat %platform% %configuration%
 if %ERRORLEVEL% neq 0 (
 	@echo ERROR in run-tests.bat
 	exit /b 1
 )
+@echo ---- end   run-tests.bat ----
