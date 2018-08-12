8657ffd8192428d839d097627fd0457bc72a5622
kobake <pg.kobake@gmail.com>
2018-08-12 14:40:19 +0900

    ビルド時の出力先に bregonig.dll を自動コピー

--- sakura/postBuild.bat
+++ sakura/postBuild.bat
@@ -1,1 +1,23 @@
 @echo off
+
+SETLOCAL
+
+@echo =======================
+@echo postBuild
+@echo =======================
+
+: ---- arguments ---- :
+: "Win32" or "x64"
+set PLATFORM=%1
+@echo PLATFORM=%PLATFORM%
+
+: "Debug" or "Release"
+set CONFIGURATION=%2
+@echo CONFIGURATION=%CONFIGURATION%
+
+: ---- copy bregonig.dll ---- :
+@echo Copy bregonig.dll to destination directory.
+copy /B ..\installer\externals\bregonig\%PLATFORM%\bregonig.dll ..\%PLATFORM%\%CONFIGURATION%\bregonig.dll
+
+
+ENDLOCAL
