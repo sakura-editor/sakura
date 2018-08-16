105f1201142600dfa9afa28dea2941f1395f9d7b
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-08-17 08:27:44 +0900

    powershell で zip ファイルを扱う

--- sakura/postBuild.bat
+++ sakura/postBuild.bat
@@ -2,6 +2,8 @@
 
 SETLOCAL
 
+set UNZIP_CMD=%~dp0..\tools\zip\unzip.bat
+
 @echo =======================
 @echo postBuild
 @echo =======================
@@ -18,8 +20,7 @@ set CONFIGURATION=%2
 set INSTALLER_RESOURCES_BRON=%~dp0..\installer\temp\bron
 set BRON_ZIP=%~dp0..\installer\externals\bregonig\bron412.zip
 
-if exist "%INSTALLER_RESOURCES_BRON%" rmdir /s /q "%INSTALLER_RESOURCES_BRON%"
-7z x %BRON_ZIP%  -O"%INSTALLER_RESOURCES_BRON%" || (echo error && exit /b 1)
+call %UNZIP_CMD%     %BRON_ZIP% %INSTALLER_RESOURCES_BRON% || (echo error && exit /b 1)
 
 set DLL_BREGONIG_NAME=bregonig.dll
 if "%platform%" == "x64" (
