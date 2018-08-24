9a3e316ced3fc09ccc7a4d909cd49bd08f3bab9a
berryplus <berryzplus@gmail.com>
2018-08-24 22:54:29 +0900

    mingw32-make clean時のcould not found fileを抑制する

--- sakura/mingw32-del.bat
+++ sakura/mingw32-del.bat
@@ -2,9 +2,25 @@
 
 SETLOCAL
 
-:引数で渡されたファイル群のパス区切りを置換してdelコマンドに渡す
+:引数で渡されたファイル群を取得
 set OUTFILES=%*
-del /F /Q %OUTFILES:/=\%
+:パス区切りを置換
+set OUTFILES=%OUTFILES:/=\%
+
+
+:del_file
+:1ファイルずつdelコマンドに渡して削除
+for /F "tokens=1,*" %%f in ("%OUTFILES%") DO (
+  if exist %%f del /F /Q %%f
+  set OUTFILES=%%g
+)
+
+:ぜんぶ削除できたら終了
+if "%OUTFILES%" == "" goto :EOF
+goto :del_file
+
+
+:END
 
 ENDLOCAL
 exit /b
