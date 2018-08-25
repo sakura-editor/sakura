d0dfcd7fc9fc6d12d1328de4ea0d21e67775d3a4
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-08-25 17:07:21 +0900

    appveyor でビルドできるように引数でビルド構成とプラットフォームを渡せるようにする

--- build-by-cmake.bat
+++ build-by-cmake.bat
@@ -1,41 +1,52 @@
-set BUILDDIR=build
+@echo off
+set platform=%1
+set configuration=%2
+set BUILDDIR=build\%platform%
+set PROJECT_TOP=%~dp0
+
+if "%platform%" == "" (
+	call :showhelp %0
+	exit /b 1
+)
+
+if "%configuration%" == "" (
+	call :showhelp %0
+	exit /b 1
+)
 
 cd /d %~dp0
-@echo off
 for /l %%n in (1,1,10) do (
 	if exist "%BUILDDIR%" (
+		@echo ---- removing %BUILDDIR% -----
 		rmdir /s /q "%BUILDDIR%"
 	)
 )
-@echo on
+@echo ---- create %BUILDDIR% -----
 mkdir "%BUILDDIR%" || exit /b 1
-cd    "%BUILDDIR%"
 
-set RETCODE=0
-if %ERRORLEVEL% equ 0 (
-	cmake ..
-) else (
-	set RETCODE=1
+set ERROR_RESULT=0
+
+@echo ---- creating project -----
+cmake -DCMAKE_GENERATOR_PLATFORM=%platform% -B%BUILDDIR% -H%PROJECT_TOP% || set ERROR_RESULT=1
+if "%ERROR_RESULT%" == "1" (
+	@echo ERROR
+	exit /b 1
 )
 
-if %ERRORLEVEL% equ 0 (
-	cmake --build . --config Debug
-) else (
-	set RETCODE=1
+@echo ---- building project -----
+cmake --build %BUILDDIR%  --config %configuration% || set ERROR_RESULT=1
+if "%ERROR_RESULT%" == "1" (
+	@echo ERROR
+	exit /b 1
 )
 
-if %ERRORLEVEL% equ 0 (
-	cmake --build . --config Release
-) else (
-	set RETCODE=1
-)
-cd ..
+@echo ---- build succeeded -----
+exit /b 0
 
-if %ERRORLEVEL% equ 0 (
-	echo success
-	set RETCODE=0
-) else (
-	set RETCODE=1
-)
-
-exit /b %RETCODE%
+@rem ------------------------------------------------------------------------------
+@rem show help
+@rem ------------------------------------------------------------------------------
+:showhelp
+@echo off
+@echo usage %1 platform configuration
+exit /b
