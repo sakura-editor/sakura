485d984d2cc848f338de1c8ea2392dc3fee42462
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-09-12 07:54:08 +0900

    チェックアウトするブランチ日付と時間を含める

--- get-PR.bat
+++ get-PR.bat
@@ -1,6 +1,15 @@
 @echo off
+
+set yyyy=%date:~0,4%
+set mm=%date:~5,2%
+set dd=%date:~8,2%
+set time2=%time: =0%
+set hh=%time2:~0,2%
+set mn=%time2:~3,2%
+set ss=%time2:~6,2%
+set timestamp=%yyyy%%mm%%dd%-%hh%%mn%%ss%
 set PR_NUMBER=%1
-set BRANCH_NAME=pull-request/%PR_NUMBER%
+set BRANCH_NAME=PR-%PR_NUMBER%/%timestamp%
 
 if "%PR_NUMBER%" == "" (
 	echo usage  : get-PR.bat PR-Number
@@ -11,4 +20,5 @@ if "%PR_NUMBER%" == "" (
 @echo fetching pull request %PR_NUMBER%
 git fetch origin pull/%PR_NUMBER%/head:%BRANCH_NAME%
 
+@echo checkout PR %PR_NUMBER% to branch %BRANCH_NAME%
 git checkout %BRANCH_NAME%
