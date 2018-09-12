effb9718eccd68787b843d904f21175cb17c5b3a
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-09-12 20:27:51 +0900

    意図通り動かないのを修正

--- get-PR.bat
+++ get-PR.bat
@@ -9,23 +9,37 @@ set mn=%time2:~3,2%
 set ss=%time2:~6,2%
 set timestamp=%yyyy%%mm%%dd%-%hh%%mn%%ss%
 set PR_NUMBER=%1
+set REMOTE_NAME=%2
 set BRANCH_NAME=PR-%PR_NUMBER%/%timestamp%
 
 if "%PR_NUMBER%" == "" (
-	echo usage  : get-PR.bat PR-Number
-	echo example: get-PR.bat 1
+	echo usage   : get-PR.bat PR-Number [remote name]
+	echo example1: get-PR.bat 1
+	echo example2: get-PR.bat 1 upstream
+	echo example3: get-PR.bat 1 origin
+	echo.
+	echo [remote name]
+	echo 1. If "remote name" is valid, it is used as origin of PR.
+	echo 2. If "remote name" is not valid and env SAKURA_EDITOR_REMOTE_NAME is valid, 
+	echo    env SAKURA_EDITOR_REMOTE_NAME is used as the origin of PR.
+	echo 3. Otherwise, "origin" is used as the origin of PR.
 	exit /b 1
 )
 
-if not "%%2%" == "" (
-	set SAKURA_EDITOR_REMOTE_NAME=%2
-) else if "%SAKURA_EDITOR_REMOTE_NAME%" == "" (
-	set SAKURA_EDITOR_REMOTE_NAME=origin
+if not "%REMOTE_NAME%" == "" (
+	@rem nothing to do
+) else if not "%SAKURA_EDITOR_REMOTE_NAME%" == "" (
+	set REMOTE_NAME=%SAKURA_EDITOR_REMOTE_NAME%
+) else (
+	set REMOTE_NAME=origin
 )
 
-@echo on
-@echo fetching pull request %PR_NUMBER%
-git fetch %SAKURA_EDITOR_REMOTE_NAME% pull/%PR_NUMBER%/head:%BRANCH_NAME%
+@echo fetching pull request %PR_NUMBER% from %REMOTE_NAME%
+echo git fetch %REMOTE_NAME% pull/%PR_NUMBER%/head:%BRANCH_NAME%
+     git fetch %REMOTE_NAME% pull/%PR_NUMBER%/head:%BRANCH_NAME% || exit /b 1
 
 @echo checkout PR %PR_NUMBER% to branch %BRANCH_NAME%
-git checkout %BRANCH_NAME%
+@echo git checkout %BRANCH_NAME%
+      git checkout %BRANCH_NAME% || exit /b 1
+
+exit /b 0
