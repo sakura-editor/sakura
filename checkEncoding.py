9965cd3fc3e97e0c1057ef90a841c92934b7d9c9
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-09-25 21:45:59 +0900

    rc ファイルのチェックに対応するためにリファクタリング

--- checkEncoding.py
+++ checkEncoding.py
@@ -2,7 +2,22 @@
 import chardet
 import os
 import sys
-import subprocess 
+import subprocess
+
+#########################################################
+# 定数
+#########################################################
+patternUTF8_BOM   = (
+	"utf-8-sig",
+	"ascii"
+)
+expectEncoding = {
+	".cpp" : patternUTF8_BOM,
+	".h"   : patternUTF8_BOM,
+}
+
+# チェック対象の拡張子リスト
+extensions = expectEncoding.keys()
 
 # 指定したファイルの文字コードを返す
 def check_encoding(file_path):
@@ -17,16 +32,12 @@ def check_encoding(file_path):
 
 # チェック対象の拡張子か判断する
 def checkExtension(file):
-	extensions = [
-		".cpp",
-		".h",
-	]
 	base, ext = os.path.splitext(file)
 	if ext in extensions:
 		return True
 	else:
 		return False
-		
+
 def getMergeBase():
 	output = subprocess.check_output('git show-branch --merge-base origin/master HEAD')
 	outputDec = output.decode()
@@ -39,40 +50,49 @@ def getDiffFiles():
 
 	output = subprocess.check_output('git diff ' + mergeBase + ' --name-only --diff-filter=dr')
 	diffFiles = output.decode()
-	return diffFiles.splitlines()
-
-def checkEncodingResult(encoding):
-	expectEncoding = [
-		"utf-8-sig",
-		"ascii",
-	]
-	encoding = encoding.lower()
-	if encoding in expectEncoding:
-		return True
-
-	return False
+	for file in diffFiles:
+		if checkExtension(file):
+			yield file
 
 # デバッグ用
-# すべてのファイルの文字コードを調べてチェックする
+# すべてのファイルを対象にチェック対象の拡張子のファイルの文字コードを調べてチェックする
 def checkAll():
 	for rootdir, dirs, files in os.walk('.'):
 		for file in files:
 			if checkExtension(file):
 				full = os.path.join(rootdir, file)
-				encoding = check_encoding(full)
-				if not checkEncodingResult(encoding):
-					print (encoding, full)
+				yield full
+
+# 指定したファイルの文字コードが期待通りか確認する
+def checkEncodingResult(file, encoding):
+	base, ext = os.path.splitext(file)
+	encoding = encoding.lower()
+	if encoding in expectEncoding.get(ext, ()):
+		return True
+	return False
+
+# 指定されたファイルリストに対して文字コードが適切かチェックする
+# (条件に満たないファイル数を返す。)
+def processFiles(files):
+	# 条件に満たないファイル数
+	count = 0
+
+	for file in files:
+		encoding = check_encoding(file)
+		if not checkEncodingResult(file, encoding):
+			print ("NG", encoding, file)
+			count = count + 1
+		else:
+			#print ("OK", encoding, file)
+			pass
+	return count
 
 if __name__ == '__main__':
 	count = 0
-
-	files = getDiffFiles()
-	for file in files:
-		if checkExtension(file):
-			encoding = check_encoding(file)
-			if not checkEncodingResult(encoding):
-				print (encoding, file)
-				count = count + 1
+	if len(sys.argv) > 1 and sys.argv[1] == "all":
+		count = processFiles(checkAll())
+	else:
+		count = processFiles(getDiffFiles())
 
 	if count > 0:
 		print ("return 1")
