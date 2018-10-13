0ba38d48a6e9264a9c2a640a334300f41840ee51
berryplus <berryzplus@gmail.com>
2018-10-13 22:54:24 +0900

    IsWin2000_or_laterを除去

--- sakura_core/env/CFileNameManager.cpp
+++ sakura_core/env/CFileNameManager.cpp
@@ -525,7 +525,7 @@ void CFileNameManager::GetIniFileNameDirect( LPTSTR pszPrivateIniFile, LPTSTR ps
 	//		exeと同じフォルダに置かれたマルチユーザ構成設定ファイル（sakura.exe.ini）の内容
 	//		に従ってマルチユーザ用のiniファイルパスを決める
 	pszPrivateIniFile[0] = _T('\0');
-	if( IsWin2000_or_later() ){
+	{
 		auto_snprintf_s( szPath, _MAX_PATH - 1, _T("%ts%ts%ts%ts"), szDrive, szDir, szFname, _T(".exe.ini") );
 		int nEnable = ::GetPrivateProfileInt(_T("Settings"), _T("MultiUser"), 0, szPath );
 		if( nEnable ){
--- sakura_core/uiparts/HandCursor.h
+++ sakura_core/uiparts/HandCursor.h
@@ -37,15 +37,7 @@
 
 inline void SetHandCursor ()
 {
-#ifdef IDC_CURSOR_HAND
-	if (!IsWin2000_or_later()) {
-		SetCursor( LoadCursor( GetModuleHandle( NULL ), MAKEINTRESOURCE( IDC_CURSOR_HAND ) ) );
-	}
-	else
-#endif
-	{
-		SetCursor( LoadCursor( NULL, IDC_HAND ) );
-	}
+	SetCursor( LoadCursor( NULL, IDC_HAND ) );
 }
 
 #endif	// SAKURA_HANDCURSOR_A545F10D_9F24_4AB5_889F_13732FC0150B_H_
