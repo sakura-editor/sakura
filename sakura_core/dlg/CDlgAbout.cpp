4cc8280b1c432e323ce22cc74b13374e6df19941
berryplus <berryzplus@gmail.com>
2018-10-20 20:37:13 +0900

    バッチ生成変数APPVEYOR_PR_NUMBER_LABEL追加
    リソーススクリプト内でリテラル文字列の連結が行われない対策として、
    githash.batでラベル用のリテラル文字列を生成させるように変更。
    当初APPVEYOR_PULL_REQUEST_NUMBER_LABELで作成したが、
    シンボル名が長すぎて切り詰められるので短い名前に変更。
    
    切り詰めが発生したときのログ↓
    3>..\sakura_core\sakura_rc.rc(119): warning RC4011: identifier truncated to 'APPVEYOR_PULL_REQUEST_NUMBER_LA'
    3>
    3>sakura.vcxproj -> F:\gitroot\sakura-mygithub\Win32\Debug\sakura.exe

--- sakura_core/dlg/CDlgAbout.cpp
+++ sakura_core/dlg/CDlgAbout.cpp
@@ -268,7 +268,7 @@ BOOL CDlgAbout::OnInitDialog( HWND hwndDlg, WPARAM wParam, LPARAM lParam )
 #ifdef APPVEYOR_SHORTHASH
 	m_UrlGitHubCommitWnd.SetSubclassWindow( GetItemHwnd( IDC_STATIC_URL_GITHUB_COMMIT ) );
 #endif
-#ifdef APPVEYOR_PULL_REQUEST_NUMBER
+#ifdef APPVEYOR_PR_NUMBER_LABEL
 	m_UrlGitHubPRWnd.SetSubclassWindow( GetItemHwnd( IDC_STATIC_URL_GITHUB_PR ) );
 #endif
 
