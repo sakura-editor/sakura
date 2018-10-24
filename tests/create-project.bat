532e01cde976874d15808c85a4b3dbca83d09ee3
ds14050 <ds14050@vvvvvv.sakura.ne.jp>
2018-10-25 00:44:00 +0900

    Fix #561 「通常のソリューションのコンパイル前に tests\build-and-test.bat でビルドするとコンパイルエラーになる」

--- tests/create-project.bat
+++ tests/create-project.bat
@@ -2,7 +2,18 @@ set platform=%1
 set configuration=%2
 set ERROR_RESULT=0
 
-pushd %~dp0
+pushd "%~dp0.."
+rem   produces header files neccessary in creating the project.
+@echo ---- start build-sln.bat ----
+call  build-sln.bat %platform% %configuration% || set ERROR_RESULT=1
+@echo ---- end   build-sln.bat ----
+popd
+if "%ERROR_RESULT%" == "1" (
+	@echo ERROR
+	exit /b 1
+)
+
+pushd "%~dp0"
 
 if not exist "googletest\CMakeLists.txt" (
     git submodule init
