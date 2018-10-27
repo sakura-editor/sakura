165f6e5f38f0a7b1e55603bb1812806b8a2adf64
berryplus <berryzplus@gmail.com>
2018-10-27 19:22:57 +0900

    msbuildを探すバッチを他エディションに対応させる

--- tools/msbuild/find-msbuild.bat
+++ tools/msbuild/find-msbuild.bat
@@ -1,32 +1,38 @@
 @rem see readme.md
 @echo off
 set CMD_MSBUILD=
-set PATH_MSBUILD_1=
-set PATH_MSBUILD_2=
-set PATH_MSBUILD_3=
 
-if not "%ProgramFiles%"      == "" set "PATH_MSBUILD_1=%ProgramFiles%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
-if not "%ProgramFiles(x86)%" == "" set "PATH_MSBUILD_2=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
-if not "%ProgramW6432%"      == "" set "PATH_MSBUILD_3=%ProgramW6432%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+call :pathTest Enterprise   && goto :done
+call :pathTest Professional && goto :done
+call :pathTest Community    && goto :done
 
-set RESULT_PATH_MSBUILD_1=--
-set RESULT_PATH_MSBUILD_2=--
-set RESULT_PATH_MSBUILD_3=--
+echo MsBuild has not found.
+exit /b 1
 
-if exist "%PATH_MSBUILD_1%" (
-	set RESULT_PATH_MSBUILD_1=OK
-	set "CMD_MSBUILD=%PATH_MSBUILD_1%"
-) else if exist "%PATH_MSBUILD_2%" (
-	set RESULT_PATH_MSBUILD_2=OK
-	set "CMD_MSBUILD=%PATH_MSBUILD_2%"
-) else if exist "%PATH_MSBUILD_3%" (
-	set RESULT_PATH_MSBUILD_3=OK
-	set "CMD_MSBUILD=%PATH_MSBUILD_3%"
+
+:pathTest
+:: will take a parameter to specify vs2017 editions
+if        "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+  call :invokeTest "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\%~1\MSBuild\15.0\Bin\MSBuild.exe"
+) else if "%PROCESSOR_ARCHITECTURE%" == "x86"   (
+  call :invokeTest "%ProgramFiles%\Microsoft Visual Studio\2017\%~1\MSBuild\15.0\Bin\MSBuild.exe"
+) else (
+  :: echo architecture '%PROCESSOR_ARCHITECTURE%' is currently not supported.
+  exit /b 1
 )
+if %errorlevel% neq 0 exit /b %errorlevel%
+exit /b 0
 
-@echo %RESULT_PATH_MSBUILD_1% %PATH_MSBUILD_1%
-@echo %RESULT_PATH_MSBUILD_2% %PATH_MSBUILD_2%
-@echo %RESULT_PATH_MSBUILD_3% %PATH_MSBUILD_3%
-@echo.
+
+:invokeTest
+:: will take a parameter to invoke
+%1 /version /nologo > NUL 2>&1
+if %errorlevel% neq 0 exit /b %errorlevel%
+set CMD_MSBUILD=%~1
+exit /b 0
+
+
+:done
 @echo CMD_MSBUILD "%CMD_MSBUILD%"
 @echo.
+exit /b 0
