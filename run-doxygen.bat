59cf3d3fb29578c82c7cd6cbae162c6276bad6ab
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-11-07 07:30:40 +0900

    appveyor から doxygen を実行する

--- run-doxygen.bat
+++ run-doxygen.bat
@@ -1,6 +1,8 @@
 call sakura\githash.bat
 call tools\hhc\find-hhc.bat
 
+if exist html rmdir /s /q html
+
 set PROJECT_NUMBER=%GIT_SHORT_COMMIT_HASH%
 set HHC_LOCATION=%CMD_HHC%
 doxygen doxygen.conf
