53c41ebadc04b8ed5d60fd663f3b2c3a6b2d7515
ds14050 <ds14050@vvvvvv.sakura.ne.jp>
2018-11-16 19:59:54 +0900

    テストバッチのリファクタリング＆ビルドターゲット指定。
    * tests1.exe をターゲットにすることで gmock, gmock_main のビルドを省略する。
    * pushd, popd の使用を減らし、バッチ中断時に変なディレクトリに残されないように。
    * CMake のために sh.exe を PATH から外す対策をより汎用的に。
      ※遅延環境変数が必要だが PATH 操作を外部に作用させるために
        サブルーチン単位で SETLOCAL はできない。
    * 継承・共有される環境変数 ERROR_RESULT の使用をやめ、SETLOCAL する。
    * create-project.bat と build-project.bat のサブルーチンは完全に一致する。
      いずれ両バッチを統合したい。

--- tests/build-project.bat
+++ tests/build-project.bat
@@ -1,21 +1,54 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
 set platform=%1
 set configuration=%2
-set ERROR_RESULT=0
 
-pushd %~dp0
-if not exist googletest (
+pushd "%~dp0"
+if not exist "googletest\CMakeLists.txt" (
     git submodule init
     git submodule update
 )
-
-if "%platform%" == "MinGW" set PATH=C:\msys64\mingw64\bin;%PATH:C:\Program Files\Git\usr\bin;=%
-
-set BUILDDIR=build\%platform%
-cmake --build %BUILDDIR%  --config %configuration% || set ERROR_RESULT=1
-
 popd
 
-if "%ERROR_RESULT%" == "1" (
-	@echo ERROR
-	exit /b 1
-)
+set BUILDDIR=%~dp0build\%platform%
+
+call :SetEnv_%platform% %platform% %configuration%
+cmake --build "%BUILDDIR%" --config %configuration% --target tests1 ^
+ || (echo ERROR & exit /b 1)
+
+exit /b 0
+
+rem ----------------------------------------------
+rem  sub-routines
+rem ----------------------------------------------
+
+:SetEnv_Win32
+:SetEnv_x64
+	set CMAKE_GEN_OPT=-G "Visual Studio 15 2017" -A "%~1"
+	set CMAKE_BLD_OPT=--config "%~2"
+exit /b 0
+
+:SetEnv_MinGW
+	set CMAKE_GEN_OPT=-G "MinGW Makefiles" -D CMAKE_BUILD_TYPE="%~2"
+	set CMAKE_BLD_OPT=
+	set PATH=C:\msys64\mingw64\bin;%PATH%
+	call :CMAKE_PATH_FIX
+exit /b 0
+
+:CMAKE_PATH_FIX
+	if not "%~1" == "sh.exe" (
+		call %0 "sh.exe" & exit /b
+	)
+	set SHDIR=%~dp$PATH:1
+	if "%SHDIR%" == "" (
+		exit /b 0
+	)
+	set SHDIR=%SHDIR:~0,-1%
+	set ORG=;%PATH%;
+	set MOD=!ORG:;%SHDIR%\;=;%!
+	set MOD=!MOD:;%SHDIR%;=;%!
+	set PATH=%MOD:~1,-1%
+	if not "%ORG%" == "%MOD%" (
+		call %0 "sh.exe" & exit /b
+	)
+exit /b 0
--- tests/create-project.bat
+++ tests/create-project.bat
@@ -1,58 +1,72 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
 set platform=%1
 set configuration=%2
-set ERROR_RESULT=0
 
-@rem produces header files necessary in creating the project.
+rem produces header files necessary in creating the project.
+
 if "%platform%" == "MinGW" (
+	call :CMAKE_PATH_FIX
 	set BUILD_EDITOR_BAT=build-gnu.bat
 ) else (
 	set BUILD_EDITOR_BAT=build-sln.bat
 )
-pushd "%~dp0.."
 @echo ---- start %BUILD_EDITOR_BAT% ----
-call  "%BUILD_EDITOR_BAT%" %platform% %configuration% || set ERROR_RESULT=1
+cmd /C "pushd "%~dp0.." & call "%BUILD_EDITOR_BAT%" %platform% %configuration%" ^
+ || (echo ERROR & exit /b 1)
 @echo ---- end   %BUILD_EDITOR_BAT% ----
-popd
-if "%ERROR_RESULT%" == "1" (
-	@echo ERROR
-	exit /b 1
-)
+
 
 pushd "%~dp0"
-
 if not exist "googletest\CMakeLists.txt" (
     git submodule init
     git submodule update
 )
+popd
 
-set BUILDDIR=build\%platform%
+set BUILDDIR=%~dp0build\%platform%
 if exist "%BUILDDIR%" (
 	rmdir /s /q "%BUILDDIR%"
 )
 mkdir "%BUILDDIR%"
 
-call :setenv_%platform% %platform% %configuration%
-cmake %CMAKE_GEN_OPT% -H. -B"%BUILDDIR%" || set ERROR_RESULT=1
+call :SetEnv_%platform% %platform% %configuration%
+cmake %CMAKE_GEN_OPT% -H"%~dp0." -B"%BUILDDIR%" ^
+ || (echo ERROR & exit /b 1)
 
-popd
+exit /b 0
 
-if "%ERROR_RESULT%" == "1" (
-	@echo ERROR
-	exit /b 1
-)
+rem ----------------------------------------------
+rem  sub-routines
+rem ----------------------------------------------
 
-exit /b
-
-@rem ----------------------------------------------
-@rem  sub-routines
-@rem ----------------------------------------------
-
-:setenv_Win32
-:setenv_x64
+:SetEnv_Win32
+:SetEnv_x64
 	set CMAKE_GEN_OPT=-G "Visual Studio 15 2017" -A "%~1"
-exit /b
+	set CMAKE_BLD_OPT=--config "%~2"
+exit /b 0
 
-:setenv_MinGW
+:SetEnv_MinGW
 	set CMAKE_GEN_OPT=-G "MinGW Makefiles" -D CMAKE_BUILD_TYPE="%~2"
-	set PATH=C:\msys64\mingw64\bin;%PATH:C:\Program Files\Git\usr\bin;=%
-exit /b
+	set CMAKE_BLD_OPT=
+	set PATH=C:\msys64\mingw64\bin;%PATH%
+	call :CMAKE_PATH_FIX
+exit /b 0
+
+:CMAKE_PATH_FIX
+	if not "%~1" == "sh.exe" (
+		call %0 "sh.exe" & exit /b
+	)
+	set SHDIR=%~dp$PATH:1
+	if "%SHDIR%" == "" (
+		exit /b 0
+	)
+	set SHDIR=%SHDIR:~0,-1%
+	set ORG=;%PATH%;
+	set MOD=!ORG:;%SHDIR%\;=;%!
+	set MOD=!MOD:;%SHDIR%;=;%!
+	set PATH=%MOD:~1,-1%
+	if not "%ORG%" == "%MOD%" (
+		call %0 "sh.exe" & exit /b
+	)
+exit /b 0
