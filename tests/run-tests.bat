698ba2276574508025d0bf2543e16a91d022ccf8
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-11-25 21:01:27 +0900

    テストが全く存在しない場合に appveyor を失敗させる

--- tests/run-tests.bat
+++ tests/run-tests.bat
@@ -1,6 +1,7 @@
 @echo off
 set platform=%1
 set configuration=%2
+set TEST_LAUNCHED=0
 set ERROR_RESULT=0
 
 set FILTER_BAT=%~dp0test_result_filter_tell_AppVeyor.bat
@@ -11,6 +12,8 @@ set BINARY_DIR=%BUILDDIR%\unittests\%platform%\%configuration%
 
 pushd %BINARY_DIR%
 for /r %%i in (tests*.exe) do (
+	set TEST_LAUNCHED=1
+
 	@echo %%i --gtest_list_tests
 	%%i --gtest_list_tests || set ERROR_RESULT=1
 
@@ -19,6 +22,11 @@ for /r %%i in (tests*.exe) do (
 )
 popd
 popd
+
+if "%TEST_LAUNCHED%" == "0" (
+	@echo ERROR: no tests are available.
+	exit /b 1
+)
 
 if "%ERROR_RESULT%" == "1" (
 	@echo ERROR
