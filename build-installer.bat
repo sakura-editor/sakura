553f1161a7ca4fb13db38c03b319afb35e401882
ds14050 <ds14050@vvvvvv.sakura.ne.jp>
2018-12-04 23:05:18 +0900

    Rewrite build-installer.bat with a recipe.

--- build-installer.bat
+++ build-installer.bat
@@ -1,7 +1,6 @@
 @echo off
 set platform=%1
 set configuration=%2
-set ISS_LOG_FILE=iss-%platform%-%configuration%.log
 
 if "%platform%" == "Win32" (
 	@rem OK
@@ -21,45 +20,60 @@ if "%configuration%" == "Release" (
 	exit /b 1
 )
 
-call %~dp0tools\ISCC\find-ISCC.bat
-if "%CMD_ISCC%" == "" (
+rem Definitions & Dependencies
+
+if not defined Build^
+   set Build=%~dp0%Platform%\%Configuration%
+set RECIPE_BAT=%~dp0tools\recipeProcessor.bat
+set RECIPE=%~dp0installer\recipe.txt
+set DST=%Build%\installer
+set SRC=%~dp0
+set SRC=%SRC:~0,-1%
+call :Set_RELATIVE_To "%Build%" "%SRC%"
+set BLD=%RELATIVE%
+set ISS=%DST%\sakura-%platform%.iss
+set ISS_LOG=%SRC%\iss-%platform%-%configuration%.log
+
+rem Setup
+
+mkdir 2>nul "%DST%"
+
+call "%~dp0tools\ISCC\find-ISCC.bat"
+if not defined CMD_ISCC (
 	echo ISCC.exe was not found.
 	exit /b 1
 )
 
-set INSTALLER_WORK=installer\sakura
-set INSTALLER_OUTPUT=installer\Output-%platform%
+rem Build
 
-set INSTALLER_RESOURCES_SINT=installer\sinst_src
-set INSTALLER_RESOURCES_BRON=installer\temp\bron
-set INSTALLER_RESOURCES_CTAGS=installer\temp\ctags
+call|"%RECIPE_BAT%" "%RECIPE%" "%SRC%" "%DST%"^
+ || (rmdir 2>nul /S /Q "%DST" & exit /b 1)
 
-if exist "%INSTALLER_WORK%"      rmdir /s /q "%INSTALLER_WORK%"
-if exist "%INSTALLER_OUTPUT%"    rmdir /s /q "%INSTALLER_OUTPUT%"
+@echo running "%CMD_ISCC%" "%ISS%"
+"%CMD_ISCC%" "%ISS%" > "%ISS_LOG%"^
+ || (echo error && exit /b 1)
+exit /b 0
 
-mkdir %INSTALLER_WORK%
-mkdir %INSTALLER_WORK%\license\bregonig
-mkdir %INSTALLER_WORK%\keyword
-mkdir %INSTALLER_WORK%\license\ctags\
+rem ---------------------------------------------------
 
-copy /Y .\LICENSE                                           %INSTALLER_WORK%\license\ > NUL
-copy /Y %INSTALLER_RESOURCES_SINT%\sakura.exe.manifest.x    %INSTALLER_WORK%\ > NUL
-copy /Y %INSTALLER_RESOURCES_SINT%\sakura.exe.manifest.v    %INSTALLER_WORK%\ > NUL
-copy /Y %INSTALLER_RESOURCES_SINT%\sakura.exe.ini           %INSTALLER_WORK%\ > NUL
-copy /Y %INSTALLER_RESOURCES_SINT%\keyword\*.*              %INSTALLER_WORK%\keyword\ > NUL
-copy /Y %INSTALLER_RESOURCES_BRON%\*.txt                    %INSTALLER_WORK%\license\bregonig\ > NUL
-copy /Y %INSTALLER_RESOURCES_CTAGS%\license\*.*             %INSTALLER_WORK%\license\ctags\ > NUL
+:Set_RELATIVE_To
+	setlocal
 
-copy /Y /B help\sakura\sakura.chm                           %INSTALLER_WORK%\ > NUL
-copy /Y /B help\plugin\plugin.chm                           %INSTALLER_WORK%\ > NUL
-copy /Y /B help\macro\macro.chm                             %INSTALLER_WORK%\ > NUL
+	set To=?%~dpnx1
+	set From=?%~dpnx2
+	if not "%From:~-1%" == "\" set From=%From%\
+	set _Parents=%~3
+	call set Relative=%%To:%From%=%%
 
-copy /Y /B %platform%\%configuration%\*.exe                 %INSTALLER_WORK%\ > NUL
-copy /Y /B %platform%\%configuration%\*.dll                 %INSTALLER_WORK%\ > NUL
-
-set SAKURA_ISS=installer\sakura-%platform%.iss
-@echo running "%CMD_ISCC%" %SAKURA_ISS%
-"%CMD_ISCC%" %SAKURA_ISS% > %ISS_LOG_FILE% || (echo error && exit /b 1)
+	if not "%To%" == "%Relative%" (
+		endlocal & set "RELATIVE=%_Parents%%Relative%"
+		exit /b 0
+	)
+	if "%~dp2" == "%~dpnx2" (
+		endlocal & set RELATIVE=
+		exit /b 1
+	)
+	endlocal & call %0 "%~dpnx1" "%~dp2" "..\%_Parents%"
 exit /b 0
 
 @rem ------------------------------------------------------------------------------
