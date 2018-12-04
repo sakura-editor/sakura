9dbc196aefbedd9b70e968327e8eb7c30b37ee95
ds14050 <ds14050@vvvvvv.sakura.ne.jp>
2018-12-05 02:24:59 +0900

    他のバッチを束ねてエディタとテストのビルドを統括するバッチに計時機能を付ける。

--- tests/build-and-test.bat
+++ tests/build-and-test.bat
@@ -24,10 +24,30 @@ rem ----------------------------------------------
 	set Platform=%~2
 	set   Config=%~3
 
-	echo ---- start %Batch% ----
+	call :StartWatch
+	echo ---- start %Batch% [%StartDate% %StartTime%] ----
 	echo.
 	cmd /C "call "%Batch%" %Platform% %Config%" ^
 	|| exit /b 1
-	echo ---- end   %Batch% ----
+	call :StopWatch
+	echo ---- end   %Batch% [%StopDate% %StopTime% / %Elapsed% seconds elapsed.] ----
 	echo.
+exit /b 0
+
+:StartWatch
+	set StartDate=%DATE%
+	set StartTime=%TIME: =0%
+exit /b 0
+
+:StopWatch
+	rem Be aware of space-padding hours and octal number literals
+	rem when calculating the Elapsed.
+
+	set StopDate=%DATE%
+	set StopTime=%TIME: =0%
+	set /A Elapsed=(1%StopTime:~0,2%  * 3600 + 1%StopTime:~3,2%  * 60 + 1%StopTime:~6,2%) ^
+	              -(1%StartTime:~0,2% * 3600 + 1%StartTime:~3,2% * 60 + 1%StartTime:~6,2%)
+	if not "%StartDate%" == "%StopDate%" (
+		set /A Elapsed=%Elapsed% + 24 * 3600
+	)
 exit /b 0
