1d7c1f9c4e096292d746534b113453f6bd582622
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-12-17 21:40:57 +0900

    grep ダイアログを起動したときにデフォルトの検索パターンでパターンを空にして検索したときも強制的にデフォルトパターンが適用されてしまうを修正

--- sakura_core/dlg/CDlgGrep.h
+++ sakura_core/dlg/CDlgGrep.h
@@ -22,6 +22,9 @@ class CDlgGrep;
 #include "recent/CRecent.h"
 #include "util/window.h"
 
+#define DEFAULT_EXCLUDE_FILE_PATTERN    _T("*.msi;*.exe;*.obj;*.pdb;*.ilk;*.res")
+#define DEFAULT_EXCLUDE_FOLDER_PATTERN  _T(".git;.svn;.vs")
+
 //! GREPダイアログボックス
 class CDlgGrep : public CDialog
 {
