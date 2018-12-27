30fcbae989eb3dfbbbc62d94c046d0e518113bcf
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-12-27 16:29:56 +0900

    sphinx での HTML Help の作成に対応

--- build-all.bat
+++ build-all.bat
@@ -42,6 +42,11 @@ call build-chm.bat                                  || (echo error build-chm.bat
 @echo ---- end   build-chm.bat ----
 @echo.
 
+@echo ---- start sphinx\build-sphinx.bat ----
+call sphinx\build-sphinx.bat                        || (echo error sphinx\build-sphinx.bat && exit /b 1)
+@echo ---- end   sphinx\build-sphinx.bat ----
+@echo.
+
 @echo ---- start build-installer.bat ----
 call build-installer.bat %PLATFORM% %CONFIGURATION% || (echo error build-installer.bat && exit /b 1)
 @echo ---- end   build-installer.bat ----
---
+++ sphinx/build-sphinx.bat
@@ -0,0 +1,40 @@
+@echo off
+
+set MAKE_BAT=%~dp0sakura\make.bat
+set HHP_SAKURA=%~dp0sakura\build\htmlhelp\SAKURAEditordoc.hhp
+
+python -c "import sphinx"
+if errorlevel 1 (
+	echo Please run 'pip install sphinx' in advance.
+	exit /b 1
+)
+
+pushd "%~dp0"
+
+call "%MAKE_BAT%" htmlhelp
+if errorlevel 1 (
+	echo failed to create htmlhelp project files.
+	popd
+	exit /b 1
+)
+
+call "%~dp0..\tools\hhc\find-hhc.bat"
+if "%CMD_HHC%" == "" (
+	echo hhc.exe was not found.
+	popd
+	exit /b 1
+)
+
+"%CMD_HHC%" "%HHP_SAKURA%"
+if not errorlevel 1 (
+	echo error "%HHP_SAKURA%" errorlevel %errorlevel%
+	"%CMD_HHC%" "%HHP_SAKURA%"
+)
+if not errorlevel 1 (
+	echo error "%HHP_SAKURA%" errorlevel %errorlevel%
+	popd
+	exit /b 1
+)
+
+popd
+exit /b 0
