7457ad345e802bc9286c196a1e3afab7604b02d4
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-02-03 07:17:31 +0900

    Revert "バッファが確保されていない状態のときにクリア要求が来たときに落ちる不具合を修正"
    This reverts commit 6ffaefceed03173166e6dc2c4adf29322926587e.

--- sakura_core/mem/CNative.cpp
+++ sakura_core/mem/CNative.cpp
@@ -2,13 +2,6 @@
 #include "StdAfx.h"
 #include "CNative.h"
 
-//! 基底の CMemory を明示的に初期化して最低限のメモリ確保させる
-CNative::CNative( void )
-	: CMemory( NULL, 0 )
-{
-
-}
-
 //! 空っぽにする
 void CNative::Clear()
 {
--- sakura_core/mem/CNative.h
+++ sakura_core/mem/CNative.h
@@ -30,8 +30,6 @@
 //※CMemoryをprotect継承することにより、あまり自由にCMemoryを使えないようにしておく
 class CNative : protected CMemory{
 public:
-	CNative();
-	
 	//CMemory*ポインタを得る
 	CMemory* _GetMemory(){ return static_cast<CMemory*>(this); }
 	const CMemory* _GetMemory() const{ return static_cast<const CMemory*>(this); }
