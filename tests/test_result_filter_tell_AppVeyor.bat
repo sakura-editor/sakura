083f208d04e2305794deef05eba1abc2b111844c
ds14050 <ds14050@vvvvvv.sakura.ne.jp>
2019-02-06 01:07:34 +0900

    Fix #781 issue「googletest でテストに失敗しても ビルド結果が失敗にならない」 (#784)
    パイプで連結した複数のコマンドの終了コードは、最後のコマンドのもので代表されます。フィルタでテストの成功・失敗を判断しエラーレベルをセットします。
    
    1. 失敗の条件１ 失敗したテストがある
    2. 失敗の条件２ パスしたテスト数の報告(※０件の場合もある)がない。(テストのビルド・実行に失敗している可能性が高い)
    3. 成功の条件 失敗していないこと

--- tests/test_result_filter_tell_AppVeyor.bat
+++ tests/test_result_filter_tell_AppVeyor.bat
@@ -15,6 +15,10 @@ if errorlevel 1 (
 	set AppVeyor=
 )
 
+::: Test result
+set PASSED=
+set FAILED=
+
 set fileName=
 set testName=
 set testMsg=
@@ -22,32 +26,35 @@ set testMsg=
 for /F "usebackq delims=" %%L in (`FINDSTR /B "^"`) do (
 	echo %%L
 
-	if not "%AppVeyor%" == "" (
-		for /F "tokens=1,2,3,5 delims=[]() " %%A in ("%%L") do (
-			if "%%A" == "----------" (
-				if not "%%D" == "" (
-					set fileName=%%D
-				)
-			) else if "%%A" == "RUN" (
-				set testName=%%B
-				set testMsg=
-			) else if "%%A" == "OK" (
-				if not "!testName!,!fileName!" == "," (
-					rem `start` does not wait for `%AppVeyor%` to return, so the main loop goes immediately.
-					start "" "%AppVeyor%" AddTest !testName! -Framework xUnit -FileName !fileName! -Outcome Passed -Duration %%C -StdOut "!testMsg!"
-				)
-			) else if "%%A" == "FAILED" (
-				if not "!testName!,!fileName!" == "," (
-					rem `start` does not wait for `%AppVeyor%` to return, so the main loop goes immediately.
-					start "" "%AppVeyor%" AddTest !testName! -Framework xUnit -FileName !fileName! -Outcome Failed -Duration %%C -ErrorMessage "!testMsg!"
-				)
-			) else if "%%A" == "PASSED" (
-				rem
-			) else if "%%A" == "==========" (
-				rem
-			) else (
-				set testMsg=!testMsg!%%L!NL!
+	for /F "tokens=1,2,3,5 delims=[]() " %%A in ("%%L") do (
+		if "%%A" == "----------" (
+			if not "%%D" == "" (
+				set fileName=%%D
 			)
+		) else if "%%A" == "RUN" (
+			set testName=%%B
+			set testMsg=
+		) else if "%%A" == "OK" (
+			if not "!testName!,!fileName!" == "," (
+				rem `start` does not wait for `%AppVeyor%` to return, so the main loop goes immediately.
+				if defined AppVeyor start "" "%AppVeyor%" AddTest !testName! -Framework xUnit -FileName !fileName! -Outcome Passed -Duration %%C -StdOut "!testMsg!"
+			)
+		) else if "%%A" == "FAILED" (
+			if not "!testName!,!fileName!" == "," (
+				rem `start` does not wait for `%AppVeyor%` to return, so the main loop goes immediately.
+				if defined AppVeyor start "" "%AppVeyor%" AddTest !testName! -Framework xUnit -FileName !fileName! -Outcome Failed -Duration %%C -ErrorMessage "!testMsg!"
+			)
+			set FAILED=FAILED
+		) else if "%%A" == "PASSED" (
+			set PASSED=PASSED
+		) else if "%%A" == "==========" (
+			rem
+		) else (
+			set testMsg=!testMsg!%%L!NL!
 		)
 	)
 )
+
+if     defined FAILED exit /b 1
+if not defined PASSED exit /b 1
+exit /b 0
