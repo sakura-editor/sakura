adcb5b7d39f33cdf3d66745f0a1bd16f354887f9
ds14050 <ds14050@vvvvvv.sakura.ne.jp>
2019-02-20 03:01:39 +0900

    Ignore once tracked but currently untracked files properly.

--- tools/RestoreCommitterDateRoughly.ps1
+++ tools/RestoreCommitterDateRoughly.ps1
@@ -3,25 +3,41 @@
 	Git ワーキングツリーに含まれるファイルの更新日時に最終コミット日時を設定します。
 
 	.DESCRIPTION
-	Git でチェックアウトしたファイルの更新日時はチェックアウト日時になります。これはファイルのタイムスタンプを比較する場合に不都合な場合があり、代わりに最終コミット日時をタイムスタンプに設定します。
+	Git でチェックアウトしたファイルの更新日時はチェックアウト日時になります。これはファイルのタイムスタンプを比較するのに不都合があるため、代わりに最終コミット日時を設定します。
 
-	Git リポジトリ内で実行します。すべての階層のファイルが対象になります。
+	Git ワーキングツリー内で実行します。すべての階層のファイルが対象になります。
 
 	制限: ログを遡りすべてのファイルの最終コミット日時を取得することは１分以上かかることがあるため、このスクリプトでは１か月を上限としてログを遡ります。そのため最も古いタイムスタンプは実行日時を基準として前月同日の00:00:00になります。
 #>
-$TopDir = git rev-parse --show-toplevel
+$TopDir = (git rev-parse --show-toplevel)
 $Oldest = [DateTime]::Today.AddMonths(-1) # Of course, this is incorrect for the oldest timestamp. That's what "Roughly" means.
 
+$waiting = @{}
+(git ls-files --full-name "$TopDir") | foreach {
+	$waiting[$_] = $NULL
+}
+
 @(
-	"?$($Oldest.ToString("yyyy-MM-dd HH:mm:ss"))"
+	(git log --format=format:?%ci --name-only --since="$($Oldest.ToString("yyyy-MM-dd HH:mm:ss"))")
+	, "?$($Oldest.ToString("yyyy-MM-dd HH:mm:ss"))"
 	, (git ls-files --full-name "$TopDir")
-	, (git log --format=format:?%ci --name-only --since="$($Oldest.ToString("yyyy-MM-dd HH:mm:ss"))" --reverse)
-) | foreach { $_ | foreach { $_ } } | foreach {
+) | foreach { $_ | foreach {
 	if ($_ -eq "") {
 	} elseif ($_[0] -eq "?") {
-		$t = $_.Substring(1) -as [DateTime]
+		$t = $_.Substring(1)
 	} else {
-		try { ([System.IO.FileInfo]"$TopDir\$_").LastWriteTime = $t }
-		catch { Write-Debug($_) }
+		$f = [System.IO.FileInfo] "$TopDir\$_"
+
+		# run once a file.
+		$c = $waiting.Count
+		$waiting.Remove($_)
+		if ($waiting.Count -ne $c -and $f.Exists) {
+			try {
+				$t = [System.DateTime] $t
+				$f.LastWriteTime = $t
+			} catch {
+				Write-Debug $_
+			}
+		}
 	}
-}
+} }
