dc1872534496d4e01f2fb9bd95f57c6d65645438
ds14050 <ds14050@vvvvvv.sakura.ne.jp>
2019-04-14 01:28:09 +0900

    more tests.

--- tests/unittests/test-is_mailaddress.cpp
+++ tests/unittests/test-is_mailaddress.cpp
@@ -28,6 +28,7 @@
 #include <tchar.h>
 #include <wchar.h>
 #include <assert.h>
+#include <string>
 #include <Windows.h>
 #include "parse/CWordParse.h"
 
@@ -228,7 +229,7 @@ TEST(testIsMailAddress, OffsetParameter)
 	}
 
 	/*
-	   Run tests.
+	   Apply IsMailAddress to the cases.
 	*/
 	for (auto& aCase: testCases) {
 		EXPECT_EQ(
@@ -236,6 +237,74 @@ TEST(testIsMailAddress, OffsetParameter)
 			bool(IsMailAddress(aCase.buffer(), aCase.offset, BufferEnd - aCase.buffer(), NULL))
 		) << "1st param of IsMailAddress: pszBuf is \"" << aCase.buffer() << "\"\n"
 		  << "2nd param of IsMailAddress: offset is "   << aCase.offset;
+	}
+}
+
+TEST(testIsMailAddress, OffsetParameter2)
+{
+	const wchar_t* const Text             = L"   test@example.com   ";
+	const wchar_t* const Address          = Text +  3; // Address is "test@example.com"
+	const wchar_t* const PseudoAddress    = Text +  6; // PseudoAddress is "t@example.c", shortest form recognized by IsMailAddress.
+	const wchar_t* const PseudoAddressEnd = Text + 17;
+	const wchar_t* const AddressEnd       = Text + 19;
+	const wchar_t* const TextEnd          = Text + 22;
+
+	struct Result {
+		bool is_address;
+		int  length;
+	};
+	const Result FalseResult = {false, 0};
+	auto ExpectedResult = [=](const wchar_t* p1, const wchar_t* p2, const wchar_t* p3) -> Result
+	{
+		/*
+		   p2 と p3 が以下の条件を外れたら、TRUE 判定の可能性はゼロ。
+		   * Address <= p2 <= PseudoAddress
+		   * PseudoAddressEnd <= p3
+		*/
+		if (p2 < Address || PseudoAddress < p2) {
+			return FalseResult;
+		}
+		if (p3 < PseudoAddressEnd) {
+			return FalseResult;
+		}
+
+		if (p2 == Address) {
+			if (AddressEnd <= p3) {
+				return Result{true, static_cast<int>(AddressEnd - Address)}; // 文句なしの TRUE 判定。
+			} else {
+				return Result{true, static_cast<int>(p3 - Address)}; // アドレスの終端が切り詰められているが、IsMailAddress には知る由がない。ゆえに問題なし。
+			}
+		} else if (p2 <= p1) {
+			if (AddressEnd <= p3) {
+				return Result{true, static_cast<int>(AddressEnd - p2)}; // アドレスの先端が切り詰められているが、IsMailAddress には知る由がない。ゆえに問題なし。
+			} else {
+				return Result{true, static_cast<int>(p3 - p2)}; // アドレスの先端と終端が切り詰められているが、IsMailAddress には知る由がない。ゆえに問題なし。
+			}
+		} else {
+			return FalseResult; // アドレスの先端が切り詰められ、IsMailAddress が境界判定によりそれを検知した。文句なしの FALSE 判定。
+		}
+	};
+	auto IsEqualResult = [](const Result& expected, const Result& actual) -> testing::AssertionResult
+	{
+		if (expected.is_address != actual.is_address) {
+			return testing::AssertionFailure() << "IsMailAddress returned " << (actual.is_address?"TRUE":"FALSE") << " but expected " << (expected.is_address?"TRUE":"FALSE") << ".";
+		}
+		if (expected.length != actual.length) {
+			return testing::AssertionFailure() << "IsMailAddress returned the address length " << (actual.length) << " but expected " << (expected.length) << ".";
+		}
+		return testing::AssertionSuccess();
+	};
+
+	for (const wchar_t* p1 = Text; p1 != TextEnd; ++p1)   // p1 is a pointer to buffer.
+	for (const wchar_t* p2 = Text; p2 != TextEnd; ++p2)   // p2 is a pointer to address.
+	for (const wchar_t* p3 = Text; p3 != TextEnd; ++p3) { // p3 is a pointer to the end of buffer.
+		Result actual = {false, 0};
+		actual.is_address = IsMailAddress(p1, p2 - p1, p3 - p1, &(actual.length));
+
+		EXPECT_TRUE(IsEqualResult(ExpectedResult(p1, p2, p3), actual))
+		<< "1st param of IsMailAddress: pszBuf is \"" << (p1 <= p3 ? std::string(p1, p3) : "") << "\"\n"
+		<< "2nd param of IsMailAddress: offset is "   << (p2 - p1) << "\n"
+		<< "pszBuf + offset is \"" << (p2 <= p3 ? std::string(p2, p3) : "") << "\"";
 	}
 }
 
