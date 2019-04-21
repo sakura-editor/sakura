53ee2ea0cb999cc0025587d95a3ecfa5d8d36e8d
ds14050 <ds14050@vvvvvv.sakura.ne.jp>
2019-04-22 02:50:37 +0900

    postBuild.bat から失われた build-installer.bat が依存する処理を復元する。
    関連 #388 「installer\externals\bregonig\bron412.zip がビルド要件になっている件について」
    関連 #390 「bron412.zip の解凍が必要ないときは bron412.zip の解凍をスキップする」
    関連 #790 「vs2017ビルド時のビルドログを整理する」

--- sakura/postBuild.bat
+++ sakura/postBuild.bat
@@ -29,11 +29,11 @@ if "%platform%" == "x64" (
 ) else (
 	set BRON_DIR=%~dp0%BRON_TMP%
 )
+if not exist "%BRON_DIR%\%BREGONIG_DLL%" (
+	call "%UNZIP_CMD%" "%~dp0%BRON_ZIP%" "%~dp0%BRON_TMP%" > NUL || (echo error && exit /b 1)
+)
 if not exist "%OUT_DIR%\%BREGONIG_DLL%" (
 	@echo %BRON_ZIP% -^> %DEST_DIR%\%BREGONIG_DLL%
-	if not exist "%BRON_DIR%\%BREGONIG_DLL%" (
-		call "%UNZIP_CMD%" "%~dp0%BRON_ZIP%" "%~dp0%BRON_TMP%" > NUL || (echo error && exit /b 1)
-	)
 	copy /Y /B "%BRON_DIR%\%BREGONIG_DLL%" "%OUT_DIR%\" > NUL
 )
 
@@ -50,11 +50,11 @@ if "%PLATFORM%" == "Win32" (
 set CTAGS_ZIP=..\installer\externals\universal-ctags\ctags-2018-09-16_e522743d-%CTAGS_PREFIX%.zip
 set CTAGS_TMP=..\installer\temp\ctags
 set CTAGS_DIR=%~dp0%CTAGS_TMP%
+if not exist "%CTAGS_DIR%\%CTAGS_EXE%" (
+	call "%UNZIP_CMD%" "%~dp0%CTAGS_ZIP%" "%CTAGS_DIR%" > NUL || (echo error && exit /b 1)
+)
 if not exist "%OUT_DIR%\%CTAGS_EXE%" (
 	@echo %CTAGS_ZIP% -^> %DEST_DIR%\%CTAGS_EXE%
-	if not exist "%CTAGS_DIR%\%CTAGS_EXE%" (
-		call "%UNZIP_CMD%" "%~dp0%CTAGS_ZIP%" "%CTAGS_DIR%" > NUL || (echo error && exit /b 1)
-	)
 	copy /Y /B "%CTAGS_DIR%\%CTAGS_EXE%" "%OUT_DIR%\" > NUL
 )
 
