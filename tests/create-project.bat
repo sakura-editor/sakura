1f245d205d60dd0dfb8e6356f727e5201cc41552
berryplus <berryzplus@gmail.com>
2019-06-01 18:18:24 +0900

    appveyor向けデバッグコード挿入

--- tests/create-project.bat
+++ tests/create-project.bat
@@ -59,6 +59,13 @@ exit /b
 		set /A productLineVersion=%%b
 	)
 	set CMAKE_GEN_OPT=-G "Visual Studio %NUM_VSVERSION% %productLineVersion%" -A "%~1" -D BUILD_GTEST=ON
+	::debug
+	echo cmd_vswhere is "%cmd_vswhere%"
+	echo num_vsversion is "%num_vsversion%"
+	echo num_nextvsversion is "%num_nextvsversion%"
+	echo productLineVersion is "%productLineVersion%"
+	echo productLineVersionCommand is `"%CMD_VSWHERE%" -version [%NUM_VSVERSION%^,%NUM_NEXTVSVERSION%^) -property catalog_productLineVersion`
+	echo CMAKE_GEN_OPT is "%CMAKE_GEN_OPT%"
 exit /b
 
 :setenv_MinGW
