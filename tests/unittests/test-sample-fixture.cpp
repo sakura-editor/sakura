0d40a0607769f8e62e93980f0f671dc05cb5cd23
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-06-08 06:40:01 +0900

    Fixture テストのサンプルを追加

---
+++ tests/unittests/test-sample-fixture.cpp
@@ -0,0 +1,46 @@
+﻿#include <gtest/gtest.h>
+#include <cstdio>
+#include <exception>
+
+
+/*!
+	`test fixture class` のサンプル
+
+	以下を参照
+	- http://opencv.jp/googletestdocs/primer.html#primer-test-fixtures
+	- https://github.com/google/googletest/blob/ea9c49b9cd410cca4df39b376f7da7cddf2dcf6d/googletest/docs/primer.md#basic-concepts
+	- https://github.com/google/googletest/blob/ea9c49b9cd410cca4df39b376f7da7cddf2dcf6d/googletest/docs/primer.md#test-fixtures-using-the-same-data-configuration-for-multiple-tests
+*/
+class FixtureSample : public ::testing::Test
+{
+protected:
+	/*!
+		テスト前の準備
+	*/
+	virtual void SetUp()
+	{
+		printf("%s\n", __FUNCTION__);
+	}
+
+	/*!
+		テスト後の後始末
+	*/
+	virtual void TearDown()
+	{
+		printf("%s\n", __FUNCTION__);
+	}
+};
+
+/*!
+	Fixture テストのサンプル
+*/
+TEST_F(FixtureSample, Test1)
+{
+}
+
+/*!
+	Fixture テストのサンプル
+*/
+TEST_F(FixtureSample, Test2)
+{
+}
