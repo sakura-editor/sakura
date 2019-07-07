2a2e9611ef4b5972e9aac0a3734f4206f611d684
berryplus <berryzplus@gmail.com>
2019-07-07 18:49:16 +0900

    x64ビルドできてなかったのを修正

--- tests/create-project-test.bat
+++ tests/create-project-test.bat
@@ -26,24 +26,39 @@ for /f "usebackq" %%b in (`vswhere -version [%NUM_VSVERSION%^,%NUM_NEXTVSVERSION
 	set /A productLineVersion=%%b 
 ) 
 
-for /f "usebackq delims=" %%a in (`vswhere -version [%NUM_VSVERSION%^,%NUM_NEXTVSVERSION%^) -find Common7\Tools\vsdevcmd.bat`) do (
-    call "%%a" || exit /b 1
+set vcvarsArchitecture=
+if "%PROCESSOR_ARCHITECTURE%" == "x86" (
+	if "%platform%" == "x64" (
+		set vcvarsArchitecture=x86_x64
+	) else (
+		set vcvarsArchitecture=x86
+	)
+) else (
+	if "%platform%" == "x64" (
+		set vcvarsArchitecture=x64
+	) else (
+		set vcvarsArchitecture=x64_x86
+	)
+)
+
+for /f "usebackq delims=" %%a in (`vswhere -version [%NUM_VSVERSION%^,%NUM_NEXTVSVERSION%^) -find VC/Auxiliary/Build/vcvarsall.bat`) do (
+    call "%%a" %vcvarsArchitecture% || exit /b 1
 )
 
 echo test for 'Visual Studio' generator.
-set CMAKE_GEN_OPT=-G "Visual Studio %NUM_VSVERSION% %productLineVersion%" -A "%platform%"
-for /L %%a IN (0,1,10) DO (
+if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+	set CMAKE_GEN_OPT=-G "Visual Studio %NUM_VSVERSION% %productLineVersion% Win64"
+) else (
+	set CMAKE_GEN_OPT=-G "Visual Studio %NUM_VSVERSION% %productLineVersion%" -A "%platform%"
+)
+for /L %%a IN (0,1,9) DO (
 	set BUILDDIR=build\vs%%a
 	call :run_cmake vs %%a || exit /b 1
 )
 
 echo test for 'Ninja' generator.
-for /f "usebackq delims=;" %%c in (`vswhere -version [%NUM_VSVERSION%^,%NUM_NEXTVSVERSION%^) -find VC/Tools/MSVC/**/bin/Hostx86/x86/cl.exe`) do ( 
-	set "CMD_CL=%%c"
-)
-set "CMD_CL=%CMD_CL:\=/%"
-set CMAKE_GEN_OPT=-G "Ninja" "-DCMAKE_C_COMPILER=%CMD_CL%" "-DCMAKE_CXX_COMPILER=%CMD_CL%"
-for /L %%a IN (0,1,10) DO (
+set CMAKE_GEN_OPT=-G "Ninja"
+for /L %%a IN (0,1,9) DO (
 	set BUILDDIR=build\nj%%a
 	call :run_cmake Ninja %%a || exit /b 1
 )
@@ -57,7 +72,7 @@ exit /b 0
 :run_cmake
 echo .
 echo %1(%2) - start
-cmake %CMAKE_GEN_OPT% -D BUILD_GMOCK=OFF -B"%BUILDDIR%" -Hgoogletest || exit /b 1
+cmake %CMAKE_GEN_OPT% -DCMAKE_BUILD_TYPE=%configuration% -Dgtest_build_tests=OFF -Dgtest_build_samples=OFF -D BUILD_GMOCK=OFF -B"%BUILDDIR%" -Hgoogletest || exit /b 1
 cmake --build "%BUILDDIR%" --config %configuration% || exit /b 1
 echo %1(%2) - end
 echo .
