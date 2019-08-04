003d908fb23e766c854814fc96edbf3531f347e4
beru <berunpon@gmail.com>
2019-08-04 15:09:56 +0900

    文字エンコーディング修正

--- sakura_core/mem/CMemPool.h
+++ sakura_core/mem/CMemPool.h
@@ -1,4 +1,4 @@
-/*
+﻿/*
 	Copyright (C) 2018-2019 Sakura Editor Organization
 
 	This software is provided 'as-is', without any express or implied
@@ -28,8 +28,8 @@
 #include <memory>
 #include "util/design_template.h"
 
-// T : �v�f�^
-// BlockSize : �u���b�N�̑傫���̃o�C�g��
+// T : 要素型
+// BlockSize : ブロックの大きさのバイト数
 template <typename T, size_t BlockSize = 4096>
 class CMemPool final
 {
@@ -38,13 +38,13 @@ public:
 
 	CMemPool()
 	{
-		// �n�߂̃u���b�N���������m��
+		// 始めのブロックをメモリ確保
 		AllocateBlock();
 	}
 
 	~CMemPool()
 	{
-		// �������m�ۂ����̈�̘A�����X�g��H���đS�Ẵu���b�N���̃��������
+		// メモリ確保した領域の連結リストを辿って全てのブロック分のメモリ解放
 		Node* curr = m_currentBlock;
 		while (curr) {
 			Node* next = curr->next;
@@ -53,7 +53,7 @@ public:
 		}
 	}
 
-	// �v�f�\�z�A�����͗v�f�^�̃R���X�g���N�^����
+	// 要素構築、引数は要素型のコンストラクタ引数
 	template <typename... Args>
 	T* Construct(Args&&... args)
 	{
@@ -62,7 +62,7 @@ public:
 		return p;
 	}
 	
-	// �v�f�j���A������ Construct ���Ԃ����|�C���^
+	// 要素破棄、引数は Construct が返したポインタ
 	void Destruct(T* p)
 	{
 		if (p) {
@@ -72,63 +72,63 @@ public:
 	}
 
 private:
-	// ���p�̂��g�����ŗv�f�^�Ǝ��ȎQ�Ɨp�̃|�C���^�𓯂��̈�Ɋ��蓖�Ă�
-	// ���p�̂̃T�C�Y�͊e�����o���i�[�ł���T�C�Y�ɂȂ鎖�𗘗p����
+	// 共用体を使う事で要素型と自己参照用のポインタを同じ領域に割り当てる
+	// 共用体のサイズは各メンバを格納できるサイズになる事を利用する
 	union Node {
 		~Node() {}
-		T element;	// �v�f�^
-		Node* next; // �u���b�N�̃w�b�_�̏ꍇ�́A���̃u���b�N�Ɍq����
-					// �����̖������̈�̏ꍇ�͎��̖������̈�Ɍq����
+		T element;	// 要素型
+		Node* next; // ブロックのヘッダの場合は、次のブロックに繋がる
+					// 解放後の未割当領域の場合は次の未割当領域に繋がる
 	};
 	
-	// �u���b�N�̑傫���͍Œ� Node 2���ȏ�Ƃ���
+	// ブロックの大きさは最低 Node 2個分以上とする
 	static_assert(BlockSize >= 2 * sizeof(Node), "BlockSize too small.");
 
-	// �v�f�̃������m�ۏ����A�v�f�̗̈�̃|�C���^��Ԃ�
+	// 要素のメモリ確保処理、要素の領域のポインタを返す
 	T* Allocate()
 	{
-		// �������m�ێ��ɂ͖������̈悩��g�p���Ă���
+		// メモリ確保時には未割当領域から使用していく
 		if (m_unassignedNode) {
 			T* ret = reinterpret_cast<T*>(m_unassignedNode);
 			m_unassignedNode = m_unassignedNode->next;
 			return ret;
 		}
 		else {
-			// �������̈悪�����ꍇ�́A�u���b�N�̒�����؂�o��
-			// ���݂̃u���b�N��Node�T�C�Y���̗̈悪�����ꍇ�͐V�K�̃u���b�N���m��
+			// 未割当領域が無い場合は、ブロックの中から切り出す
+			// 現在のブロックにNodeサイズ分の領域が無い場合は新規のブロックを確保
 			Node* border = reinterpret_cast<Node*>(reinterpret_cast<char*>(m_currentBlock) + BlockSize - sizeof(Node) + 1);
 			if (m_currentNode >= border) {
 				AllocateBlock();
 			}
-			// �v�f�̗̈�̃|�C���^��Ԃ��Ɠ����Ƀ|�C���^�����ɐi�߂Đ؂�o���ʒu���X�V����
+			// 要素の領域のポインタを返すと同時にポインタを次に進めて切り出す位置を更新する
 			return reinterpret_cast<T*>(m_currentNode++);
 		}
 	}
 
-	// ��������������A�v�f�̗̈�̃|�C���^���󂯎��
-	// Allocate ���\�b�h�ŕԂ����|�C���^��n����
+	// メモリ解放処理、要素の領域のポインタを受け取る
+	// Allocate メソッドで返したポインタを渡す事
 	void Free(T* p)
 	{
 		if (p) {
-			// ��������������̈�𖢊����̈�Ƃ��Ď��ȎQ�Ƌ��p�̂̕Е����A�����X�g�Ōq����
-			// ����̃������m�ێ��ɂ��̗̈���ė��p����
+			// メモリ解放した領域を未割当領域として自己参照共用体の片方向連結リストで繋げる
+			// 次回のメモリ確保時にその領域を再利用する
 			Node* next = m_unassignedNode;
 			m_unassignedNode = reinterpret_cast<Node*>(p);
 			m_unassignedNode->next = next;
 		}
 	}
 
-	// �Ăяo���̓x�Ƀ������̓��I�m�ۂ��ׂ����s�����������ׂɁA�ꊇ�Ńu���b�N�̈���m��
-	// �u���b�N�̐擪(head)�ɂ̓u���b�N�̘A���p�̃|�C���^���z�u����A�c��̈�ibody�j�ɂ͗v�f���L�^�����
+	// 呼び出しの度にメモリの動的確保を細かく行う事を避ける為に、一括でブロック領域を確保
+	// ブロックの先頭(head)にはブロックの連結用のポインタが配置され、残る領域（body）には要素が記録される
 	void AllocateBlock()
 	{
 		char* buff = reinterpret_cast<char*>(operator new (BlockSize));
 		Node* next = m_currentBlock;
-		// �u���b�N�̈�̐擪�ihead�j��Node�̃|�C���^�Ƃ��Ĉ����A�ȑO�ɍ쐬�����u���b�N�ɘA������
+		// ブロック領域の先頭（head）はNodeのポインタとして扱い、以前に作成したブロックに連結する
 		m_currentBlock = reinterpret_cast<Node*>(buff);
 		m_currentBlock->next = next;
 
-		// �u���b�N�̈�̎c�镔���͗v�f�̗̈�Ƃ��邪�A�A���C�����g�����
+		// ブロック領域の残る部分は要素の領域とするが、アライメントを取る
 		void* body = buff + sizeof(Node*);
 		size_t space = BlockSize - sizeof(Node*);
 		body = std::align(alignof(Node), sizeof(Node), body, space);
@@ -136,9 +136,9 @@ private:
 		m_currentNode = reinterpret_cast<Node*>(body);
 	}
 
-	Node* m_unassignedNode = nullptr; // �������̈�̐擪
-	Node* m_currentBlock = nullptr; // ���݂̃u���b�N
-	Node* m_currentNode = nullptr; // �v�f�m�ۏ������Ɍ��݂̃u���b�N�̒�����؂�o��Node���w���|�C���^�A�������m�ێ��ɖ������̈悪�����ꍇ�͂������g��
+	Node* m_unassignedNode = nullptr; // 未割当領域の先頭
+	Node* m_currentBlock = nullptr; // 現在のブロック
+	Node* m_currentNode = nullptr; // 要素確保処理時に現在のブロックの中から切り出すNodeを指すポインタ、メモリ確保時に未割当領域が無い場合はここを使う
 };
 
 #endif /* SAKURA_CMEMPOOL_H_ */
