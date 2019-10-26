305ea042651a8f5cf298fb23c71aee621d420f1c
berryplus <berryzplus@gmail.com>
2019-10-26 13:37:36 +0900

    EditInfo初期化にメンバーイニシャライザーを使う

--- sakura_core/EditInfo.h
+++ sakura_core/EditInfo.h
@@ -72,25 +72,25 @@ struct EditInfo {
 	
 	// Mar. 7, 2002 genta
 	// Constructor 確実に初期化するため
-	EditInfo()
-	: m_nCharCode( CODE_AUTODETECT )
-	, m_bBom( false )
-	, m_nTypeId( -1 )
-	, m_nViewTopLine( -1 )
-	, m_nViewLeftCol( -1 )
-	, m_ptCursor(CLogicInt(-1), CLogicInt(-1))
-	, m_bIsModified( false )
-	, m_bIsGrep( false )
-	, m_szGrepKey{ 0 }
-	, m_bIsDebug( false )
-	, m_nWindowSizeX( -1 )
-	, m_nWindowSizeY( -1 )
-	, m_nWindowOriginX( CW_USEDEFAULT )	//	2004.05.13 Moca “指定無し”を-1からCW_USEDEFAULTに変更
-	, m_nWindowOriginY( CW_USEDEFAULT )
+	EditInfo() noexcept
+		: m_szPath{ 0 }
+		, m_nCharCode( CODE_AUTODETECT )
+		, m_bBom( false )
+		, m_szDocType{ 0 }
+		, m_nTypeId( -1 )
+		, m_nViewTopLine( -1 )
+		, m_nViewLeftCol( -1 )
+		, m_ptCursor{ -1, -1 }
+		, m_bIsModified( false )
+		, m_bIsGrep( false )
+		, m_szGrepKey{ 0 }
+		, m_bIsDebug( false )
+		, m_szMarkLines{ 0 }
+		, m_nWindowSizeX( -1 )
+		, m_nWindowSizeY( -1 )
+		, m_nWindowOriginX( CW_USEDEFAULT )	//	2004.05.13 Moca “指定無し”を-1からCW_USEDEFAULTに変更
+		, m_nWindowOriginY( CW_USEDEFAULT )
 	{
-		m_szPath[0] = '\0';
-		m_szMarkLines[0] = L'\0';
-		m_szDocType[0] = '\0';
 	}
 	bool operator == (const EditInfo& rhs) const noexcept {
 		if (this == &rhs) return true;
