c7b3022ab25de0fd76ca180f9c4bc79ece7da016
berryplus <berryzplus@gmail.com>
2019-11-17 23:15:31 +0900

    等価比較演算子をstd::wstring_viewを使って実装

--- sakura_core/print/CPrint.h
+++ sakura_core/print/CPrint.h
@@ -65,9 +65,9 @@ struct	MYDEVMODE {
 	bool operator == (const MYDEVMODE& rhs) const noexcept {
 		if (this == &rhs) return true;
 		return m_bPrinterNotFound == rhs.m_bPrinterNotFound
-			&& 0 == wcsncmp(m_szPrinterDriverName, rhs.m_szPrinterDriverName, _countof(m_szPrinterDriverName))
-			&& 0 == wcsncmp(m_szPrinterDeviceName, rhs.m_szPrinterDeviceName, _countof(m_szPrinterDeviceName))
-			&& 0 == wcsncmp(m_szPrinterOutputName, rhs.m_szPrinterOutputName, _countof(m_szPrinterOutputName))
+			&& std::wstring_view(m_szPrinterDriverName) == rhs.m_szPrinterDriverName
+			&& std::wstring_view(m_szPrinterDeviceName) == rhs.m_szPrinterDeviceName
+			&& std::wstring_view(m_szPrinterOutputName) == rhs.m_szPrinterOutputName
 			&& dmFields == rhs.dmFields
 			&& dmOrientation == rhs.dmOrientation
 			&& dmPaperSize == rhs.dmPaperSize
@@ -82,7 +82,7 @@ struct	MYDEVMODE {
 			&& dmYResolution == rhs.dmYResolution
 			&& dmTTOption == rhs.dmTTOption
 			&& dmCollate == rhs.dmCollate
-			&& 0 == wcsncmp(dmFormName, rhs.dmFormName, _countof(dmFormName))
+			&& std::wstring_view(dmFormName) == rhs.dmFormName
 			&& dmLogPixels == rhs.dmLogPixels
 			&& dmBitsPerPel == rhs.dmBitsPerPel
 			&& dmPelsWidth == rhs.dmPelsWidth
