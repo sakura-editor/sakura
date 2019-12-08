4bd9984849c514198dc396d3a8caedc6f39d58e3
Katsuhisa Yuasa <berupon@gmail.com>
2019-12-09 02:29:58 +0900

    N_SHAREDATA_VERSION を更新

--- sakura_core/config/system_constants.h
+++ sakura_core/config/system_constants.h
@@ -541,12 +541,15 @@
 	Version 175:
 	Vistaスタイルのファイルダイアログ（CommonSetting_Edit::m_bVistaStyleFileDialog）を追加
 
+	Version 176:
+	MYDEVMODE の未使用メンバを削除した為、PRINTSETTING や DLLSHAREDATA のメモリレイアウトが変更
+
 	-- 統合されたので元に戻す（1000～1023が使用済み） 	2008.11.16 nasukoji
 	-- Version 1000:
 	-- バージョン1000以降を本家統合までの間、使わせてください。かなり頻繁に構成が変更されると思われるので。by kobake 2008.03.02
 
 */
-#define N_SHAREDATA_VERSION		175
+#define N_SHAREDATA_VERSION		176
 #define STR_SHAREDATA_VERSION	NUM_TO_STR(N_SHAREDATA_VERSION)
 #define	GSTR_SHAREDATA	(L"SakuraShareData" _T(CON_SKR_MACHINE_SUFFIX_) _T(_CODE_SUFFIX_) _T(_DEBUG_SUFFIX_) _T(STR_SHAREDATA_VERSION))
 
