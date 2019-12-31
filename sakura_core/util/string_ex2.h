86ecd00acb666d51a9fe33d7403cd74bc2b10337
berryplus <berryzplus@gmail.com>
2019-12-31 15:37:03 +0900

    int64_tの最小値を扱えない不具合を修正

--- sakura_core/util/string_ex2.h
+++ sakura_core/util/string_ex2.h
@@ -163,14 +163,18 @@ size_t int2num(
 	// 定数：文字列表現に使う文字
 	constexpr char numChars[] = "0123456789ABCDEF";
 
+	size_t digits = maxCount;
+
+//デバッグ時のみ、バッファ容量が足りているかチェックする
+#ifdef _DEBUG
+
 	//数値を文字列化するのに必要な文字数を求める
-	size_t digits = countDigits<nRadix>(value);
+	digits = countDigits<nRadix>(value);
 
 	// 格納可能な文字数を超える場合 0 を返却して終了する
 	if (maxCount <= digits) return 0;
 
-	// 符号を確認する
-	const bool negative = (value < 0);
+#endif //ifdef _DEBUG
 
 	// 下位桁から変換するのでリバースイテレータを使う
 	auto rbegin = std::reverse_iterator(destBuf + digits + 1);
@@ -178,15 +182,21 @@ size_t int2num(
 
 	*it++ = '\0'; // NUL終端する
 
-	// 文字列変換を始める前に絶対値をとって正数にしておく
-	for (auto v = std::abs(value); v != 0; ++it) {
-		auto w = v % nRadix;
-		*it = numChars[w];
+	// 1桁目だけ別処理する(int64_tのminをabsすると桁溢れするから)
+	{
+		auto w = value % nRadix;
+		*it = numChars[w < 0 ? ~w + 1 : w]; //絶対値を取ってる
+		++it;
+	}
+
+	// 2桁目を処理する前に絶対値に変換し、あとは順番に変換していく
+	for (auto v = std::abs(value / static_cast<NumType>(nRadix)); v != 0; ++it) {
+		*it = numChars[v % nRadix];
 		v /= nRadix;
 	}
 
 	// 符号を処理する
-	if (negative) *it++ = '-';
+	if (value < 0) *it++ = '-';
 
 	// 文字数を返却する
 	return digits;
