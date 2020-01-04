a85667b4d66e73fb9909995e53cfb45eae145685
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-01-04 20:21:47 +0900

    入力ファイルをあらかじめソートしておく。

--- tools/ToolBarTools/ToolBarImageMuxer/Program.cs
+++ tools/ToolBarTools/ToolBarImageMuxer/Program.cs
@@ -4,9 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Collections;
+using System.Text.RegularExpressions;
 
 namespace ToolBarImageMuxer
 {
+    public class BmpFileComparer : IComparer<string>
+    {
+        static Regex regex = new Regex(@"(\d+)\.bmp", RegexOptions.Compiled);
+    
+        public int Compare(string x, string y)
+        {
+            var matchX = regex.Match(x);
+            var matchY = regex.Match(y);
+
+            if (matchX.Success && matchY.Success)
+            {
+                var indexX = int.Parse(matchX.Groups[1].Value);
+                var indexY = int.Parse(matchY.Groups[1].Value);
+
+                return indexX.CompareTo(indexY);
+            }
+            return x.CompareTo(y);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -18,6 +40,9 @@ namespace ToolBarImageMuxer
             var inputDir = args[0];
             var outfile = args[1];
             var files = Directory.GetFiles(inputDir, "*.bmp", SearchOption.TopDirectoryOnly);
+
+            var compare = new BmpFileComparer();
+            Array.Sort<string>(files, compare);
 
             BmpMuxer.Mux(files, outfile, 33);
         }
