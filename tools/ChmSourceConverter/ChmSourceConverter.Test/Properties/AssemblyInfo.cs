53a5f6f74fbfccdd7564500a065f20aa54d984a0
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-01-18 06:46:53 +0900

    Copyright を 2020 にする

--- tools/ChmSourceConverter/ChmSourceConverter.Test/Properties/AssemblyInfo.cs
+++ tools/ChmSourceConverter/ChmSourceConverter.Test/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UnitTestProject1")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2019-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
