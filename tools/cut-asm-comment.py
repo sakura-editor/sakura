68b318a891e1db555a3dd17e0a30c2b8fdd496bf
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-01-26 10:29:21 +0900

    ASM のコメント削除用のスクリプト

---
+++ tools/cut-asm-comment.py
@@ -0,0 +1,26 @@
+﻿# -*- coding: utf-8 -*-
+import os
+import sys
+import re
+import codecs
+
+def cutcomment(fileName):
+	if fileName.endswith('.asm'):
+		print (fileName)
+		tmp_file = fileName + ".tmp"
+		with codecs.open(tmp_file, "w", "utf_8_sig") as fout:
+			with codecs.open(fileName, "r", "utf_8_sig") as fin:
+				for line in fin:
+					line = re.sub(r'\s*;.*$', '', line)
+					fout.write(line)
+		os.remove(fileName)
+		os.rename(tmp_file, fileName)
+
+def processFiles(topdir):
+	for workdir, dis, files in os.walk(topdir):
+		for file in files:
+			fullPath = os.path.join(workdir, file)
+			cutcomment(fullPath)
+
+if __name__ == '__main__':
+	processFiles('.')
