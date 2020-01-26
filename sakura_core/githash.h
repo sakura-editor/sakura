b839ed554159a615df6823decd3acca292b12247
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-01-26 18:47:54 +0900

    (Debug用) githash.h を固定

---
+++ sakura_core/githash.h
@@ -0,0 +1,24 @@
+/*! @file */
+#pragma once
+#define GIT_SHORT_COMMIT_HASH "d6e5f24bd"
+#define GIT_COMMIT_HASH "d6e5f24bdcd0a66222febf17f0f494982f4233a5"
+// TEMP_GIT_SHORT_COMMIT_HASH is not defined
+// TEMP_GIT_COMMIT_HASH is not defined
+#define GIT_REMOTE_ORIGIN_URL "https://github.com/sakura-editor/sakura.git"
+// GIT_TAG_NAME is not defined
+// APPVEYOR_URL is not defined
+// APPVEYOR_REPO_NAME is not defined
+#define APPVEYOR_DEV_VERSION
+// APPVEYOR_ACCOUNT_NAME is not defined
+// APPVEYOR_PROJECT_SLUG is not defined
+// APPVEYOR_BUILD_VERSION is not defined
+// APPVEYOR_BUILD_NUMBER     is not defined
+// APPVEYOR_BUILD_NUMBER_INT is not defined
+// APPVEYOR_BUILD_NUMBER_LABEL is not defined
+// APPVEYOR_PULL_REQUEST_NUMBER     is not defined
+// APPVEYOR_PULL_REQUEST_NUMBER_INT is not defined
+// APPVEYOR_PR_NUMBER_LABEL         is not defined
+// GITHUB_COMMIT_URL is not defined
+// GITHUB_COMMIT_URL_PR_HEAD is not defined
+// APPVEYOR_SHORTHASH_PR_HEAD is not defined
+// APPVEYOR_BUILD_URL is not defined
