68ddcd57aba6cec1db5bda1737b1ff4ef7db347c
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-02-01 08:47:09 +0900

    (h) Edit.FormatDocument で整形 (sakura_core/mfclike)

--- sakura_core/mfclike/CMyWnd.h
+++ sakura_core/mfclike/CMyWnd.h
@@ -30,51 +30,68 @@
 	2008.01.26 kobake 作成
 */
 
-class CMyWnd{
-public:
-	CMyWnd() : m_hWnd(NULL) { }
+class CMyWnd
+{
+  public:
+    CMyWnd()
+        : m_hWnd(NULL)
+    {
+    }
 
-	void SetHwnd(HWND hwnd){ m_hWnd = hwnd; }
-	HWND GetHwnd() const{ return m_hWnd; }
-	HWND GetSafeHwnd() const{ return this?m_hWnd:NULL; }
-	void InvalidateRect(LPCRECT lpRect, BOOL bErase = TRUE){ ::InvalidateRect(m_hWnd, lpRect, bErase); }
-	int ScrollWindowEx(int dx, int dy, const RECT* prcScroll, const RECT* prcClip, HRGN hrgnUpdate, RECT* prcUpdate, UINT uFlags)
-	{
-		return ::ScrollWindowEx(m_hWnd, dx, dy, prcScroll, prcClip, hrgnUpdate, prcUpdate, uFlags);
-	}
-	HDC GetDC() const
-	{
-		return ::GetDC(m_hWnd);
-	}
-	int ReleaseDC(HDC hdc) const
-	{
-		return ::ReleaseDC(m_hWnd,hdc);
-	}
-	HWND GetAncestor(UINT gaFlags) const
-	{
-		return ::GetAncestor(m_hWnd,gaFlags);
-	}
-	BOOL CreateCaret(HBITMAP hBitmap, int nWidth, int nHeight)
-	{
-		return ::CreateCaret(m_hWnd, hBitmap, nWidth, nHeight);
-	}
-	BOOL ClientToScreen(LPPOINT lpPoint) const
-	{
-		return ::ClientToScreen(m_hWnd, lpPoint);
-	}
+    void SetHwnd(HWND hwnd)
+    {
+        m_hWnd = hwnd;
+    }
+    HWND GetHwnd() const
+    {
+        return m_hWnd;
+    }
+    HWND GetSafeHwnd() const
+    {
+        return this ? m_hWnd : NULL;
+    }
+    void InvalidateRect(LPCRECT lpRect, BOOL bErase = TRUE)
+    {
+        ::InvalidateRect(m_hWnd, lpRect, bErase);
+    }
+    int ScrollWindowEx(int dx, int dy, const RECT *prcScroll, const RECT *prcClip, HRGN hrgnUpdate, RECT *prcUpdate, UINT uFlags)
+    {
+        return ::ScrollWindowEx(m_hWnd, dx, dy, prcScroll, prcClip, hrgnUpdate, prcUpdate, uFlags);
+    }
+    HDC GetDC() const
+    {
+        return ::GetDC(m_hWnd);
+    }
+    int ReleaseDC(HDC hdc) const
+    {
+        return ::ReleaseDC(m_hWnd, hdc);
+    }
+    HWND GetAncestor(UINT gaFlags) const
+    {
+        return ::GetAncestor(m_hWnd, gaFlags);
+    }
+    BOOL CreateCaret(HBITMAP hBitmap, int nWidth, int nHeight)
+    {
+        return ::CreateCaret(m_hWnd, hBitmap, nWidth, nHeight);
+    }
+    BOOL ClientToScreen(LPPOINT lpPoint) const
+    {
+        return ::ClientToScreen(m_hWnd, lpPoint);
+    }
 
-	BOOL UpdateWindow()
-	{
-		return ::UpdateWindow(m_hWnd);
-	}
-	HWND SetFocus()
-	{
-		return ::SetFocus(m_hWnd);
-	}
-	BOOL GetClientRect(LPRECT lpRect) const
-	{
-		return ::GetClientRect(m_hWnd, lpRect);
-	}
-private:
-	HWND m_hWnd;
+    BOOL UpdateWindow()
+    {
+        return ::UpdateWindow(m_hWnd);
+    }
+    HWND SetFocus()
+    {
+        return ::SetFocus(m_hWnd);
+    }
+    BOOL GetClientRect(LPRECT lpRect) const
+    {
+        return ::GetClientRect(m_hWnd, lpRect);
+    }
+
+  private:
+    HWND m_hWnd;
 };
