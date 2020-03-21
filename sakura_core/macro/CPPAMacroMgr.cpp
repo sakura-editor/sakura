a660381e8264a388f071d55f66e536dd4c83cddb
berryplus <berryzplus@gmail.com>
2020-03-22 01:00:32 +0900

    コピー代入のメソッド名を書き間違っているのを訂正
    CNativeW::SetNativeData(const CNativeW& rhs) (コピー)
    CNativeW::SetString(const WCHAR* rhs) (コピー)

--- sakura_core/macro/CPPAMacroMgr.cpp
+++ sakura_core/macro/CPPAMacroMgr.cpp
@@ -73,7 +73,7 @@ BOOL CPPAMacroMgr::LoadKeyMacro( HINSTANCE hInstance, const WCHAR* pszPath )
 */
 BOOL CPPAMacroMgr::LoadKeyMacroStr( HINSTANCE hInstance, const WCHAR* pszCode )
 {
-	m_cBuffer.SetNativeData( pszCode );	//	m_cBufferにコピー
+	m_cBuffer.SetString( pszCode );	//	m_cBufferにコピー
 
 	m_nReady = true;
 	return TRUE;
