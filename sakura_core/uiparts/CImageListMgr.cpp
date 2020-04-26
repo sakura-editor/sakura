427d66c5fe915efd701449163775ce20e8a8bf43
berryplus <berryzplus@gmail.com>
2020-04-26 23:04:31 +0900

    ユーザー指定ビットマップの縦横比制約を外せないか検討

--- sakura_core/uiparts/CImageListMgr.cpp
+++ sakura_core/uiparts/CImageListMgr.cpp
@@ -659,11 +659,14 @@ HBITMAP CImageListMgr::ResizeToolIcons(
 
 	// DIBセクションからサイズを取得する
 	int cx = di.dsBm.bmWidth / cols;
-	int cy = di.dsBm.bmHeight / rows;
-	if ( cx != cy ) {
-		DEBUG_TRACE( L"tool bitmap size is unexpected." );
-		return NULL;
-	}
+	//int cy = di.dsBm.bmHeight / rows;
+	//if ( cx != cy ) {
+	//	DEBUG_TRACE( L"tool bitmap size is unexpected." );
+	//	return NULL;
+	//}
+	// 縦のサイズは動的に判定するようにしてみる
+	int cy = cx;
+	rows = di.dsBm.bmHeight / cy;
 
 	const int cxSmIcon = ::GetSystemMetrics( SM_CXSMICON );
 	const int cySmIcon = ::GetSystemMetrics( SM_CYSMICON );
