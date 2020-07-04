fbe47c72e554fb504b72dcee0e4ffe8f862a492a
berryzplus <berryzplus@gmail.com>
2020-07-05 00:49:00 +0900

    tests/build-and-test.batの修正
    このバッチを叩けばビルド＆テストが実行できるように修正。

--- tests/build-and-test.bat
+++ tests/build-and-test.bat
@@ -2,6 +2,23 @@ set platform=%1
 set configuration=%2
 set ERROR_RESULT=0
 
+if "%platform%" == "MinGW" (
+	goto :RUN_CREATE_PROJECT
+)
+
+@echo ---- start build-sln.bat ----
+pushd "%~dp0.."
+call  build-sln.bat %platform% %configuration% || set ERROR_RESULT=1
+if errorlevel 1 (
+	@echo ERROR in build-sln.bat %errorlevel%
+	popd
+	exit /b 1
+)
+popd
+@echo ---- end   build-sln.bat ----
+goto :RUN_TESTS
+
+:RUN_CREATE_PROJECT
 @echo ---- start create-project.bat ----
 call "%~dp0create-project.bat" %platform% %configuration%
 if errorlevel 1 (
@@ -18,6 +35,7 @@ if errorlevel 1 (
 )
 @echo ---- end   build-project.bat ----
 
+:RUN_TESTS
 @echo ---- start run-tests.bat ----
 call "%~dp0run-tests.bat" %platform% %configuration%
 if errorlevel 1 (
