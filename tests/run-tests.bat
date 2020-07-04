9c868b92bc25fc0dbb512e813f60b44dc89ba40a
berryzplus <berryzplus@gmail.com>
2020-07-04 23:05:37 +0900

    tests/run-tests.batの修正
    ・BUILDDIRの生成規則を変えてしまったのを反映してなかったので反映。
    ・テスト実行が失敗した際のエラーを検出できていないのを修正。
    ・テスト結果とは別に、モジュールに含まれるテスト一覧を出力しているのが無駄なので削除。

--- tests/run-tests.bat
+++ tests/run-tests.bat
@@ -8,15 +8,12 @@ if defined APPVEYOR (
 	set FILTER_BAT=%~dp0test_result_filter_tell_AppVeyor.bat
 )
 
-set BUILDDIR=build\%platform%
-set BINARY_DIR=%~dp0%BUILDDIR%\unittests\%platform%\%configuration%
+set BUILDDIR=build\%platform%\%configuration%
+set BINARY_DIR=%~dp0%BUILDDIR%\unittests
 
 pushd %BINARY_DIR%
 for /r %%i in (tests*.exe) do (
 	set TEST_LAUNCHED=1
-
-	@echo %%i --gtest_list_tests
-	%%i --gtest_list_tests || set ERROR_RESULT=1
 
 	call :RunTest %%i %%i-googletest-%platform%-%configuration%.xml || set ERROR_RESULT=1
 )
@@ -28,7 +25,7 @@ if "%TEST_LAUNCHED%" == "0" (
 )
 
 if "%ERROR_RESULT%" == "1" (
-	@echo ERROR
+	@echo ERROR in some test(s^).
 	exit /b 1
 )
 exit /b 0
@@ -39,9 +36,9 @@ set XMLPATH=%2
 
 if defined FILTER_BAT (
 	@echo %EXEPATH% --gtest_output=xml:%XMLPATH% ^| "%FILTER_BAT%"
-	      %EXEPATH% --gtest_output=xml:%XMLPATH%  | "%FILTER_BAT%" || set TEMP_RESULT=1
+	      %EXEPATH% --gtest_output=xml:%XMLPATH%  | "%FILTER_BAT%"
 ) else (
 	@echo %EXEPATH% --gtest_output=xml:%XMLPATH%
-	      %EXEPATH% --gtest_output=xml:%XMLPATH%                   || set TEMP_RESULT=1
+	      %EXEPATH% --gtest_output=xml:%XMLPATH%
 )
-exit /b %TEMP_RESULT%
+exit /b %errorlevel%
