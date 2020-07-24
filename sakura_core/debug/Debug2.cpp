bd88c9c46e78a7b5a93c9d5cddd47a5e5e9958d6
berryzplus <berryzplus@gmail.com>
2020-07-24 19:49:31 +0900

    Debug2のリファクタリング

--- sakura_core/debug/Debug2.cpp
+++ sakura_core/debug/Debug2.cpp
@@ -2,41 +2,12 @@
 #include "StdAfx.h"
 #include "debug/Debug2.h"
 
-//2007.08.30 kobake 追加
-
-#ifdef _DEBUG
-//!デバッグメッセージ出力
-void debug_output(const char* str, ...)
-{
-	char buf[_MAX_PATH+150];
-	va_list mark;
-	va_start(mark,str);
-	// FILE名, LINE 式 分必要
-	tchar_vsnprintf_s(buf,_countof(buf),str,mark);
-	va_end(mark);
-
-	//デバッガに出力
-	OutputDebugStringA(buf);
-}
-
-//!強制終了
 void debug_exit()
 {
-	MessageBox(NULL,L"assertとかに引っ掛かったぽいです",GSTR_APPNAME,MB_OK);
-	exit(1);
-}
-
-void debug_exit2(const char* file, int line, const char* exp)
-{
-	char szBuffer[1024];
-	wsprintfA(szBuffer, "assert\n%s(%d):\n%s", file, line, exp);
-	MessageBoxA(NULL, szBuffer , "sakura", MB_OK);
-	exit(1);
+	::exit( 1 );
 }
 
 void warning_point()
 {
-	int n;
-	n=0; //※←ここにブレークポイントを設けておくと、任意ワーニングでブレークできる
+	::DebugBreak();
 }
-#endif	// _DEBUG
