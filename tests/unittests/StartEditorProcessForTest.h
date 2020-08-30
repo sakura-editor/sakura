9da897d596af01a8870d936655c0a40c9f1ff7a8
berryzplus <berryzplus@gmail.com>
2020-08-30 14:41:08 +0900

    サクラエディタの起動テストを追加する
    1. tests1.exeをサクラエディタとして起動できるようにする
    2. tests1.exeを使ったテストで影響が出る箇所を修正する
    3. WinMainTest - runWithNoWin追加(コントロールプロセスの起動テスト)
    4. WinMainTest - runEditorProcess追加(エディタプロセスの起動テスト)

---
+++ tests/unittests/StartEditorProcessForTest.h
@@ -0,0 +1,48 @@
+﻿/*! @file */
+/*
+	Copyright (C) 2018-2020 Sakura Editor Organization
+
+	This software is provided 'as-is', without any express or implied
+	warranty. In no event will the authors be held liable for any damages
+	arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any purpose,
+	including commercial applications, and to alter it and redistribute it
+	freely, subject to the following restrictions:
+
+		1. The origin of this software must not be misrepresented;
+		   you must not claim that you wrote the original software.
+		   If you use this software in a product, an acknowledgment
+		   in the product documentation would be appreciated but is
+		   not required.
+
+		2. Altered source versions must be plainly marked as such,
+		   and must not be misrepresented as being the original software.
+
+		3. This notice may not be removed or altered from any source
+		   distribution.
+*/
+#pragma once
+
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif /* #ifndef NOMINMAX */
+
+#include <tchar.h>
+#include <Windows.h>
+
+#include <string_view>
+
+/*!
+ * テストコード専用wWinMain呼出のラッパー関数
+ *
+ * 単体テストから wWinMain を呼び出すためのラッパー関数です。
+ *
+ * wWinMain は呼出元のグローバル変数を汚してしまうため、
+ * ASSERT_EXIT, ASSERT_DEATH などを使って別プロセスで実行するようにしてください。
+ *
+ * この関数をコントロールプロセスの起動に使用しないでください。
+ * googletestでは、ASSERT_EXITで起動したプロセスの完全な終了を待機できないようです。
+ * コントロールプロセスが終了する前に他のテストが実行されると期待した動作にならない場合があります。
+ */
+int StartEditorProcessForTest( const std::wstring_view& strCommandLine );
