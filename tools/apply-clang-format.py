fc532907fad1d9a2f97a9a362fc302881d632493
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-09-06 09:57:16 +0900

    clang-format を実行するスクリプトを追加

---
+++ tools/apply-clang-format.py
@@ -0,0 +1,19 @@
+﻿# -*- coding: utf-8 -*-
+import os
+import sys
+import re
+import codecs
+
+clang_format = r'"C:\Program Files\LLVM\bin\clang-format.exe"'
+
+def processFiles(topdir):
+	for workdir, dis, files in os.walk(topdir):
+		for file in files:
+			fullPath = os.path.join(workdir, file)
+			if fullPath.endswith('.cpp') or fullPath.endswith('.h'):
+				command = clang_format + ' -style=file -i ' + fullPath
+				print(command)
+				os.system(command)
+
+if __name__ == '__main__':
+	processFiles('.')
