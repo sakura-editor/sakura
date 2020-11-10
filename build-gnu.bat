1f5afac22e0c39406c57cb85f41c696bd9ea674c
D.Miwa <suconbu@gmail.com>
2020-11-11 00:28:13 +0900

    リソースファイルのUTF-8変換処理をMakefileへ移動

--- build-gnu.bat
+++ build-gnu.bat
@@ -28,21 +28,12 @@ if "%configuration%" == "Release" (
 @rem path=C:\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev1\mingw64\bin;%path%
 path=C:\msys64\mingw64\bin;%path%
 
-@rem Preparation for resource compile (windres.exe cannot recognize utf-16)
-set rc_dir=sakura_core\_rc_utf8
-if not exist %rc_dir% mkdir %rc_dir%
-for %%i in (sakura_core\*.rc?) do (
-	C:\msys64\usr\bin\iconv -f utf-16 -t utf-8 %%i > %rc_dir%\%%~nxi%
-)
-
 @echo mingw32-make -C sakura_core MYDEFINES="%MYDEFINES%" MYCFLAGS="%MYCFLAGS%" MYLIBS="%MYLIBS%"
 mingw32-make -C sakura_core MYDEFINES="%MYDEFINES%" MYCFLAGS="%MYCFLAGS%" MYLIBS="%MYLIBS%" -j4
 if errorlevel 1 (
 	echo error 2 errorlevel %errorlevel%
 	exit /b 1
 )
-
-rmdir /s /q %rc_dir%
 
 exit /b 0
 
