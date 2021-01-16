66146d19df22289b043c593a55875daf28b9f0b2
berryzplus <berryzplus@gmail.com>
2021-01-16 20:02:01 +0900

    カバレッジ除外パスの条件を緩和する

--- tests/unittests/coverage.cpp
+++ tests/unittests/coverage.cpp
@@ -27,6 +27,6 @@
 // Exclude all the code from a particular files:
 // see https://docs.microsoft.com/ja-jp/visualstudio/test/using-code-coverage-to-determine-how-much-code-is-being-tested
 ExcludeSourceFromCodeCoverage(Exclusion1, L"*\\tests\\unittests\\*");
-ExcludeSourceFromCodeCoverage(Exclusion2, L"*\\tests\\googletest\\googletest\\*");
-ExcludeSourceFromCodeCoverage(Exclusion3, L"*\\Windows Kits\\10\\Include\\*\\winrt\\wrl\\client.h");
+ExcludeSourceFromCodeCoverage(Exclusion2, L"*\\googletest\\*");
+ExcludeSourceFromCodeCoverage(Exclusion3, L"*\\Windows Kits\\10\\Include\\*");
 ExcludeSourceFromCodeCoverage(Exclusion4, L"*\\VC\\Tools\\MSVC\\*\\include\\*");
