e689610c3a26dfb034ed765477f8c35b45400aea
Kengo Ide <kasumikagari@gmail.com>
2021-04-19 15:08:50 +0900

    変換前に reserve する

--- sakura_core/convert/CConvert_TabToSpace.cpp
+++ sakura_core/convert/CConvert_TabToSpace.cpp
@@ -24,6 +24,9 @@
 */
 #include "StdAfx.h"
 #include "CConvert_TabToSpace.h"
+
+#include <algorithm>
+#include <string_view>
 #include "charset/charcode.h"
 #include "CEol.h"
 #include "util/string_ex2.h"
@@ -32,20 +35,24 @@
 //! TAB→空白
 bool CConvert_TabToSpace::DoConvert(CNativeW* pcData)
 {
+	const std::wstring_view source(pcData->GetStringPtr(), pcData->GetStringLength());
+	const std::ptrdiff_t numOfTabs = std::count(source.begin(), source.end(), L'\t');
 	std::wstring buffer;
+	buffer.reserve(source.length() + numOfTabs * (m_nTabWidth - 1));
+
 	int begin = 0;
 	while (true) {
 		int lineLength;
 		CEol eol;
 		/* CRLFで区切られる「行」を返す。CRLFは行長に加えない */
-		const wchar_t* line = GetNextLineW(pcData->GetStringPtr(),
-			pcData->GetStringLength(), &lineLength, &begin, &eol, m_bExtEol);
+		const wchar_t* line = GetNextLineW(source.data(),
+			source.length(), &lineLength, &begin, &eol, m_bExtEol);
 		if (!line)
 			break;
 		// 先頭行については開始桁位置を考慮する（さらに折り返し関連の対策が必要？）
-		int pos = (pcData->GetStringPtr() == line) ? m_nStartColumn : 0;
+		int pos = (source.data() == line) ? m_nStartColumn : 0;
 		for (int i = 0; i < lineLength; ++i) {
-			if (line[i] == WCODE::TAB) {
+			if (line[i] == L'\t') {
 				const int width = m_nTabWidth - (pos % m_nTabWidth);
 				buffer.append(width, L' ');
 				pos += width;
