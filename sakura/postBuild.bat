32e7b9fb8fea2addb80802c8f6d9ac7374586082
berryzplus <berryzplus@gmail.com>
2021-05-09 19:35:38 +0900

    MSVCプロジェクトにbregonigとctagsのzip展開を組み込む
    postBuild.batに実装されていた展開処理をプロジェクトに組み込む。
    build-installer.batにライセンスファイルの展開処理を復元する。

--- sakura/postBuild.bat
+++ sakura/postBuild.bat
@@ -20,42 +20,4 @@ set CONFIGURATION=%2
 set DEST_DIR=%PLATFORM%\%CONFIGURATION%
 set OUT_DIR=%~dp0..\%DEST_DIR%
 
-: ---- bregonig.dll ---- :
-set BREGONIG_DLL=bregonig.dll
-set BRON_ZIP=..\installer\externals\bregonig\bron420.zip
-set BRON_TMP=..\installer\temp\bron
-if "%platform%" == "x64" (
-	set BRON_DIR=%~dp0%BRON_TMP%\x64
-) else (
-	set BRON_DIR=%~dp0%BRON_TMP%
-)
-if not exist "%BRON_DIR%\%BREGONIG_DLL%" (
-	call "%UNZIP_CMD%" "%~dp0%BRON_ZIP%" "%~dp0%BRON_TMP%" > NUL || (echo error && exit /b 1)
-)
-if not exist "%OUT_DIR%\%BREGONIG_DLL%" (
-	@echo %BRON_ZIP% -^> %DEST_DIR%\%BREGONIG_DLL%
-	copy /Y /B "%BRON_DIR%\%BREGONIG_DLL%" "%OUT_DIR%\" > NUL
-)
-
-: ---- ctags.exe ---- :
-set CTAGS_EXE=ctags.exe
-if "%PLATFORM%" == "Win32" (
-	set CTAGS_PREFIX=x86
-) else if "%PLATFORM%" == "x64" (
-	set CTAGS_PREFIX=x64
-) else (
-	echo unknown PLATFORM %PLATFORM%
-	exit /b 1
-)
-set CTAGS_ZIP=..\installer\externals\universal-ctags\ctags-2020-01-12_feffe43a-%CTAGS_PREFIX%.zip
-set CTAGS_TMP=..\installer\temp\ctags
-set CTAGS_DIR=%~dp0%CTAGS_TMP%
-if not exist "%CTAGS_DIR%\%CTAGS_EXE%" (
-	call "%UNZIP_CMD%" "%~dp0%CTAGS_ZIP%" "%CTAGS_DIR%" > NUL || (echo error && exit /b 1)
-)
-if not exist "%OUT_DIR%\%CTAGS_EXE%" (
-	@echo %CTAGS_ZIP% -^> %DEST_DIR%\%CTAGS_EXE%
-	copy /Y /B "%CTAGS_DIR%\%CTAGS_EXE%" "%OUT_DIR%\" > NUL
-)
-
 ENDLOCAL
