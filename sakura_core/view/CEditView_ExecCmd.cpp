242f7853570a8837a0b3f72fb26b2927b33c4b05
katsuhisa yuasa <berupon@gmail.com>
2021-08-29 20:00:17 +0900

    外部コマンド実行でキャンセルした時のメッセージが異なる問題を修正

--- sakura_core/view/CEditView_ExecCmd.cpp
+++ sakura_core/view/CEditView_ExecCmd.cpp
@@ -570,7 +570,7 @@ user_cancel:
 		if( bCancelEnd && bOutputExtInfo ){
 			//	2006.12.03 maru アウトプットウィンドウにのみ出力
 			//最後にテキストを追加
-			oa.OutputW( LS(STR_EDITVIEW_EXECCMD_STOP) );
+			oa.OutputW( CLoadString().LoadStringW(STR_EDITVIEW_EXECCMD_STOP) );
 		}
 		
 		{
