978a9a5e26d92b1cf17cb521a4ec57adb8419f34
Kohki Akikaze <67105596+kazasaku@users.noreply.github.com>
2022-01-02 10:28:04 +0900

    ApiWrap::List_GetText()のテストを追加

--- tests/unittests/test-StdControl.cpp
+++ tests/unittests/test-StdControl.cpp
@@ -48,3 +48,35 @@ TEST(StdControl, Wnd_GetText2)
 	DestroyWindow(hwnd);
 	ASSERT_STREQ(s.c_str(), text);
 }
+
+TEST(StdControl, List_GetText) {
+	std::wstring text = L"0123456789abcdef";
+	HWND list = ::CreateWindow(L"LISTBOX", nullptr, 0, 1, 1, 1, 1, nullptr, nullptr, nullptr, nullptr);
+	ApiWrap::List_AddString(list, text.c_str());
+	ApiWrap::List_AddString(list, L"");
+
+	std::wstring result1;
+	ASSERT_TRUE(ApiWrap::List_GetText(list, 0, result1));
+	ASSERT_EQ(result1, text);
+
+	result1.clear();
+	ASSERT_TRUE(ApiWrap::List_GetText(list, 1, result1));
+	ASSERT_TRUE(result1.empty());
+
+	ASSERT_FALSE(ApiWrap::List_GetText(list, 2, result1));
+
+	wchar_t result2[15]{};
+	ASSERT_EQ(ApiWrap::List_GetText(list, 0, result2), LB_ERRSPACE);
+
+	wchar_t result3[16]{};
+	ASSERT_EQ(ApiWrap::List_GetText(list, 0, result3), LB_ERRSPACE);
+
+	wchar_t result4[17]{};
+	ASSERT_EQ(ApiWrap::List_GetText(list, 0, result4), text.length());
+	ASSERT_STREQ(result4, text.c_str());
+
+	result4[0] = L'\0';
+	ASSERT_EQ(ApiWrap::List_GetText(list, 2, result4), LB_ERR);
+
+	::DestroyWindow(list);
+}
