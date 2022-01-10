018ef0ad0acd6c03bc1792c7473aa323679cfc7b
berryzplus <berryzplus@gmail.com>
2022-01-10 22:09:51 +0900

    find-tools.batからCMD_CPPCHECKを削る

--- tools/find-tools.bat
+++ tools/find-tools.bat
@@ -7,7 +7,6 @@ if "%1" equ "clear" (
     set CMD_7Z=
     set CMD_HHC=
     set CMD_ISCC=
-    set CMD_CPPCHECK=
     set CMD_DOXYGEN=
     set CMD_VSWHERE=
     set CMD_MSBUILD=
@@ -33,7 +32,6 @@ if not defined CMD_GIT      call :Git      2> nul
 if not defined CMD_7Z       call :7z       2> nul
 if not defined CMD_HHC      call :hhc      2> nul
 if not defined CMD_ISCC     call :iscc     2> nul
-if not defined CMD_CPPCHECK call :cppcheck 2> nul
 if not defined CMD_DOXYGEN  call :doxygen  2> nul
 if not defined CMD_VSWHERE  call :vswhere  2> nul
 if not defined CMD_MSBUILD  call :msbuild  2> nul
@@ -45,7 +43,6 @@ echo ^|- CMD_GIT=%CMD_GIT%
 echo ^|- CMD_7Z=%CMD_7Z%
 echo ^|- CMD_HHC=%CMD_HHC%
 echo ^|- CMD_ISCC=%CMD_ISCC%
-echo ^|- CMD_CPPCHECK=%CMD_CPPCHECK%
 echo ^|- CMD_DOXYGEN=%CMD_DOXYGEN%
 echo ^|- CMD_VSWHERE=%CMD_VSWHERE%
 echo ^|- CMD_MSBUILD=%CMD_MSBUILD%
@@ -59,7 +56,6 @@ endlocal ^
     && set "CMD_7Z=%CMD_7Z%"                    ^
     && set "CMD_HHC=%CMD_HHC%"                  ^
     && set "CMD_ISCC=%CMD_ISCC%"                ^
-    && set "CMD_CPPCHECK=%CMD_CPPCHECK%"        ^
     && set "CMD_DOXYGEN=%CMD_DOXYGEN%"          ^
     && set "CMD_VSWHERE=%CMD_VSWHERE%"          ^
     && set "CMD_MSBUILD=%CMD_MSBUILD%"          ^
@@ -117,15 +113,6 @@ set APPDIR=Inno Setup 6
 set PATH2=%PATH%;%ProgramFiles%\%APPDIR%\;%ProgramFiles(x86)%\%APPDIR%\;%ProgramW6432%\%APPDIR%\;
 for /f "usebackq delims=" %%a in (`where $PATH2:ISCC.exe`) do ( 
     set "CMD_ISCC=%%a"
-    exit /b
-)
-exit /b
-
-:cppcheck
-set APPDIR=cppcheck
-set PATH2=%PATH%;%ProgramFiles%\%APPDIR%\;%ProgramFiles(x86)%\%APPDIR%\;%ProgramW6432%\%APPDIR%\;
-for /f "usebackq delims=" %%a in (`where $PATH2:cppcheck.exe`) do ( 
-    set "CMD_CPPCHECK=%%a"
     exit /b
 )
 exit /b
