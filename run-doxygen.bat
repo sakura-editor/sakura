9874561ab46841a68dcacd489a53b4d1929e6925
berryzplus <berryzplus@gmail.com>
2022-01-16 22:34:58 +0900

    find-tools.batからCMD_DOXYGENを削る

--- run-doxygen.bat
+++ run-doxygen.bat
@@ -1,5 +1,5 @@
 call sakura\githash.bat "%~dp0sakura_core"
-call tools\find-tools.bat
+if not defined CMD_DOXYGEN call :doxygen
 
 if exist html rmdir /s /q html
 
@@ -7,10 +7,18 @@ set PROJECT_NUMBER=%GIT_SHORT_COMMIT_HASH%
 set HHC_LOCATION=%CMD_HHC%
 set DOXYGEN_LOG=doxygen-%platform%-%configuration%.log
 
-if "%CMD_DOXYGEN%" == "" (
+if not exist "%CMD_DOXYGEN%" (
 	echo doxygen was not found
-) else if exist "%CMD_DOXYGEN%" (
-	"%CMD_DOXYGEN%" doxygen.conf > %DOXYGEN_LOG%
-) else (
-	echo doxygen was not found
+	exit /b
 )
+"%CMD_DOXYGEN%" doxygen.conf > %DOXYGEN_LOG%
+exit /b
+
+:doxygen
+set APPDIR=doxygen\bin
+set PATH2=%PATH%;%ProgramFiles%\%APPDIR%\;%ProgramFiles(x86)%\%APPDIR%\;%ProgramW6432%\%APPDIR%\;
+for /f "usebackq delims=" %%a in (`where $PATH2:doxygen.exe`) do ( 
+    set "CMD_DOXYGEN=%%a"
+    exit /b
+)
+exit /b
