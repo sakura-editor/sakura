f4fbe9fae66a5f6442287a0410a93629eef31282
berryzplus <berryzplus@gmail.com>
2022-02-28 22:30:17 +0900

    表示しようとしたメッセージを取得できるようにする
    元から仕組みがあったがgtestで使えないので改良。
    gtestではワイド文字列を扱えないのでUTF8で書き出して読み出した文字列をUTF16LEに戻してから評価する。

---
+++ tests/unittests/eval_outputs.hpp
@@ -0,0 +1,33 @@
+﻿/*! @file */
+/*
+	Copyright (C) 2022, Sakura Editor Organization
+
+	This software is provided 'as-is', without any express or implied
+	warranty. In no event will the authors be held liable for any damages
+	arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any purpose,
+	including commercial applications, and to alter it and redistribute it
+	freely, subject to the following restrictions:
+
+		1. The origin of this software must not be misrepresented;
+		   you must not claim that you wrote the original software.
+		   If you use this software in a product, an acknowledgment
+		   in the product documentation would be appreciated but is
+		   not required.
+
+		2. Altered source versions must be plainly marked as such,
+		   and must not be misrepresented as being the original software.
+
+		3. This notice may not be removed or altered from any source
+		   distribution.
+*/
+#include <gtest/gtest.h>
+
+#include "util/string_ex.h"
+
+// 標準エラー出力に吐き出されたメッセージを評価します
+#define EXPECT_ERROUT(statementExpression, expected) \
+	testing::internal::CaptureStderr(); \
+	statementExpression; \
+	EXPECT_STREQ(strprintf(L"%s\n", expected).data(), u8stowcs(testing::internal::GetCapturedStderr()).data())
