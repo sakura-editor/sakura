5fa1001c2613dd5b95575f7ccd51fa3438fbd15b
berryzplus <berryzplus@gmail.com>
2022-03-02 19:00:27 +0900

    PPA.DLLの実装部を外部化する

---
+++ sakura_core/extmodule/CPpaDllImpl.cpp
@@ -0,0 +1,258 @@
+﻿/*!	@file */
+/*
+	Copyright (C) 2022, Sakura Editor Organization
+
+	This software is provided 'as-is', without any express or implied
+	warranty. In no event will the authors be held liable for any damages
+	arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any purpose, 
+	including commercial applications, and to alter it and redistribute it 
+	freely, subject to the following restrictions:
+
+		1. The origin of this software must not be misrepresented;
+		   you must not claim that you wrote the original software.
+		   If you use this software in a product, an acknowledgment
+		   in the product documentation would be appreciated but is
+		   not required.
+
+		2. Altered source versions must be plainly marked as such, 
+		   and must not be misrepresented as being the original software.
+
+		3. This notice may not be removed or altered from any source
+		   distribution.
+*/
+
+#include "StdAfx.h"
+#include "extmodule/CPpaDllImpl.h"
+
+/*!
+	DLLの初期化
+
+	関数のアドレスを取得してメンバに保管する．
+
+	@retval true 成功
+	@retval false アドレス取得に失敗
+*/
+bool CPpaDllImpl::InitDllImp()
+{
+	const ImportTable table[] = 
+	{
+		{ &m_pfnExecute,		"Execute" },
+		{ &m_pfnSetDeclare,		"SetDeclare" },
+		{ &m_pfnSetSource,		"SetSource" },
+		{ &m_pfnSetDefProc,		"SetDefProc" },
+		{ &m_pfnSetDefine,		"SetDefine" },
+		{ &m_pfnSetIntFunc,		"SetIntFunc" },
+		{ &m_pfnSetStrFunc,		"SetStrFunc" },
+		{ &m_pfnSetProc,		"SetProc" },
+		{ &m_pfnSetErrProc,		"SetErrProc" },
+		{ &m_pfnAbort,			"ppaAbort" },
+		{ &m_pfnGetVersion,		"GetVersion" },
+		{ &m_pfnDeleteVar,		"DeleteVar" },
+		{ &m_pfnGetArgInt,		"GetArgInt" },
+		{ &m_pfnGetArgStr,		"GetArgStr" },
+		{ &m_pfnGetArgBStr,		"GetArgBStr" },
+		{ &m_pfnGetIntVar,		"GetIntVar" },
+		{ &m_pfnGetStrVar,		"GetStrVar" },
+		{ &m_pfnGetBStrVar,		"GetBStrVar" },
+		{ &m_pfnSetIntVar,		"SetIntVar" },
+		{ &m_pfnSetStrVar,		"SetStrVar" },
+		{ &m_pfnAddIntObj,		"AddIntObj" },
+		{ &m_pfnAddStrObj,		"AddStrObj" },
+		{ &m_pfnAddIntVar,		"AddIntVar" },
+		{ &m_pfnAddStrVar,		"AddStrVar" },
+		{ &m_pfnSetIntObj,		"SetIntObj" },
+		{ &m_pfnSetStrObj,		"SetStrObj" },
+		{ &m_pfnAddRealVar,		"AddRealVar" },
+		{ &m_pfnSetRealObj,		"SetRealObj" },
+		{ &m_pfnAddRealObj,		"AddRealObj" },
+		{ &m_pfnGetRealVar,		"GetRealVar" },
+		{ &m_pfnSetRealVar,		"SetRealVar" },
+		{ &m_pfnSetRealFunc,	"SetRealFunc" },
+		{ &m_pfnGetArgReal,		"GetArgReal" },
+		{ &m_pfnIsRunning,		"IsRunning" },
+		{ &m_pfnSetFinishProc,	"SetFinishProc"},
+		{ NULL, 0 }
+	};
+
+	return RegisterEntries(table);
+}
+
+LPCWSTR CPpaDllImpl::GetDllNameImp(int nIndex)
+{
+	return L"PPA.DLL";
+}
+
+void CPpaDllImpl::Execute() const
+{
+	m_pfnExecute();
+}
+
+void CPpaDllImpl::SetSource(const char* ss) const
+{
+	m_pfnSetSource(ss);
+}
+
+void CPpaDllImpl::SetDeclare(const char* ss) const
+{
+	m_pfnSetDeclare(ss);
+}
+
+void CPpaDllImpl::SetDefProc(const char* ss) const
+{
+	m_pfnSetDefProc(ss);
+}
+
+void CPpaDllImpl::SetDefine(const char* ss) const
+{
+	m_pfnSetDefine(ss);
+}
+
+void CPpaDllImpl::AddIntVar(const char* lpszDef, int nVal, int nCnst) const
+{
+	m_pfnAddIntVar(lpszDef, nVal, nCnst);
+}
+
+void CPpaDllImpl::AddStrVar(const char* lpszDef, const char* lpszVal, int nCnst) const
+{
+	m_pfnAddStrVar(lpszDef, lpszVal, nCnst);
+}
+
+void CPpaDllImpl::SetIntFunc(void* proc) const
+{
+	m_pfnSetIntFunc(proc);
+}
+
+void CPpaDllImpl::SetStrFunc(void* proc) const
+{
+	m_pfnSetStrFunc(proc);
+}
+
+void CPpaDllImpl::SetProc(void* proc) const
+{
+	m_pfnSetProc(proc);
+}
+
+void CPpaDllImpl::SetErrProc(void* proc) const
+{
+	m_pfnSetErrProc(proc);
+}
+
+void CPpaDllImpl::Abort() const
+{
+	m_pfnAbort();
+}
+
+int CPpaDllImpl::GetVersion() const
+{
+	return m_pfnGetVersion();
+}
+
+void CPpaDllImpl::DeleteVar(const char* ss) const
+{
+	m_pfnDeleteVar(ss);
+}
+
+int CPpaDllImpl::GetArgInt(int index) const
+{
+	return m_pfnGetArgInt(index);
+}
+
+char* CPpaDllImpl::GetArgStr(int index) const
+{
+	return m_pfnGetArgStr(index);
+}
+
+char* CPpaDllImpl::GetArgBStr(int index) const
+{
+	return m_pfnGetArgBStr(index);
+}
+
+void CPpaDllImpl::SetStrObj(void* proc) const
+{
+	m_pfnSetStrObj(proc);
+}
+
+void CPpaDllImpl::SetIntObj(void* proc) const
+{
+	m_pfnSetIntObj(proc);
+}
+
+void CPpaDllImpl::AddIntObj(const char* ss, int def, BOOL read, int index) const
+{
+	m_pfnAddIntObj(ss, def, read, index);
+}
+
+void CPpaDllImpl::AddStrObj(const char* ss, const char* def, BOOL read, int index) const
+{
+	m_pfnAddStrObj(ss, def, read, index);
+}
+
+int CPpaDllImpl::GetIntVar(const char* ss) const
+{
+	return m_pfnGetIntVar(ss);
+}
+
+char* CPpaDllImpl::GetStrVar(const char* ss) const
+{
+	return m_pfnGetStrVar(ss);
+}
+
+char* CPpaDllImpl::GetBStrVar(const char* ss) const
+{
+	return m_pfnGetBStrVar(ss);
+}
+
+BOOL CPpaDllImpl::SetIntVar(const char* ss, int val) const
+{
+	return m_pfnSetIntVar(ss, val);
+}
+
+BOOL CPpaDllImpl::SetStrVar(const char* ss, const char* val) const
+{
+	return m_pfnSetStrVar(ss, val);
+}
+
+void CPpaDllImpl::AddRealVar(const char* ss, double val, BOOL cnst) const
+{
+	m_pfnAddRealVar(ss, val, cnst);
+}
+
+void CPpaDllImpl::SetRealObj(void* proc) const
+{
+	m_pfnSetRealObj(proc);
+}
+void CPpaDllImpl::AddRealObj(const char* ss, double val, BOOL read, LONG index) const
+{
+	m_pfnAddRealObj(ss, val, read, index);
+}
+double CPpaDllImpl::GetRealVar(const char* ss) const
+{
+	return m_pfnGetRealVar(ss);
+}
+
+BOOL CPpaDllImpl::SetRealVar(const char* ss, double val) const
+{
+	return m_pfnSetRealVar(ss, val);
+}
+
+void CPpaDllImpl::SetRealFunc(void* proc) const
+{
+	m_pfnSetRealFunc(proc);
+}
+
+DWORD CPpaDllImpl::GetArgReal(int index) const
+{
+	return m_pfnGetArgReal(index);
+}
+
+BOOL CPpaDllImpl::IsRunning() const
+{
+	return m_pfnIsRunning();
+}
+
+void CPpaDllImpl::SetFinishProc(void* proc) const
+{
+	m_pfnSetFinishProc(proc);
+}
---
+++ sakura_core/extmodule/CPpaDllImpl.h
@@ -0,0 +1,112 @@
+﻿/*!	@file */
+/*
+	Copyright (C) 2022, Sakura Editor Organization
+
+	This software is provided 'as-is', without any express or implied
+	warranty. In no event will the authors be held liable for any damages
+	arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any purpose, 
+	including commercial applications, and to alter it and redistribute it 
+	freely, subject to the following restrictions:
+
+		1. The origin of this software must not be misrepresented;
+		   you must not claim that you wrote the original software.
+		   If you use this software in a product, an acknowledgment
+		   in the product documentation would be appreciated but is
+		   not required.
+
+		2. Altered source versions must be plainly marked as such, 
+		   and must not be misrepresented as being the original software.
+
+		3. This notice may not be removed or altered from any source
+		   distribution.
+*/
+#pragma once
+
+#include <Windows.h>
+
+#include "extmodule/CDllHandler.h"
+
+class CPpaDllImpl : public CDllImp {
+private:
+	void	(WINAPI *m_pfnExecute)();
+	void	(WINAPI* m_pfnSetSource)(const char* ss);
+	void	(WINAPI* m_pfnSetDeclare)(const char* ss);
+	void	(WINAPI* m_pfnSetDefProc)(const char* ss);
+	void	(WINAPI* m_pfnSetDefine)(const char* ss);
+	void	(WINAPI* m_pfnAddIntVar)(const char*, int, int);
+	void	(WINAPI* m_pfnAddStrVar)(const char*, const char*, int);
+	void	(WINAPI *m_pfnSetIntFunc)(void* p);
+	void	(WINAPI *m_pfnSetStrFunc)(void* p);
+	void	(WINAPI *m_pfnSetProc)(void* p);
+	void	(WINAPI *m_pfnSetErrProc)(void* p);
+	void	(WINAPI *m_pfnAbort)();
+	int		(WINAPI *m_pfnGetVersion)();
+	void	(WINAPI *m_pfnDeleteVar)(const char*);
+	int		(WINAPI *m_pfnGetArgInt)(int);
+	char*	(WINAPI *m_pfnGetArgStr)(int);
+	char*	(WINAPI *m_pfnGetArgBStr)(int);
+	void	(WINAPI *m_pfnSetStrObj)(void* proc);
+	void	(WINAPI *m_pfnSetIntObj)(void* proc);
+	void	(WINAPI *m_pfnAddIntObj)(const char*, int, BOOL, int);
+	void	(WINAPI *m_pfnAddStrObj)(const char*, const char*, BOOL, int);
+	int		(WINAPI *m_pfnGetIntVar)(const char* ss);
+	char*	(WINAPI *m_pfnGetStrVar)(const char* ss);
+	char*	(WINAPI *m_pfnGetBStrVar)(const char* ss);
+	BOOL	(WINAPI *m_pfnSetIntVar)(const char*, int);
+	BOOL	(WINAPI *m_pfnSetStrVar)(const char*, const char*);
+	void	(WINAPI *m_pfnAddRealVar)(const char*, double, BOOL);
+	void	(WINAPI *m_pfnSetRealObj)(void* p);
+	void	(WINAPI *m_pfnAddRealObj)(const char*, double, BOOL, LONG);
+	double	(WINAPI *m_pfnGetRealVar)(const char*);
+	BOOL	(WINAPI *m_pfnSetRealVar)(const char*, double);
+	void	(WINAPI *m_pfnSetRealFunc)(void* p);
+	DWORD	(WINAPI *m_pfnGetArgReal)(int);
+	BYTE	(WINAPI *m_pfnIsRunning)();
+	void	(WINAPI *m_pfnSetFinishProc)(void* p);	//	2003.06.01 Moca
+
+protected:
+	bool InitDllImp() override;
+
+	void	Execute() const;
+	void	SetSource(const char* ss) const;
+	void	SetDeclare(const char* ss) const;
+	void	SetDefProc(const char* ss) const;
+	void	SetDefine(const char* ss) const;
+	void	AddIntVar(const char* lpszDef, int nVal, int nCnst) const;
+	void	AddStrVar(const char* lpszDef, const char* lpszVal, int nCnst) const;
+	void	SetIntFunc(void* proc) const;
+	void	SetStrFunc(void* proc) const;
+	void	SetProc(void* proc) const;
+	void	SetErrProc(void* proc) const;
+	void	Abort() const;
+	int		GetVersion() const;
+	void	DeleteVar(const char* ss) const;
+	int		GetArgInt(int index) const;
+	char*	GetArgStr(int index) const;
+	char*	GetArgBStr(int index) const;
+	void	SetStrObj(void* proc) const;
+	void	SetIntObj(void* proc) const;
+	void	AddIntObj(const char* ss, int def, BOOL read, int index) const;
+	void	AddStrObj(const char* ss, const char* def, BOOL read, int index) const;
+	int		GetIntVar(const char* ss) const;
+	char*	GetStrVar(const char* ss) const;
+	char*	GetBStrVar(const char* ss) const;
+	BOOL	SetIntVar(const char* ss, int val) const;
+	BOOL	SetStrVar(const char* ss, const char* val) const;
+
+	void	AddRealVar(const char* ss, double val, BOOL cnst) const;
+	void	SetRealObj(void* proc) const;
+	void	AddRealObj(const char* ss, double val, BOOL read, LONG index) const;
+	double	GetRealVar(const char* ss) const;
+	BOOL	SetRealVar(const char* ss, double val) const;
+	void	SetRealFunc(void* proc) const;
+	DWORD	GetArgReal(int index) const;
+
+	BOOL	IsRunning() const;
+	void	SetFinishProc(void* proc) const;
+
+public:
+	LPCWSTR	GetDllNameImp(int nIndex) override;
+};
