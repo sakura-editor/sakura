c84d7b7d41b614d5d44935082088a5e9f94ab9c7
berryzplus <berryzplus@gmail.com>
2022-03-06 23:32:04 +0900

    MinGWビルドがうまく行かない問題の対処

--- build-gnu.bat
+++ build-gnu.bat
@@ -79,7 +79,7 @@ if errorlevel 1 (
 @rem build "ppa_stub".
 set PPA_BUILD_DIR=%~dp0build\%platform%\%configuration%\ppa_stub\
 mkdir "%PPA_BUILD_DIR%" > NUL 2>&1
-"%~dp0tests\stubs\stub.build.cmd" "%~dp0tests\stubs\ppa_stub\CMakeLists.txt" "%PPA_BUILD_DIR%" "%OUTDIR%"
+"%~dp0tests\stubs\stub.build.cmd" "%~dp0tests\stubs\ppa_stub\CMakeLists.txt" "%PPA_BUILD_DIR%\CMakeCache.txt" "%OUTDIR%\ppa_stub.dll"
 
 @rem build "tests1".
 set TESTS1_MAKEFILE=%~dp0tests\unittests\Makefile
