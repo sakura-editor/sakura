33d207f65f95e497994ead8c2f8f7564b4b9552c
berryzplus <berryzplus@gmail.com>
2022-10-14 21:11:51 +0900

    無駄なif分岐を削除(CDlgTagJumpList::GetFullPathFromDepth)
    if文のtrueパートとfalseパートが全く同じ無駄な分岐を削除する。
    これ自体バグの可能性もあるが、内容の検証は行わない。

--- sakura_core/dlg/CDlgTagJumpList.cpp
+++ sakura_core/dlg/CDlgTagJumpList.cpp
@@ -1633,14 +1633,7 @@ WCHAR* CDlgTagJumpList::GetFullPathFromDepth( WCHAR* pszOutput, int count,
 	DEBUG_TRACE( L"depth %d\n",  depth );
 	//完全パス名を作成する。
 	const WCHAR	*p = fileName;
-	if( p[0] == L'\\' ){	//ドライブなし絶対パスか？
-		if( p[1] == L'\\' ){	//ネットワークパスか？
-			wcscpy( pszOutput, p );	//何も加工しない。
-		}else{
-			//ドライブ加工したほうがよい？
-			wcscpy( pszOutput, p );	//何も加工しない。
-		}
-	}else if( _istalpha( p[0] ) && p[1] == L':' ){	//絶対パスか？
+	if( p[0] == L'\\' || ( _istalpha( p[0] ) && p[1] == L':' ) ){	//絶対パスか？
 		wcscpy( pszOutput, p );	//何も加工しない。
 	}else{
 		for( int i = 0; i < depth; i++ ){
