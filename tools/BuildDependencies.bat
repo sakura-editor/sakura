1d3d2aa6ae465be81b6b9bcbf7bca186b9ed27f3
berryzplus <berryzplus@gmail.com>
2024-07-21 17:23:25 +0900

    vcpkgを更新する
    vcpkgをマニフェストモードに切り替える
    googletestの独自ビルドをやめる

--- tools/BuildDependencies.bat
+++ tools/BuildDependencies.bat
@@ -1,54 +1,20 @@
 @echo off
 setlocal
 
-pushd "%~dp0vcpkg"
-
-IF not EXIST vcpkg.exe (
-	CALL bootstrap-vcpkg.bat
+if not defined CMD_GIT call "%~dp0find-tools.bat"
+if not defined CMD_GIT (
+    @echo NOTE: No git command
+    exit /b 1
 )
 
-: For tests1
-vcpkg install ^
-    gtest:x64-windows-static ^
-    gtest:x86-windows-static
-
-vcpkg export ^
-    gtest:x64-windows-static ^
-    gtest:x86-windows-static ^
-	--nuget --nuget-id=tests1.dependencies --nuget-version=1.0.0 --output-dir="%~dp0..\.nuget"
-
-if not defined CMD_NUGET call :nuget
-if defined CMD_NUGET (
-    "%CMD_NUGET%" install tests1.dependencies -Source "%~dp0..\.nuget" -OutputDirectory "%~dp0..\packages"
+IF not exist "%~dp0vcpkg\bootstrap-vcpkg.bat" (
+	git submodule update --init
 )
 
-: For sakura
-mkdir ports\header-make
-copy /Y ..\HeaderMake\portfile.cmake ports\header-make\
-copy /Y ..\HeaderMake\vcpkg.json     ports\header-make\
-
-vcpkg install ^
-    header-make:x64-windows ^
-    header-make:x86-windows
-
-vcpkg export ^
-    header-make:x64-windows ^
-    header-make:x86-windows ^
-	--nuget --nuget-id=sakura.dependencies --nuget-version=1.0.0 --output-dir="%~dp0..\.nuget"
-
-"%CMD_NUGET%" install sakura.dependencies -Source "%~dp0..\.nuget" -OutputDirectory "%~dp0..\packages"
-
-popd
-
-exit /b
-
-:nuget
-for /f "usebackq delims=" %%a in (`dir nuget.exe /b /s`) do ( 
-    set "CMD_NUGET=%%a"
-    exit /b
+IF not exist "%~dp0vcpkg\vcpkg.exe" (
+    chcp 65001 >NUL 
+	CALL "%~dp0vcpkg\bootstrap-vcpkg.bat"
 )
-for /f "usebackq delims=" %%a in (`where nuget.exe`) do ( 
-    set "CMD_NUGET=%%a"
-    exit /b
-)
-exit /b
+
+: install command
+: "%~dp0vcpkg\vcpkg.exe" install --x-wait-for-lock --triplet "x64-windows-static" "--x-manifest-root=%~dp0.." "--x-install-root=%~dp0..\build\vcpkg_installed"
