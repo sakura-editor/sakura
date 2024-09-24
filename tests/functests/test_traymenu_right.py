bc566723bb3fd6a037aff1ef3d3490c642db3803
berryzplus <berryzplus@gmail.com>
2024-09-24 23:10:46 +0900

    メニューをアクセスキーで選択するテストを追加

--- tests/functests/test_traymenu_right.py
+++ tests/functests/test_traymenu_right.py
@@ -1,12 +1,12 @@
 ﻿#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import ctypes
 import os
 import time
 
 from appium.webdriver.common.appiumby import AppiumBy
 from appium.webdriver.webdriver import WebDriver
 from appium.webdriver.webelement import WebElement
+from helper import set_foreground_window
 from helper import wait_for_context_menu
 from helper import wait_for_dialog
 from helper import cancel_element
@@ -25,7 +25,7 @@ def test_show_type_list(root_driver, desktop):
     F_TYPE_LIST: Final = '31110'
     context_menu = wait_for_context_menu(desktop,  F_TYPE_LIST)
     context_menu.click()
-    WebDriverWait(desktop, 15).until_not(EC.visibility_of_element_located((By.ID, context_menu.id)))
+    WebDriverWait(desktop, 120).until_not(EC.visibility_of_element_located((By.ID, context_menu.id)))
 
     # 「タイプ別設定一覧」ダイアログが表示されるまで待つ
     type_list = wait_for_dialog(desktop, 'タイプ別設定一覧')
@@ -47,7 +47,7 @@ def test_show_common_prop(root_driver, desktop):
     F_OPTION: Final = '31112'
     context_menu = wait_for_context_menu(desktop,  F_OPTION)
     context_menu.click()
-    WebDriverWait(desktop, 15).until_not(EC.visibility_of_element_located((By.ID, context_menu.id)))
+    WebDriverWait(desktop, 120).until_not(EC.visibility_of_element_located((By.ID, context_menu.id)))
 
     # 「共通設定」ダイアログが表示されるまで待つ
     common_prop = wait_for_dialog(desktop, '共通設定')
@@ -62,7 +62,7 @@ def test_show_about_dialog(root_driver, desktop):
     F_ABOUT: Final = '31455'
     context_menu = wait_for_context_menu(desktop,  F_ABOUT)
     context_menu.click()
-    WebDriverWait(desktop, 15).until_not(EC.visibility_of_element_located((By.ID, context_menu.id)))
+    WebDriverWait(desktop, 120).until_not(EC.visibility_of_element_located((By.ID, context_menu.id)))
 
     # 「バージョン情報」ダイアログが表示されるまで待つ
     about_dialog = wait_for_dialog(desktop, 'バージョン情報')
