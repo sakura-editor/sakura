96af681fcc6c125b6c8bd7f15d54af05f1ce250e
gorogoro123 <897627+gorogoro123@users.noreply.github.com>
2025-02-19 14:32:29 +0900

    Remove _T() macro

--- sakura_core/debug/Debug1.h
+++ sakura_core/debug/Debug1.h
@@ -58,7 +58,7 @@ void DebugOutW( LPCWSTR lpFmt, ...);
 
 //トレース出力（トレース箇所のファイルパスと行番号を出力してエラー解析を容易にする目的）
 #ifdef _DEBUG
-	#define TRACE( format, ... )	DEBUG_TRACE( _T("%hs(%d): ") _T(format) _T("\n"), __FILE__, __LINE__, __VA_ARGS__ )
+	#define TRACE( format, ... )	DEBUG_TRACE( L"%hs(%d): " TEXT(format) L"\n", __FILE__, __LINE__, __VA_ARGS__ )
 #else
 	#define TRACE( ... )
 #endif
