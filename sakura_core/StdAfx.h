77b79135f81ec19488e77a257b2ad8fcc52cc50e
berryzplus <berryzplus@gmail.com>
2025-06-04 20:04:11 +0900

    VCランタイムのヘッダーを利用する

--- sakura_core/StdAfx.h
+++ sakura_core/StdAfx.h
@@ -104,6 +104,11 @@
 #include <wrl.h>
 #include <wtypes.h>
 
+// VC++ランタイムのヘッダー
+#include <comdef.h>
+#include <comutil.h>
+#include <crtdbg.h>
+
 // プロジェクト内のファイルだがプリコンパイル対象とする。
 // プリコンパイルの有無がビルドパフォーマンスに大きく影響するため。
 #include "env/DLLSHAREDATA.h"
@@ -113,6 +118,120 @@ using namespace std::literals::string_literals;
 
 // 文字列参照リテラルのサッフィクス L""sv を有効にします
 using namespace std::literals::string_view_literals;
+
+// COMスマートポインタの定義用マクロ
+#if _MSC_VER
+#define DEFINE_COM_SMARTPTR(Interface) _COM_SMARTPTR_TYPEDEF(Interface, __uuidof(Interface))
+#else
+
+/*!
+ * MSVCのコンパイラCOM対応クラス _com_ptr_t を模倣するテンプレート
+ * 
+ * 必要最小限の機能のみ実装する
+ */
+template<typename Interface>
+class TComPtr {
+private:
+	using Me = TComPtr<Interface>;
+
+    Microsoft::WRL::ComPtr<Interface> m_pInterface = nullptr;
+
+public:
+	TComPtr() = default;
+    ~TComPtr() = default;
+
+	TComPtr(std::nullptr_t) noexcept {}
+
+	template<typename U>
+	TComPtr(_In_opt_ U* other) {
+		*this = other;
+	}
+
+	/*!
+	 * インターフェースを作成する
+	 */
+    HRESULT CreateInstance(
+		const CLSID& rclsid,
+		IUnknown* pOuter = nullptr,
+		DWORD dwClsContext = CLSCTX_INPROC_SERVER
+	) noexcept
+    {
+		// 以前のインターフェースを解放する
+		m_pInterface.Reset();
+
+		// SDK関数を使ってインスタンスを作成する
+		Microsoft::WRL::ComPtr<IUnknown> pUnknown;
+		_com_util::CheckError(CoCreateInstance(rclsid, pOuter, dwClsContext, IID_PPV_ARGS(&pUnknown)));
+
+		// 生成したIUnknownから希望のインターフェースを取得する
+		_com_util::CheckError(pUnknown.As(&m_pInterface));
+
+		return S_OK;
+    }
+
+    Interface* GetInterfacePtr() const noexcept
+    {
+        return m_pInterface.Get();
+    }
+
+    Interface& GetInterfaceRef() const
+    {
+        if (m_pInterface)
+        {
+            return *GetInterfacePtr();
+        }
+
+		_com_raise_error(E_POINTER);
+    }
+
+	Me& operator = (std::nullptr_t) noexcept {
+		m_pInterface.Reset();
+		return *this;
+	}
+
+	template<typename U>
+	Me& operator = (_In_opt_ U* other) {
+		if (!other) {
+			return operator = (nullptr);
+
+		} else {
+			Microsoft::WRL::ComPtr<U> pOther(other);
+			_com_util::CheckError(pOther.As(&m_pInterface));
+			return *this;
+		}
+	}
+
+	/*!
+	 * インターフェース型への暗黙変換を行う
+	 */
+	/* implicit */ operator Interface* () const noexcept
+    {
+        return GetInterfacePtr();
+    }
+
+	Interface& operator*() const
+    {
+		return GetInterfaceRef();
+	}
+
+	Interface** operator&() noexcept
+    {
+        return m_pInterface.ReleaseAndGetAddressOf();
+    }
+
+    Interface* operator->() const
+    {
+        if (m_pInterface)
+        {
+            return GetInterfacePtr();
+        }
+
+		_com_raise_error(E_POINTER);
+    }
+};
+
+#define DEFINE_COM_SMARTPTR(Interface) using Interface ## Ptr = TComPtr<Interface>
+#endif
 
 //{{AFX_INSERT_LOCATION}}
 // Microsoft Visual C++ は前行の直前に追加の宣言を挿入します。
