27b9fac7510403bf1882e250c9f9454e71516c81
berryzplus <berryzplus@gmail.com>
2025-06-04 20:07:48 +0900

    文字列クラスの一括リファクタリング
    SString/TCharBuffer/CNativeW/CMemory

--- sakura_core/mem/CNative.h
+++ sakura_core/mem/CNative.h
@@ -11,19 +11,143 @@
 
 #include "mem/CMemory.h"
 
-//※CMemoryをprotect継承することにより、あまり自由にCMemoryを使えないようにしておく
-class CNative : protected CMemory{
-public:
-	//CMemory*ポインタを得る
-	CMemory* _GetMemory(){ return static_cast<CMemory*>(this); }
-	const CMemory* _GetMemory() const{ return static_cast<const CMemory*>(this); }
+#include "util/string_ex.h"
+
+/*!
+ * 文字列バッファ管理クラス
+ *
+ * ヒープメモリにバッファ領域を確保する
+ * ※CMemoryをprotect継承することにより、あまり自由にCMemoryを使えないようにしておく
+ *
+ * @author kobake
+ * @date 2007/11/06 kobake 新規作成
+ */
+template<typename C>
+class CNative : protected CMemory {
+private:
+	using Me = CNative<C>;
+	using Base = CMemory;
 
 public:
-	//汎用
-	void Clear(); //!< 空っぽにする
+	using char_type = C;
+	using string_type = std::basic_string<char_type>;
+	using string_view_type = std::basic_string_view<char_type>;
+
+	CNative() noexcept = default;
+
+	CNative(
+		_In_reads_(cchData)
+		const char_type* pData,
+		size_t cchData)
+	{
+		SetString(pData, cchData);
+	}
+
+	explicit CNative(string_view_type rhs) { SetString(rhs.data(), rhs.length()); }
+
+	/* implicit */ CNative(_In_opt_z_ const char_type* pszData) { SetString(pszData); }
+	/* implicit */ CNative(const string_type& rhs) { SetString(rhs.data(), rhs.length()); }
+
+	CMemory*       _GetMemory()       { return static_cast<CMemory*>(this); }       //<! CMemoryのポインタを取得（旧コード互換用）
+	const CMemory* _GetMemory() const { return static_cast<const CMemory*>(this); } //<! CMemoryのポインタを取得（旧コード互換用）
+
+	void _SetStringLength(size_t nLength) { _SetRawLength(nLength * sizeof(char_type)); }
+
+	/*!
+	 * (重要：nDataLenは文字単位) バッファサイズの調整。必要に応じて拡大する。
+	 */
+	void AllocStringBuffer(size_t nDataLen) {
+		AllocBuffer((nDataLen + 1) * sizeof(char_type));
+	}
+
+	void AppendString( _In_reads_( nDataLen ) const char_type* pData, size_t nDataLen ) { Base::AppendRawData( pData, nDataLen * sizeof( char_type ) ); }	//!< バッファの最後にデータを追加する。nLengthは文字単位。
+
+	//! バッファの最後にデータを追加する
+	void AppendString(_In_z_ const char_type* pszData) {
+		if (pszData) {
+			AppendString(pszData, auto_strlen(pszData));
+		}
+	}
+
+	void AppendStringF(_In_z_ _Printf_format_string_ const char_type* pszFormat, ...)          //!< バッファの最後にデータを追加する (フォーマット機能付き)
+	{
+		// auto_vscprintf に NULL を渡してはならないので除外する
+		if (!pszFormat || !*pszFormat) {
+			throw std::invalid_argument("format can't be empty");
+		}
+
+		va_list v;
+		va_start(v, pszFormat);
+
+		// 整形によって追加される文字数をカウント
+		const auto additional = auto_vscprintf(pszFormat, v);
+
+		// 現在の文字列長を取得
+		const auto currentLength = length();
+
+		// 現在の文字数 + 追加文字数が収まるようにバッファを拡張する
+		AllocStringBuffer(currentLength + additional);
+
+		// 追加処理の実体はCRTに委譲。この関数は無効な書式を与えると即死する。
+		auto_vsprintf_s(data() + currentLength, additional + 1, pszFormat, v);
+
+		va_end(v);
+
+		// 文字列終端を再設定する
+		_SetStringLength(currentLength + additional);
+	}
+
+	const char_type* c_str() const noexcept { return data(); }
+
+	void Clear() { _SetRawLength(0); } //!< 空っぽにする
+
+	char_type*       data()        noexcept { return get<char_type>(); }
+	const char_type* data()  const noexcept { return get<char_type>(); }
+
+	bool    empty() const noexcept { return length() == 0; } //!< 空っぽかどうか
+
+	char_type*       GetStringPtr()       { return data(); }
+	const char_type* GetStringPtr() const { return data(); }
+
+	int     GetStringLength() const { return Length(); }
+
+	size_t  length() const noexcept { return Base::length<char_type>(); }
+	int     Length() const noexcept { return int(length()); }
+
+	//! バッファの内容を置き換える。nDataLenは文字単位。
+	void SetString(
+		_In_reads_(nDataLen)
+		const char_type* pData,
+		size_t nDataLen
+	)
+	{
+		Base::SetRawData(pData, nDataLen * sizeof(char_type));
+	}
+
+	//! バッファの内容を置き換える
+	void SetString(_In_opt_z_ const char_type* pszData) {
+		if (!pszData) {
+			Base::Reset();
+		} else {
+			SetString(pszData, auto_strlen(pszData));
+		}
+	}
+
+	//! メモリ再確保を行わずに格納できる最大文字数を求める
+	size_t size() const noexcept { return Base::capacity() / sizeof(char_type); }
+
+	Me& operator = (string_view_type rhs) { SetString(rhs.data(), rhs.length()); return *this; }
+	Me& operator = (_In_opt_z_ const char_type* rhs) { SetString(rhs); return *this; }
+	Me& operator = (const string_type& rhs) { SetString(rhs.data(), rhs.length()); return *this; }
+	Me& operator = (const char_type ch) { SetString(&ch, 1); return *this; }
+
+	Me& operator += (string_view_type rhs) { AppendString(rhs.data(), rhs.length()); return *this; }
+	Me& operator += (_In_z_ const char_type* rhs) { AppendString(rhs); return *this; }
+	Me& operator += (const string_type& rhs) { AppendString(rhs.data(), rhs.length()); return *this; }
+	Me& operator += (const char_type ch) { AppendString(&ch, 1); return *this; }
+
+	explicit operator string_view_type() const noexcept { return string_view_type(data(), length()); }
+	explicit operator string_type() const noexcept { return string_type(data(), length()); }
 };
-
-// 派生クラスでメンバー追加禁止
-static_assert(sizeof(CNative) == sizeof(CMemory), "size check");
 
 #endif /* SAKURA_CNATIVE_D712C69D_7FA3_49CE_B63A_49273441B558_H_ */
--- tests/unittests/test-statictype.cpp
+++ tests/unittests/test-statictype.cpp
@@ -89,6 +89,13 @@ TEST(StaticString, init001)
 	// ASSERT
 	EXPECT_THAT(buf, StrEq(L""));
 	EXPECT_THAT(buf.GetBufferCount(), 4);
+	EXPECT_TRUE(buf.empty());
+	EXPECT_THAT(LPWSTR(buf), StrEq(L""));
+	EXPECT_THAT(LPCWSTR(buf), StrEq(L""));
+	EXPECT_THAT(buf.length(), 0);
+	EXPECT_THAT(buf.Length(), 0);
+	EXPECT_THAT(std::size(buf), 4);
+	EXPECT_THAT(buf.at(0), 0);
 }
 
 /*!
@@ -106,7 +113,70 @@ TEST(StaticString, init002)
 
 	// ASSERT
 	EXPECT_THAT(buf, StrEq(L"初期値"));
-	EXPECT_THAT(buf.Length(), 3);
+	EXPECT_THAT(buf.length(), 3);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 文字列で初期化する
+ */
+TEST(StaticString, init003)
+{
+	// ACT
+	StaticString<4> szText(L"初期値"s);
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"初期値"));
+	EXPECT_THAT(szText.length(), 3);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 文字列参照で初期化する
+ */
+TEST(StaticString, init004)
+{
+	// ACT
+	StaticString<4> szText(L"初期値"sv);
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"初期値"));
+	EXPECT_THAT(szText.length(), 3);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * ファイルパスで初期化する
+ */
+TEST(StaticString, init006)
+{
+	// ARRANGE
+	std::filesystem::path path = L"初期値";
+
+	// ACT
+	StaticString<4> szText = path;
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"初期値"));
+	EXPECT_THAT(szText.length(), 3);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 文字列ポインタで初期化する(値がnullptr)
+ */
+TEST(StaticString, init101)
+{
+	// ACT
+	StaticString<4> szText = nullptr;	// SALによりコンパイラ警告が発生する
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L""));
+	EXPECT_THAT(szText.length(), 0);
 }
 
 /*!
@@ -121,7 +191,22 @@ TEST(StaticString, init102)
 
 	// ASSERT
 	EXPECT_THAT(buf, StrEq(L""));
-	EXPECT_THAT(buf.Length(), 0);
+	EXPECT_THAT(buf.length(), 0);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 文字列参照で初期化する(サイズオーバー)
+ */
+TEST(StaticString, init104)
+{
+	// ACT
+	StaticString<4> szText = L"長過ぎる";
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"長過ぎ"));
+	EXPECT_THAT(szText.length(), 3);
 }
 
 /*!
@@ -138,7 +223,107 @@ TEST(StaticString, assign001)
 
 	// ASSERT
 	EXPECT_THAT(buf, StrEq(L"設定値"));
-	EXPECT_THAT(buf.Length(), 3);
+	EXPECT_THAT(buf.length(), 3);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 文字列ポインタを代入する
+ */
+TEST(StaticString, assign002)
+{
+	// ARRANGE
+	StaticString<4> szText = L"初期値";
+
+	// ACT
+	szText = L"設定値";
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"設定値"));
+	EXPECT_THAT(szText.length(), 3);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 文字列を代入する
+ */
+TEST(StaticString, assign003)
+{
+	// ARRANGE
+	StaticString<4> szText = L"初期値";
+
+	// ACT
+	szText = L"設定値"s;
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"設定値"));
+	EXPECT_THAT(szText.length(), 3);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 文字列参照を代入する
+ */
+TEST(StaticString, assign004)
+{
+	// ARRANGE
+	StaticString<4> szText = L"初期値";
+
+	// ACT
+	szText = L"設定値"sv;
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"設定値"));
+	EXPECT_THAT(szText.length(), 3);
+
+	// ACT
+	szText = std::wstring_view(szText, 1);
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"設"));
+	EXPECT_THAT(szText.length(), 1);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 1文字を代入する
+ */
+TEST(StaticString, assign005)
+{
+	// ARRANGE
+	StaticString<4> szText = L"初期値";
+
+	// ACT
+	szText = L'a';
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"a"));
+	EXPECT_THAT(szText.length(), 1);
+	EXPECT_EQ(szText[0], L'a');
+	EXPECT_EQ(szText[1], L'\0');
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * ファイルパスを代入する
+ */
+TEST(StaticString, assign006)
+{
+	// ARRANGE
+	StaticString<4> szText = L"初期値";
+	std::filesystem::path path = L"設定値";
+
+	// ACT
+	szText = path;
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"設定値"));
+	EXPECT_THAT(szText.length(), 3);
 }
 
 /*!
@@ -157,7 +342,394 @@ TEST(StaticString, assign101)
 
 	// ASSERT
 	EXPECT_THAT(buf, StrEq(L""));
-	EXPECT_THAT(buf.Length(), 0);
+	EXPECT_THAT(buf.length(), 0);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 文字列ポインタを代入する(値がnullptr)
+ */
+TEST(StaticString, assign102)
+{
+	// ARRANGE
+	StaticString<4> szText = L"初期値";
+	LPCWSTR pszNull = nullptr;
+
+	// ACT
+	szText = pszNull;
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L""));
+	EXPECT_THAT(szText.length(), 0);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 文字列参照を代入する(サイズオーバー)
+ */
+TEST(StaticString, assign104)
+{
+	// ARRANGE
+	StaticString<4> szText = L"初期値";
+
+	// ACT
+	szText = L"長過ぎる"sv;
+	
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"長過ぎ"));
+	EXPECT_THAT(szText.length(), 3);
+
+	// ACT2
+	EXPECT_EQ(STRUNCATE, szText.assign(L"overflow"sv));
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"ove"));
+	EXPECT_THAT(szText.length(), 3);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 末尾に文字列ポインタを追加する
+ */
+TEST(StaticString, append002)
+{
+	// ARRANGE
+	StaticString<4> szText = L"ザク";
+
+	// ACT
+	szText += L"Ⅱ";
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"ザクⅡ"));
+	EXPECT_THAT(szText.length(), 3);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 末尾に文字列を追加する
+ */
+TEST(StaticString, append003)
+{
+	// ARRANGE
+	StaticString<4> szText = L"ザク";
+
+	// ACT
+	szText += L"Ⅱ"s;
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"ザクⅡ"));
+	EXPECT_THAT(szText.length(), 3);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 末尾に文字列参照タを追加する
+ */
+TEST(StaticString, append004)
+{
+	// ARRANGE
+	StaticString<4> szText = L"ザク";
+
+	// ACT
+	szText += L"Ⅱ"sv;
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"ザクⅡ"));
+	EXPECT_THAT(szText.length(), 3);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 末尾に1文字を追加する
+ */
+TEST(StaticString, append005)
+{
+	// ARRANGE
+	StaticString<4> szText = L"ザク";
+
+	// ACT
+	szText += L'Ⅱ';
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"ザクⅡ"));
+	EXPECT_THAT(szText.length(), 3);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 末尾にnullptrを追加する
+ */
+TEST(StaticString, append101)
+{
+	// ARRANGE
+	StaticString<4> szText = L"初期値";
+
+	// ACT
+	szText += nullptr;	// SALによりコンパイラ警告が発生する
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"初期値"));
+	EXPECT_THAT(szText.length(), 3);
+
+	// ACT & ASSERT
+	EXPECT_THAT(szText.append(nullptr), EINVAL);
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"初期値"));
+	EXPECT_THAT(szText.length(), 3);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 末尾に文字列ポインタを追加する(値がnullptr)
+ */
+TEST(StaticString, append102)
+{
+	// ARRANGE
+	StaticString<4> szText = L"初期値";
+	LPCWSTR pszNull = nullptr;
+
+	// ACT
+	szText += pszNull;	// SALによりコンパイラ警告が発生する
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"初期値"));
+	EXPECT_THAT(szText.length(), 3);
+
+	// ACT & ASSERT
+	EXPECT_THAT(szText.append(pszNull), EINVAL);
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"初期値"));
+	EXPECT_THAT(szText.length(), 3);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 末尾に文字列を追加する(値が空文字列)
+ */
+TEST(StaticString, append103)
+{
+	// ARRANGE
+	StaticString<4> szText = L"初期値";
+
+	// ACT & ASSERT
+	EXPECT_THAT(szText.append(L""s), EINVAL);
+	EXPECT_THAT(szText, StrEq(L"初期値"));
+	EXPECT_THAT(szText.length(), 3);
+
+	// ACT
+	szText += L""s;
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"初期値"));
+	EXPECT_THAT(szText.length(), 3);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 末尾に文字列参照を追加する(サイズオーバー)
+ */
+TEST(StaticString, append104)
+{
+	// ARRANGE
+	StaticString<4> szText = L"ザク";
+
+	// ACT & ASSERT
+	szText += L"Ⅱ MS-06F"sv;
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"ザクⅡ"));
+	EXPECT_THAT(szText.length(), 3);
+
+	// ARRANGE
+	szText = L"ザク";
+
+	// ACT & ASSERT
+	EXPECT_THAT(szText.append(L"Ⅱ MS-06F"sv), STRUNCATE);
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"ザクⅡ"));
+	EXPECT_THAT(szText.length(), 3);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 末尾に文字列ポインタを追加する(途中にNULを含む)
+ */
+TEST(StaticString, append202)
+{
+	// ARRANGE
+	StaticString<4> szText = L"ザ";
+
+	// ACT
+	szText += L"ク\0Ⅱ";
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"ザク"));
+	EXPECT_THAT(szText.length(), 2);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 末尾に文字列参照を追加する(途中にNULを含む)
+ */
+TEST(StaticString, append204)
+{
+	StaticString<4> szText = L"ザ";
+
+	// ACT
+	szText += L"ク\0Ⅱ"sv;
+
+	// ASSERT
+	EXPECT_THAT(szText, StrEq(L"ザク"));
+	EXPECT_THAT(szText.length(), 2);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 文字列長を計算する
+ */
+TEST(StaticString, length)
+{
+	// ARRANGE
+	StaticString<4> szText = L"ザクⅡ";
+	const wchar_t dummy = 0x2025;
+
+	// ASSERT
+	EXPECT_EQ(szText.length(), 3);
+	EXPECT_EQ(szText.data()[3], 0);
+
+	// ACT(領域全体にゴミ投入)
+	auto_memset(szText.data(), dummy, int(std::size(szText)));
+
+	// ASSERT
+	EXPECT_EQ(szText.length(), 0);
+	EXPECT_EQ(szText.data()[3], dummy);
+
+	// ACT(NUL終端する)
+	szText[2] = 0;
+
+	// ASSERT
+	EXPECT_EQ(szText.length(), 2);
+	EXPECT_EQ(szText.data()[3], dummy);
+
+	// ACT
+	szText = dummy;
+
+	// ASSERT
+	EXPECT_EQ(szText.length(), 1);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 文字列が空かどうか判定する
+ */
+TEST(StaticString, empty)
+{
+	// ARRANGE
+	StaticString<4> szText = L"ザクⅡ";
+
+	// ASSERT
+	EXPECT_FALSE(szText.empty());
+
+	// ACT
+	szText = nullptr;
+
+	// ASSERT
+	EXPECT_TRUE(szText.empty());
+}
+
+/*!
+ * @brief StaticStringのテスト
+ */
+TEST(StaticString, constAt)
+{
+	// ARRANGE
+	const StaticString<4> szText = L"初期値";
+
+	// ASSERT
+	EXPECT_THAT(szText.at(0), L'初');
+	EXPECT_THAT(szText.at(1), L'期');
+	EXPECT_THAT(szText.at(2), L'値');
+	EXPECT_THAT(szText.at(3), '\0');
+
+	EXPECT_THROW({ szText.at(4); }, std::out_of_range);
+	EXPECT_THROW({ szText.at(5); }, std::out_of_range);
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * 添え字演算子
+ */
+TEST(StaticString, subscriptOperator001)
+{
+	// ARRANGE
+	StaticString<4> szText = L"初期値";
+
+	// ASSERT
+	EXPECT_THAT(szText[0], L'初');
+	EXPECT_THAT(szText[1], L'期');
+	EXPECT_THAT(szText[2], L'値');
+	EXPECT_THAT(szText[3], '\0');
+	EXPECT_THAT(szText[4], '\0');
+}
+
+/*!
+ * @brief StaticStringのテスト
+ * 
+ * 添え字演算子
+ */
+TEST(StaticString, constSubscriptOperator001)
+{
+	// ARRANGE
+	const StaticString<4> szText = L"初期値";
+
+	// ASSERT
+	EXPECT_THAT(szText[0], L'初');
+	EXPECT_THAT(szText[1], L'期');
+	EXPECT_THAT(szText[2], L'値');
+	EXPECT_THAT(szText[3], '\0');
+	EXPECT_THAT(szText[4], '\0');
+}
+
+/*!
+ * @brief StaticStringのテスト
+ *
+ * ファイルパス型に変換する
+ */
+TEST(StaticString, toFilePath)
+{
+	// ARRANGE
+	StaticString<4> szText = L"初期値";
+
+	// ACT
+	auto path = std::filesystem::path(szText);
+
+	// ASSERT
+	EXPECT_FALSE(path.empty());
+
+	// ACT
+	szText = nullptr;
+	path = std::filesystem::path(szText);
+
+	// ASSERT
+	EXPECT_TRUE(path.empty());
 }
 
 } // namespace mystring
