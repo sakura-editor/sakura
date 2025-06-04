5178f616f834249da288ce662fab5c21e59514f1
berryzplus <berryzplus@gmail.com>
2025-06-04 20:02:42 +0900

    gmockのマッチャーをプリコンパイル済みヘッダーでまとめてusingする

--- tests/unittests/pch.h
+++ tests/unittests/pch.h
@@ -24,4 +24,23 @@
  */
 #include "StdAfx.h"
 
+// テストではGMockを使う前提にする
 #include <gmock/gmock.h>
+
+// マッチャーのusing（追加するときは昇順で。）
+using ::testing::_;
+using ::testing::Eq;
+using ::testing::Ge;
+using ::testing::Gt;
+using ::testing::Invoke;
+using ::testing::IsEmpty;
+using ::testing::IsFalse;
+using ::testing::IsTrue;
+using ::testing::Le;
+using ::testing::Lt;
+using ::testing::Ne;
+using ::testing::Return;
+using ::testing::StrCaseEq;
+using ::testing::StrCaseNe;
+using ::testing::StrEq;
+using ::testing::StrNe;
