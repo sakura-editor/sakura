6167554237e643b9700a285a7960d7ea68851912
berryzplus <berryzplus@gmail.com>
2025-06-04 20:03:34 +0900

    NULLマクロの再定義をやめる

--- tests/unittests/test-cnative.cpp
+++ tests/unittests/test-cnative.cpp
@@ -28,15 +28,6 @@
 #include "mem/CNativeW.h"
 #include "mem/CNativeA.h"
 
-// NULL定義補正
-// 詳細はStdAfx.hを参照
-#ifdef __cplusplus
-# pragma warning( push )
-# pragma warning( disable : 4005 )
-# define NULL nullptr
-# pragma warning( pop )
-#endif // end of #ifdef __cplusplus
-
 /*!
 	CStringRefのテスト
  */
@@ -277,7 +268,7 @@ TEST(CNativeW, AssignString)
 TEST(CNativeW, AssignStringNullPointer)
 {
 	CNativeW value(L"test");
-	value = NULL;
+	value = nullptr;	// NULLではなくnullptrを使うよう修正
 	EXPECT_EQ(0, value.GetStringLength());
 	EXPECT_EQ(NULL, value.GetStringPtr());
 }
@@ -290,7 +281,7 @@ TEST(CNativeW, AssignStringNullPointer)
 TEST(CNativeW, AssignStringNullLiteral)
 {
 	CNativeW value(L"test");
-	value = NULL;
+	value = nullptr;	// NULLではなくnullptrを使うよう修正
 	ASSERT_EQ(NULL, value.GetStringPtr());
 	EXPECT_EQ(0, value.GetStringLength());
 }
@@ -337,7 +328,7 @@ TEST(CNativeW, AppendStringNullPointer)
 {
 	CNativeW org(L"orz");
 	CNativeW value(org);
-	value += NULL;
+	value += nullptr;	// NULLではなくnullptrを使うよう修正
 	EXPECT_EQ(value.GetStringLength(), org.GetStringLength());
 	EXPECT_EQ(org, value);
 }
@@ -370,7 +361,7 @@ TEST(CNativeW, AppendStringWithFormatting)
 	ASSERT_EQ(L"いちご25%", value);
 
 	// 未確保状態からの書式化をテストする
-	value = NULL; //テスト前の初期値(未確保
+	value = nullptr; //テスト前の初期値(未確保
 	value.AppendStringF( L"KEY[%03d]", 12 );
 	ASSERT_EQ( L"KEY[012]", value );
 
@@ -385,7 +376,7 @@ TEST(CNativeW, AppendStringWithFormatting)
 	// フォーマット出力長2047字を超える条件をテストする
 	{
 		std::wstring longText( 2048, L'=' );
-		value = NULL; //テスト前の初期値(未確保
+		value = nullptr; //テスト前の初期値(未確保
 		value.AppendStringF( L"%s", longText.c_str() );
 		ASSERT_EQ( longText.c_str(), value );
 	}
@@ -408,7 +399,7 @@ TEST(CNativeW, operatorEqualNull)
 TEST(CNativeW, operatorEqualNullptr)
 {
 	CNativeW value;
-	ASSERT_EQ(value, NULL);
+	ASSERT_EQ(value, nullptr);	// NULLではなくnullptrを使うよう修正
 }
 
 /*!
@@ -551,7 +542,7 @@ TEST(CNativeW, operatorNotEqualNullptr)
 {
 	constexpr const wchar_t text[] = L"おっす！オラ(ry";
 	CNativeW value(text);
-	ASSERT_NE(value, NULL);
+	ASSERT_NE(value, nullptr);	// NULLではなくnullptrを使うよう修正
 }
 
 /*!
