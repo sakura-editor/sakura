3eb4f812b2dafb63e31e1ee10e75018a0af8db00
gorogoro123 <897627+gorogoro123@users.noreply.github.com>
2025-11-29 15:43:47 +0900

    Repalce wcsncpy (CViewCommander::Command_SET_QUOTESTRING)

--- sakura_core/cmd/CViewCommander_Settings.cpp
+++ sakura_core/cmd/CViewCommander_Settings.cpp
@@ -22,7 +22,6 @@
 #include "StdAfx.h"
 #include "CViewCommander.h"
 #include "CViewCommander_inline.h"
-
 #include "typeprop/CDlgTypeList.h"
 #include "dlg/CDlgFavorite.h"	//履歴の管理	//@@@ 2003.04.08 MIK
 #include "CEditApp.h"
@@ -30,7 +29,6 @@
 #include "CPropertyManager.h"
 #include "util/window.h"
 #include "util/zoom.h"
-#include <array>
 #include "config/system_constants.h"
 #include "config/app_constants.h"
 
@@ -520,8 +518,5 @@ void CViewCommander::Command_SET_QUOTESTRING( const wchar_t* quotestr )
 	if( quotestr == nullptr )
 		return;
 
-	wcsncpy( GetDllShareData().m_Common.m_sFormat.m_szInyouKigou, quotestr,
-		int(std::size(GetDllShareData().m_Common.m_sFormat.m_szInyouKigou)));
-	
-	GetDllShareData().m_Common.m_sFormat.m_szInyouKigou[std::size(GetDllShareData().m_Common.m_sFormat.m_szInyouKigou ) - 1 ] = L'\0';
+	wcsncpy_s( GetDllShareData().m_Common.m_sFormat.m_szInyouKigou, quotestr, _TRUNCATE);
 }
