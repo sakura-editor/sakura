faf231f25538d8e4999cabbb2834fd77b8233939
berryzplus <berryzplus@gmail.com>
2026-01-13 18:29:40 +0900

    SShare_WorkBuffer構造体の定義見直し
    新規メソッド GetBuffer<T>() を追加。
    structは全メンバーpublicなclassなのでアクセス制御できない。

--- sakura_core/cmd/CViewCommander_Support.cpp
+++ sakura_core/cmd/CViewCommander_Support.cpp
@@ -256,7 +256,6 @@ void CViewCommander::Command_EXTHTMLHELP( const WCHAR* _helpfile, const WCHAR* k
 	}
 
 	HWND		hwndHtmlHelp;
-	int			nLen;
 
 	DEBUG_TRACE( L"helpfile=%s\n", helpfile.c_str() );
 
@@ -301,14 +300,13 @@ void CViewCommander::Command_EXTHTMLHELP( const WCHAR* _helpfile, const WCHAR* k
 		// タスクトレイのプロセスにHtmlHelpを起動させる
 		// 2003.06.23 Moca 相対パスは実行ファイルからのパス
 		// 2007.05.21 ryoji 相対パスは設定ファイルからのパスを優先
-		WCHAR* pWork=GetDllShareData().m_sWorkBuffer.GetWorkBuffer<WCHAR>();
+		auto szWork = GetDllShareData().m_sWorkBuffer.GetBuffer<WCHAR>();
 		if( _IS_REL_PATH( filename ) ){
-			GetInidirOrExedir( std::span{ pWork, GetDllShareData().m_sWorkBuffer.GetWorkBufferCount<WCHAR>() }, filename );
+			GetInidirOrExedir(szWork, filename);
 		}else{
-			wcscpy( pWork, filename ); //	Jul. 5, 2002 genta
+			::wcsncpy_s(std::data(szWork), std::size(szWork), filename, _TRUNCATE);
 		}
-		nLen = (int)wcslen( pWork );
-		wcscpy( &pWork[nLen + 1], cmemCurText.GetStringPtr() );
+		::wcsncat_s(std::data(szWork), std::size(szWork), cmemCurText.GetStringPtr(), _TRUNCATE);
 		hwndHtmlHelp = (HWND)::SendMessageAny(
 			GetDllShareData().m_sHandles.m_hwndTray,
 			MYWM_HTMLHELP,
