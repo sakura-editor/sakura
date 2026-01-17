d6cc8f2a762caa5051feb73a6537d094d913b2aa
berryzplus <berryzplus@gmail.com>
2026-01-17 13:13:17 +0900

    ローカルテストスクリプトの更新

--- tools/SonarQube/Run-Test.ps1
+++ tools/SonarQube/Run-Test.ps1
@@ -1,55 +1,10 @@
 # Run-Test.ps1
 Param(
   [String]$testCommand,
-  [bool]$useOpenCppCoverage,
   [String]$HomePath = [System.IO.Path]::GetFullPath("$PSScriptRoot\..\..")
 )
 
 $testName = $([System.IO.Path]::GetFileNameWithoutExtension($testCommand))
-
-if (-not($useOpenCppCoverage)) {
-  $VsInstallationPath = vswhere -property installationPath -version "[$VsVersion,$([int]$VsVersion + 1))"
-  $VsExtentions = "$VsInstallationPath\Common7\IDE\Extensions"
-
-  Write-Host "`$VsExtentions is '$VsExtentions'"
-
-  # GoogleTestAdapterのパスを取得する
-  $testAdapterPath = (Get-ChildItem $VsExtentions -Filter GoogleTestAdapter.TestAdapter.dll -Recurse).DirectoryName
-
-  Write-Host "`$testAdapterPath is '$testAdapterPath'"
-
-  # データコレクターのパスを取得する
-  $DataCollectorPath = (Get-ChildItem $VsExtentions -Filter Microsoft.VisualStudio.TraceDataCollector.dll -Recurse).DirectoryName
-
-  Write-Host "`$DataCollectorPath is '$DataCollectorPath'"
-
-  $vstest = "$VsInstallationPath\Common7\IDE\Extensions\TestPlatform\vstest.console.exe"
-
-  # VSTestを実行する(一旦trx形式で出す)
-  & $vstest @(
-    $testCommand,
-    "/EnableCodeCoverage",
-    "/Collect:`"Code Coverage;Format=Xml`"",
-    "/ResultsDirectory:TestResults"
-    "/TestAdapterPath:`"$testAdapterPath;$DataCollectorPath`"",
-    "/Logger:trx;LogFileName=$testName-vstest.trx"
-  )
-
-  # trx2junitのインストールチェック
-  cmd.exe /c "where.exe trx2junit >NUL"
-  if ($LASTEXITCODE -ne 0) {
-      dotnet tool install --global trx2junit
-  }
-
-  # trx2junitを実行する
-  trx2junit "$HomePath\TestResults\$testName-vstest.trx"
-  Copy-Item -Path "$HomePath\TestResults\$testName-vstest.xml" -Destination "$HomePath\$testName-googletest.xml"
-
-  # xmlファイルは消しておく
-  Remove-Item -Path "$HomePath\TestResults\$testName-vstest.xml"
-
-  return 0
-}
 
 # テストコマンドをフルパスにする
 $testCommand = [System.IO.Path]::GetFullPath($testCommand)
