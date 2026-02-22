1f762937a1c8b09b3ec101d869d6033bb1b8fd0c
berryzplus <berryzplus@gmail.com>
2026-02-22 18:42:49 +0900

    CDecodeのテストを書き直す
    テストケースを分割して、壊したときにどこが壊れたか分かるようにする。

--- src/test/cpp/tests1/test-cdecode.cpp
+++ src/test/cpp/tests1/test-cdecode.cpp
@@ -1,124 +1,107 @@
 ﻿/*
-	Copyright (C) 2021-2022, Sakura Editor Organization
+	Copyright (C) 2021-2026, Sakura Editor Organization
 
 	SPDX-License-Identifier: Zlib
 */
 
 #include "pch.h"
-#include <cstdlib>
-#include <string>
 #include "convert/CDecode.h"
+
 #include "convert/CDecode_Base64Decode.h"
 #include "convert/CDecode_UuDecode.h"
 
-struct DecoderTestCase {
-	const wchar_t* input;
-	const char* output;
+namespace convert {
+
+/*!
+ * デコーダーテストのパラメーター
+ *
+ * デコーダーはワイド文字列っぽいものを受け取り、バイト列を出力する。
+ * 入力するワイド文字列によってはデコードが失敗する。
+ * デコードの正否と期待値を同時に指定できるよう std::optional を使う。
+ */
+using DecoderTestParam = std::tuple<CNativeW, std::optional<std::string>>;
+
+// Base64デコーダーのテスト
+struct Base64DecodeTest : public ::testing::TestWithParam<DecoderTestParam> {
+	CDecode_Base64Decode decoder;
 };
 
-TEST(CDecode, Base64)
+TEST_P(Base64DecodeTest, DoDecode)
 {
-	const DecoderTestCase testCases[] = {
-		{L"", ""},
-		{L"cw==", "s"},
-		{L"c2E=", "sa"},
-		{L"c2Fr", "sak"},
-		{L"c2FrdQ==", "saku"},
-		{L"c2FrdXI=", "sakur"},
-		{L"c2FrdXJh", "sakura"}
-	};
-	CNativeW s;
+	const auto& input = std::get<0>(GetParam());
+	const auto& expected = std::get<1>(GetParam());
 	CMemory m;
-	for (const auto& testCase : testCases) {
-		s.SetString(testCase.input);
-		EXPECT_TRUE(CDecode_Base64Decode().DoDecode(s, &m));
-		EXPECT_STREQ(reinterpret_cast<char*>(m.GetRawPtr()), testCase.output);
+	const auto result = decoder.DoDecode(input, &m);
+	EXPECT_THAT((bool)result, expected.has_value());
+	if (expected.has_value()) {
+		EXPECT_THAT(LPCSTR(m.GetRawPtr()), StrEq(*expected));
 	}
-
-	// 空白は無視する
-	s.SetString(L"c2Fr \t dQ==");
-	EXPECT_TRUE(CDecode_Base64Decode().DoDecode(s, &m));
-	EXPECT_STREQ(reinterpret_cast<char*>(m.GetRawPtr()), "saku");
-
-	// 異常な文字があったら変換を中止する
-	s.SetString(L"c2Fr?dQ==");
-	EXPECT_FALSE(CDecode_Base64Decode().DoDecode(s, &m));
 }
 
-TEST(CDecode, uuencode)
+INSTANTIATE_TEST_SUITE_P(
+	Base64Cases,
+	Base64DecodeTest,
+	::testing::Values(
+		DecoderTestParam{ L"",             "" },
+		DecoderTestParam{ L"cw==",         "s" },
+		DecoderTestParam{ L"c2E=",         "sa" },
+		DecoderTestParam{ L"c2Fr",         "sak" },
+		DecoderTestParam{ L"c2FrdQ==",     "saku" },
+		DecoderTestParam{ L"c2FrdXI=",     "sakur" },
+		DecoderTestParam{ L"c2FrdXJh",     "sakura" },
+		DecoderTestParam{ L"c2Fr \t dQ==", "saku" },		// 空白は無視する
+		DecoderTestParam{ L"c2Fr?dQ==",    std::nullopt }	// 異常な文字があったら変換を中止する
+	));
+
+// UUデコーダーのテスト
+struct UuDecodeTest : public ::testing::TestWithParam<DecoderTestParam> {
+	CDecode_UuDecode decoder;
+};
+
+TEST_P(UuDecodeTest, DoDecode)
 {
-	const DecoderTestCase testCases[] = {
-		{L"begin 666 test\r\n \r\nend\r\n", ""},
-		{L"begin 666 test\r\n!<P  \r\n \r\nend\r\n", "s"},
-		{L"begin 666 test\r\n\"<V$ \r\n \r\nend\r\n", "sa"},
-		{L"begin 666 test\r\n#<V%K\r\n \r\nend\r\n", "sak"},
-		{L"begin 666 test\r\n$<V%K=0  \r\n \r\nend\r\n", "saku"},
-		{L"begin 666 test\r\n%<V%K=7( \r\n \r\nend\r\n", "sakur"},
-		{L"begin 666 test\r\n&<V%K=7)A\r\n \r\nend\r\n", "sakura"}
-	};
-	CNativeW s;
+	const auto& input = std::get<0>( GetParam() );
+	const auto& expected = std::get<1>(GetParam());
 	CMemory m;
-	for (const auto& testCase : testCases) {
-		s.SetString(testCase.input);
-
-		CDecode_UuDecode decoder;
-		EXPECT_TRUE(decoder.DoDecode(s, &m));
-		EXPECT_STREQ(reinterpret_cast<char*>(m.GetRawPtr()), testCase.output);
-
-		wchar_t fileName[_MAX_PATH];
-		decoder.CopyFilename(fileName);
-		EXPECT_STREQ(fileName, L"test");
+	const auto result = decoder.DoDecode(input, &m);
+	EXPECT_THAT((bool)result, expected.has_value());
+	if (expected.has_value()) {
+		EXPECT_THAT(LPCSTR(m.GetRawPtr()), StrEq(*expected));
+		// 正常に解釈できた場合、ファイル名もチェック。
+		// （ファイル名は固定でtestを指定し、評価を簡略化している）
+		SFilePath fileName;
+		decoder.CopyFilename(static_cast<std::span<WCHAR, _MAX_PATH>>(fileName));
+		EXPECT_THAT(fileName, StrEq(L"test"));
 	}
-
-	// ヘッダーおよびフッターの先頭と末尾の空白は無視する
-	s.SetString(L"\tbegin 666 test \r\n!<P  \r\n \r\n\tend \r\n");
-	EXPECT_TRUE(CDecode_UuDecode().DoDecode(s, &m));
-	EXPECT_STREQ(reinterpret_cast<char*>(m.GetRawPtr()), "s");
-
-	// 入力文字列が空の場合
-	s.SetString(L"");
-	EXPECT_FALSE(CDecode_UuDecode().DoDecode(s, &m));
-
-	// 文字列が begin で始まっていない場合
-	s.SetString(L"benign 666 test\r\n!<P  \r\n \r\nend\r\n");
-	EXPECT_FALSE(CDecode_UuDecode().DoDecode(s, &m));
-	s.SetString(L"bigin 666 test\r\n!<P  \r\n \r\nend\r\n");
-	EXPECT_FALSE(CDecode_UuDecode().DoDecode(s, &m));
-
-	// パーミッション設定が異常である場合
-	s.SetString(L"begin 66 test\r\n!<P  \r\n \r\nend\r\n");
-	EXPECT_FALSE(CDecode_UuDecode().DoDecode(s, &m));
-	s.SetString(L"begin 888 test\r\n!<P  \r\n \r\nend\r\n");
-	EXPECT_FALSE(CDecode_UuDecode().DoDecode(s, &m));
-
-	// ファイル名が指定されていない場合
-	s.SetString(L"begin 666\r\n!<P  \r\n \r\nend\r\n");
-	EXPECT_FALSE(CDecode_UuDecode().DoDecode(s, &m));
-
-	// ファイル名の長さが_MAX_PATHを超える場合
-	std::wstring buffer;
-	for (int i = 0; i <= _MAX_PATH; ++i)
-		buffer.push_back(L'a');
-	s.SetString(L"begin 666 ");
-	s.AppendString(buffer.c_str());
-	s.AppendString(L"\r\n!<P  \r\n \r\nend\r\n");
-	EXPECT_FALSE(CDecode_UuDecode().DoDecode(s, &m));
-
-	// 改行コードがCRLFではない場合
-	s.SetString(L"begin 666 test\n!<P  \n \nend\n");
-	EXPECT_FALSE(CDecode_UuDecode().DoDecode(s, &m));
-
-	// 妥当なヘッダー行の後にEOSが現れた場合
-	s.SetString(L"begin 666 test");
-	EXPECT_FALSE(CDecode_UuDecode().DoDecode(s, &m));
-
-	// 文字列が end で終わっていない場合
-	s.SetString(L"begin 666 test\r\n!<P  \r\n \r\nen");
-	EXPECT_FALSE(CDecode_UuDecode().DoDecode(s, &m));
-	s.SetString(L"begin 666 test\r\n!<P  \r\n \r\nned\r\n");
-	EXPECT_FALSE(CDecode_UuDecode().DoDecode(s, &m));
-
-	// end の後ろに空白以外の文字がある場合
-	s.SetString(L"begin 666 test\r\n!<P  \r\n \r\nendo\r\n");
-	EXPECT_FALSE(CDecode_UuDecode().DoDecode(s, &m));
 }
+
+INSTANTIATE_TEST_SUITE_P(
+	UuDecodeCases,
+	UuDecodeTest,
+	::testing::Values(
+		DecoderTestParam{ L"begin 666 test\r\n \r\nend\r\n",                "" },
+		DecoderTestParam{ L"begin 666 test\r\n!<P  \r\n \r\nend\r\n",       "s" },
+		DecoderTestParam{ L"begin 666 test\r\n\"<V$ \r\n \r\nend\r\n",      "sa" },
+		DecoderTestParam{ L"begin 666 test\r\n#<V%K \r\n \r\nend\r\n",      "sak" },
+		DecoderTestParam{ L"begin 666 test\r\n$<V%K=0 \r\n`\r\nend\r\n",    "saku" },
+		DecoderTestParam{ L"begin 666 test\r\n%<V%K=7(\r\n~\r\nend\r\n",    "sakur" },
+		DecoderTestParam{ L"begin 666 test\r\n&<V%K=7)A\r\n \r\nend\r\n",   "sakura" },
+		DecoderTestParam{ L"  begin 666 test\r\n&<V%K=7)A\r\n \r\nend\r\n", "sakura" },		// 先頭の空白はスキップする
+		DecoderTestParam{ L"",                                              std::nullopt },	// 入力文字列が空の場合
+		DecoderTestParam{ L"benign 666 test\r\n!<P  \r\n \r\nend\r\n",      std::nullopt },	// 文字列が begin で始まっていない場合
+		DecoderTestParam{ L"bigin 666 test\r\n!<P  \r\n \r\nend\r\n",       std::nullopt },	// 文字列が begin で始まっていない場合
+		DecoderTestParam{ L"biginn 666 test\r\n!<P  \r\n \r\nend\r\n",      std::nullopt },	// 文字列が begin で始まっていない場合
+		DecoderTestParam{ L"begin 66 test\r\n!<P  \r\n \r\nend\r\n",        std::nullopt },	// パーミッション設定が異常である場合（パーミッションは8進数3桁なので、2桁はNG。）
+		DecoderTestParam{ L"begin 888 test\r\n!<P  \r\n \r\nend\r\n",       std::nullopt },	// パーミッション設定が異常である場合（パーミッションは8進数なので、8はNG。）
+		DecoderTestParam{ L"begin 666\r\n!<P  \r\n \r\nend\r\n",            std::nullopt },	// ファイル名が指定されていない場合
+		DecoderTestParam{ L"begin 666 test\n!<P  \n \nend\n",               std::nullopt },	// 改行コードがCRLFではない場合
+		DecoderTestParam{ L"begin 666 test",                                std::nullopt },	// 妥当なヘッダー行の後にEOSが現れた場合
+		DecoderTestParam{ L"begin 666 test\r\n!<P  \r\n\r\nend\r\n",        std::nullopt }, // 本文データに1文字未満の行がある場合
+		DecoderTestParam{ L"begin 666 test\r\n!<P  \r\n \r\nen",            std::nullopt },	// 文字列が end で終わっていない場合
+		DecoderTestParam{ L"begin 666 test\r\n!<P  \r\n \r\nned\r\n",       std::nullopt },	// 文字列が end で終わっていない場合
+		DecoderTestParam{ L"begin 666 test\r\n!<P  \r\n \r\nendo\r\n",      std::nullopt },	// end の後ろに空白以外の文字がある場合
+
+		DecoderTestParam{ CNativeW(std::format(L"begin 666 {:a<260}\r\n!<P  \r\n \r\nend\r\n", L'a').c_str()), std::nullopt }	// ファイル名の長さが_MAX_PATHを超える場合
+	));
+
+} // namespace convert
