b7f00149f1d4f5de96df4cb9366232cb6e68b62b
berryzplus <berryzplus@gmail.com>
2026-03-06 19:21:37 +0900

    マクロマネージャーのテストを書く

--- src/test/cpp/tests1/macro/test-macro.cpp
+++ src/test/cpp/tests1/macro/test-macro.cpp
@@ -6,8 +6,11 @@
 */
 #include "pch.h"
 
+#include "macro/CKeyMacroMgr.h"
 #include "macro/CMacroFactory.h"
 #include "macro/CPPAMacroMgr.h"
+#include "macro/CPythonMacroManager.h"
+#include "macro/CWSHManager.h"
 
 #include "window/EditorTestSuite.hpp"
 
@@ -34,6 +37,82 @@ struct MacroMgrTest : public ::testing::Test, public window::EditorTestSuite {
 		TearDownEditor();
 	}
 };
+
+/*!
+ * キーマクロマネージャーのテスト
+ */
+TEST_F(MacroMgrTest, CKeyMacroMgr001)
+{
+	EXPECT_THAT(CKeyMacroMgr::Creator(L"ppa"), IsNull());
+
+	CKeyMacroMgr::declare();
+
+	auto mgr = std::unique_ptr<CMacroManagerBase>(CMacroFactory::getInstance()->Create(L"mac"));
+
+	const HINSTANCE unusedArg1 = nullptr;
+
+	EXPECT_THAT(mgr->LoadKeyMacroStr(unusedArg1, L""), IsTrue());
+
+	EXPECT_THAT(mgr->ExecKeyMacro(&pcEditWnd->GetActiveView(), 0), IsTrue());
+
+	const auto path = GetTempFilePathWithExt(L"tes", L"mac");
+	EXPECT_THAT(mgr->LoadKeyMacro(unusedArg1, path.c_str()), IsFalse());
+
+	std::wofstream fs(path);
+	fs << L" \t" << std::endl;
+	fs << L"// comment." << std::endl;
+	fs << L"S_ChgmodINS()" << std::endl;
+	fs << L"S_ChgmodINS( 8 )" << std::endl;
+	fs << L"S_ChgmodINS( 'dummy' )" << std::endl;
+	fs.close();
+
+	EXPECT_THAT(mgr->LoadKeyMacro(unusedArg1, path.c_str()), IsTrue());
+
+	EXPECT_THAT(mgr->ExecKeyMacro(&pcEditWnd->GetActiveView(), 0), IsTrue());
+
+	std::filesystem::remove(path);
+
+	// 保存するだけ
+	EXPECT_THAT(((CKeyMacroMgr*)mgr.get())->SaveKeyMacro(unusedArg1, path.c_str()), IsTrue());
+
+	std::filesystem::remove(path);
+
+	// 引数が足りない
+	fs = std::wofstream(path);
+	fs << L"S_Char()" << std::endl;
+	fs.close();
+
+	EXPECT_THAT(mgr->LoadKeyMacro(unusedArg1, path.c_str()), IsTrue());
+
+	EXPECT_THAT(mgr->ExecKeyMacro(&pcEditWnd->GetActiveView(), 0), IsFalse());
+
+	std::filesystem::remove(path);
+
+	// 構文エラー
+	fs = std::wofstream(path);
+	fs << L"S_ChgmodINS( , )" << std::endl;
+	fs.close();
+
+	EXPECT_THAT(mgr->LoadKeyMacro(unusedArg1, path.c_str()), IsFalse());
+
+	std::filesystem::remove(path);
+
+	// 未定義のマクロコマンド
+	fs = std::wofstream(path);
+	fs << L"UnDefinedCommand()" << std::endl;
+	fs.close();
+
+	EXPECT_THAT(mgr->LoadKeyMacro(unusedArg1, path.c_str()), IsFalse());
+
+	std::filesystem::remove(path);
+
+	mgr = nullptr;
+
+	CMacroFactory::getInstance()->Unregister(CKeyMacroMgr::Creator);
+
+	std::array<LPARAM, 4> lparams = {};
+	pcSMacroMgr->Append(STAND_KEYMACRO, F_CHGMOD_INS, std::data(lparams), &pcEditWnd->GetActiveView());
+}
 
 /*!
  * CPPAマクロマネージャーのテスト
@@ -73,6 +152,86 @@ TEST_F(MacroMgrTest, CPPAMacroMgr001)
 	mgr = nullptr;
 
 	CMacroFactory::getInstance()->Unregister(CPPAMacroMgr::Creator);
+}
+
+/*!
+ * Pythonマクロマネージャーのテスト
+ */
+TEST_F(MacroMgrTest, CPythonMacroManager001)
+{
+	EXPECT_THAT(CPythonMacroManager::Creator(L"mac"), IsNull());
+
+	CPythonMacroManager::declare();
+
+	auto mgr = std::unique_ptr<CMacroManagerBase>(CMacroFactory::getInstance()->Create(L"py"));
+
+	// DLLパスをバックアップする
+	const auto pathOld = GetDllShareData().m_Common.m_sMacro.m_szPythonDirectory;
+
+	std::wcout << L"dll path is '" << pathOld.c_str() << L"'" << std::endl;
+
+	// 不正なDLLのパスを作る
+	const auto badDllPath = GetExeFileName().replace_filename(L"python3.dll");
+
+	// 不正なDLLなDLLを作る
+	std::wofstream fs(badDllPath);
+	fs << L"dummy dll file" << std::endl;
+	fs.close();
+
+	// DLLパスに不正なDLLのパスを入れる
+	GetDllShareData().m_Common.m_sMacro.m_szPythonDirectory = badDllPath.parent_path();
+
+	const HINSTANCE unusedArg1 = nullptr;
+
+	EXPECT_THAT(mgr->LoadKeyMacroStr(unusedArg1, L""), IsTrue());
+
+	EXPECT_THAT(mgr->ExecKeyMacro(&pcEditWnd->GetActiveView(), 0), IsFalse());
+
+	std::filesystem::remove(badDllPath);
+
+	// DLLパスを元に戻す
+	GetDllShareData().m_Common.m_sMacro.m_szPythonDirectory = pathOld;
+
+	mgr = nullptr;
+
+	CMacroFactory::getInstance()->Unregister(CPythonMacroManager::Creator);
+}
+
+/*!
+ * WSHマクロマネージャーのテスト
+ */
+TEST_F(MacroMgrTest, CWSHMacroManager001)
+{
+	EXPECT_THAT(CWSHMacroManager::Creator(L"mac"), IsNull());
+
+	CWSHMacroManager::declare();
+
+	auto mgr = std::unique_ptr<CMacroManagerBase>(CMacroFactory::getInstance()->Create(L"js"));
+
+	const HINSTANCE unusedArg1 = nullptr;
+
+	EXPECT_THAT(mgr->LoadKeyMacroStr(unusedArg1, L""), IsTrue());
+
+	EXPECT_THAT(mgr->ExecKeyMacro(&pcEditWnd->GetActiveView(), 0), IsTrue());
+
+	const auto path = GetTempFilePathWithExt(L"tes", L"js");
+	EXPECT_THAT(mgr->LoadKeyMacro(unusedArg1, path.c_str()), IsFalse());
+
+	std::wofstream fs(path);
+	fs << L"" << std::endl;
+	fs << L"// comment." << std::endl;
+	fs << L"ChgmodINS(8);" << std::endl;
+	fs.close();
+
+	EXPECT_THAT(mgr->LoadKeyMacro(unusedArg1, path.c_str()), IsTrue());
+
+	std::filesystem::remove(path);
+
+	EXPECT_THAT(mgr->ExecKeyMacro(&pcEditWnd->GetActiveView(), 0), IsTrue());
+
+	mgr = nullptr;
+
+	CMacroFactory::getInstance()->Unregister(CWSHMacroManager::Creator);
 }
 
 TEST(CSMacroMgr, GetFuncInfoByID001)
