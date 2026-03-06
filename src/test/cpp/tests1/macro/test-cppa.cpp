b0fb5451c9b80f345e42c58c10646d6a07a441d3
berryzplus <berryzplus@gmail.com>
2026-03-06 19:19:52 +0900

    test-macro.cppを追加する

--- src/test/cpp/tests1/macro/test-cppa.cpp
+++ src/test/cpp/tests1/macro/test-cppa.cpp
@@ -1,23 +1,15 @@
 ﻿/*! @file */
 /*
-	Copyright (C) 2022, Sakura Editor Organization
+	Copyright (C) 2026, Sakura Editor Organization
 
 	SPDX-License-Identifier: Zlib
 */
 #include "pch.h"
-
 #include "macro/CPPA.h"
-
-#include "macro/CMacroFactory.h"
-#include "macro/CPPAMacroMgr.h"
 
 #include "window/EditorTestSuite.hpp"
 
 #include "eval_outputs.hpp"
-
-#include <fstream>
-
-std::filesystem::path GetTempFilePathWithExt(std::wstring_view prefix, std::wstring_view extension);
 
 namespace macro {
 
@@ -283,76 +275,6 @@ TEST_F(CPpaTest, ppaStrObj)
 	EXPECT_THAT(pszValue, StrEq(test));
 
 	EXPECT_THAT(CPPA::CallStrObj(info, 0, true, &pszValue), Eq(-1));
-}
-
-/*!
- * CPPAマクロマネージャーのテスト
- */
-TEST_F(CPpaTest, CPPAMacroMgr001)
-{
-	EXPECT_THAT(CPPAMacroMgr::Creator(L"mac"), IsNull());
-
-	// スタブDLLを読み込む
-	CPPAMacroMgr::m_cPPA.InitDll(L"ppa_stub.dll");
-
-	CPPAMacroMgr::declare();
-
-	auto mgr = std::unique_ptr<CMacroManagerBase>(CMacroFactory::getInstance()->Create(L"ppa"));
-
-	const HINSTANCE unusedArg1 = nullptr;
-
-	EXPECT_THAT(mgr->LoadKeyMacroStr(unusedArg1, L"macro str;"), IsTrue());
-
-	EXPECT_THAT(mgr->ExecKeyMacro(&pcEditWnd->GetActiveView(), 0), IsTrue());
-
-	const auto path = GetTempFilePathWithExt(L"tes", L"ppa");
-	EXPECT_THAT(mgr->LoadKeyMacro(unusedArg1, path.c_str()), IsFalse());
-
-	std::wofstream fs(path);
-	fs << L"macro str1;" << std::endl;
-	fs << L"macro str2;" << std::endl;
-	fs << L"macro str3;" << std::endl;
-	fs.close();
-
-	EXPECT_THAT(mgr->LoadKeyMacro(unusedArg1, path.c_str()), IsTrue());
-
-	std::filesystem::remove(path);
-
-	EXPECT_THAT(mgr->ExecKeyMacro(&pcEditWnd->GetActiveView(), 0), IsTrue());
-
-	mgr = nullptr;
-
-	CMacroFactory::getInstance()->Unregister(CPPAMacroMgr::Creator);
-}
-
-TEST(CSMacroMgr, GetFuncInfoByID001)
-{
-	EXPECT_THAT(CSMacroMgr::GetFuncInfoByID(F_FILENEW), NotNull());
-}
-
-TEST(CSMacroMgr, GetFuncInfoByID101)
-{
-	EXPECT_THAT(CSMacroMgr::GetFuncInfoByID(F_INVALID), IsNull());
-}
-
-TEST(CSMacroMgr, GetFuncInfoByID102)
-{
-	EXPECT_THAT(CSMacroMgr::GetFuncInfoByID(F_0), IsNull());
-}
-
-TEST(CSMacroMgr, GetFuncInfoByName001)
-{
-	EXPECT_THAT(CSMacroMgr::GetFuncInfoByName(nullptr, L"S_FileNew", nullptr), F_FILENEW);
-}
-
-TEST(CSMacroMgr, GetFuncInfoByName002)
-{
-	EXPECT_THAT(CSMacroMgr::GetFuncInfoByName(nullptr, L"ChangeInsertSpace", nullptr), F_CHGINSSPACE);
-}
-
-TEST(CSMacroMgr, GetFuncInfoByName101)
-{
-	EXPECT_THAT(CSMacroMgr::GetFuncInfoByName(nullptr, nullptr, nullptr), F_INVALID);
 }
 
 } // namespace macro
---
+++ src/test/cpp/tests1/macro/test-macro.cpp
@@ -0,0 +1,108 @@
+﻿/*! @file */
+/*
+	Copyright (C) 2026, Sakura Editor Organization
+
+	SPDX-License-Identifier: Zlib
+*/
+#include "pch.h"
+
+#include "macro/CMacroFactory.h"
+#include "macro/CPPAMacroMgr.h"
+
+#include "window/EditorTestSuite.hpp"
+
+#include <fstream>
+
+std::filesystem::path GetTempFilePathWithExt(std::wstring_view prefix, std::wstring_view extension);
+
+namespace macro {
+
+struct MacroMgrTest : public ::testing::Test, public window::EditorTestSuite {
+	/*!
+	 * テストスイートの開始前に1回だけ呼ばれる関数
+	 */
+	static void SetUpTestSuite()
+	{
+		SetUpEditor();
+	}
+
+	/*!
+	 * テストスイートの終了後に1回だけ呼ばれる関数
+	 */
+	static void TearDownTestSuite()
+	{
+		TearDownEditor();
+	}
+};
+
+/*!
+ * CPPAマクロマネージャーのテスト
+ */
+TEST_F(MacroMgrTest, CPPAMacroMgr001)
+{
+	EXPECT_THAT(CPPAMacroMgr::Creator(L"mac"), IsNull());
+
+	// スタブDLLを読み込む
+	CPPAMacroMgr::m_cPPA.InitDll(L"ppa_stub.dll");
+
+	CPPAMacroMgr::declare();
+
+	auto mgr = std::unique_ptr<CMacroManagerBase>(CMacroFactory::getInstance()->Create(L"ppa"));
+
+	const HINSTANCE unusedArg1 = nullptr;
+
+	EXPECT_THAT(mgr->LoadKeyMacroStr(unusedArg1, L"macro str;"), IsTrue());
+
+	EXPECT_THAT(mgr->ExecKeyMacro(&pcEditWnd->GetActiveView(), 0), IsTrue());
+
+	const auto path = GetTempFilePathWithExt(L"tes", L"ppa");
+	EXPECT_THAT(mgr->LoadKeyMacro(unusedArg1, path.c_str()), IsFalse());
+
+	std::wofstream fs(path);
+	fs << L"macro str1;" << std::endl;
+	fs << L"macro str2;" << std::endl;
+	fs << L"macro str3;" << std::endl;
+	fs.close();
+
+	EXPECT_THAT(mgr->LoadKeyMacro(unusedArg1, path.c_str()), IsTrue());
+
+	std::filesystem::remove(path);
+
+	EXPECT_THAT(mgr->ExecKeyMacro(&pcEditWnd->GetActiveView(), 0), IsTrue());
+
+	mgr = nullptr;
+
+	CMacroFactory::getInstance()->Unregister(CPPAMacroMgr::Creator);
+}
+
+TEST(CSMacroMgr, GetFuncInfoByID001)
+{
+	EXPECT_THAT(CSMacroMgr::GetFuncInfoByID(F_FILENEW), NotNull());
+}
+
+TEST(CSMacroMgr, GetFuncInfoByID101)
+{
+	EXPECT_THAT(CSMacroMgr::GetFuncInfoByID(F_INVALID), IsNull());
+}
+
+TEST(CSMacroMgr, GetFuncInfoByID102)
+{
+	EXPECT_THAT(CSMacroMgr::GetFuncInfoByID(F_0), IsNull());
+}
+
+TEST(CSMacroMgr, GetFuncInfoByName001)
+{
+	EXPECT_THAT(CSMacroMgr::GetFuncInfoByName(nullptr, L"S_FileNew", nullptr), F_FILENEW);
+}
+
+TEST(CSMacroMgr, GetFuncInfoByName002)
+{
+	EXPECT_THAT(CSMacroMgr::GetFuncInfoByName(nullptr, L"ChangeInsertSpace", nullptr), F_CHGINSSPACE);
+}
+
+TEST(CSMacroMgr, GetFuncInfoByName101)
+{
+	EXPECT_THAT(CSMacroMgr::GetFuncInfoByName(nullptr, nullptr, nullptr), F_INVALID);
+}
+
+} // namespace macro
