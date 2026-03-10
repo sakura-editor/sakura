bbfb1532fd5143973d0c3aee9c6dbee34d09a65b
berryzplus <berryzplus@gmail.com>
2026-03-11 00:07:23 +0900

    メソッド引数を解析するためのメタタイプを再定義する
    x64を基本に、x86の場合を特殊ケースとして定義し直す。

--- src/main/cpp/cxx/type_of_Nth_arg.hpp
+++ src/main/cpp/cxx/type_of_Nth_arg.hpp
@@ -13,9 +13,32 @@ namespace cxx {
 template<typename T>
 struct args_of_function;
 
+//! 関数型の引数を解析するためのメタタイプ
+template<typename R, typename... Args>
+struct args_of_function<R(Args...)>
+{
+	using types = std::tuple<Args...>;
+};
+
+//! 関数ポインタの引数を解析するためのメタタイプ
+template<typename R, typename... Args>
+struct args_of_function<R(*)(Args...)>
+{
+	using types = std::tuple<Args...>;
+};
+
+//! クラスメソッド引数を解析するためのメタタイプ
+template<typename R, typename C, typename... Args>
+struct args_of_function<R (C::*)(Args...)>
+{
+	using types = std::tuple<Args...>;
+};
+
+#if defined(_M_IX86)
+
 //! API関数ポインタの引数を解析するためのメタタイプ
 template<typename R, typename... Args>
-struct args_of_function<R(WINAPI *)(Args...)>
+struct args_of_function<R(__stdcall *)(Args...)>
 {
 	using types = std::tuple<Args...>;
 };
@@ -26,6 +49,8 @@ struct args_of_function<R (STDMETHODCALLTYPE C::*)(Args...)>
 {
 	using types = std::tuple<Args...>;
 };
+
+#endif // defined(_M_IX86)
 
 //! COMインターフェースメソッド引数を数える関数
 template<typename FuncDecl>
