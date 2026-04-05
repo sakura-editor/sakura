791e2c342ff033ca979e825880f68315ef128036
berryzplus <berryzplus@gmail.com>
2026-04-05 14:14:06 +0900

    EOL(行終端子)の名前をリソースに入れる(2/2)

--- sakura_core/basis/CEol.cpp
+++ sakura_core/basis/CEol.cpp
@@ -7,88 +7,69 @@
 /*
 	Copyright (C) 2000-2001, genta
 	Copyright (C) 2000, Frozen, Moca
-	Copyright (C) 2018-2022, Sakura Editor Organization
+	Copyright (C) 2018-2026, Sakura Editor Organization
 
 	SPDX-License-Identifier: Zlib
 */
 #include "StdAfx.h"
 #include "basis/CEol.h"
 
+#include "charset/CCodeFactory.h"
+#include "charset/charset.h"
+#include "CSelectLang.h"
+
+#include "sakura_rc.h"
+
 //-----------------------------------------------
 //	固定データ
 //-----------------------------------------------
 
-const SEolDefinition g_aEolTable[] = {
-	{ L"改行無",	L"",			"",			0 },
-	{ L"CRLF",		L"\x0d\x0a",	"\x0d\x0a",	2 },
-	{ L"LF",		L"\x0a",		"\x0a",		1 },
-	{ L"CR",		L"\x0d",		"\x0d",		1 },
-	{ L"NEL",		L"\x85",		"",			1 },
-	{ L"LS",		L"\u2028",		"",			1 },
-	{ L"PS",		L"\u2029",		"",			1 },
-};
-
-struct SEolDefinitionForUniFile{
-	const char*	m_szDataW;
-	const char* m_szDataWB;
-	size_t		m_nLen;
-
-	bool StartsWithW(const char* pData, size_t nLen) const{ return m_nLen<=nLen && 0==memcmp(pData,m_szDataW,m_nLen); }
-	bool StartsWithWB(const char* pData, size_t nLen) const{ return m_nLen<=nLen && 0==memcmp(pData,m_szDataWB,m_nLen); }
-};
-static const SEolDefinitionForUniFile g_aEolTable_uni_file[] = {
-	{ "",					"", 					0 },
-	{ "\x0d\x00\x0a\x00",	"\x00\x0d\x00\x0a",		4 },
-	{ "\x0a\x00",			"\x00\x0a",				2 },
-	{ "\x0d\x00",			"\x00\x0d",				2 },
-	{ "\x85\x00",			"\x00\x85",				2 },
-	{ "\x28\x20",			"\x20\x28",				2 },
-	{ "\x29\x20",			"\x20\x29",				2 },
+const std::array g_EolDefinitions = {
+	SEolDefinition{ EEolType::none,					IDS_EOLTYPENAME_0, {}                 },
+	SEolDefinition{ EEolType::cr_and_lf,			IDS_EOLTYPENAME_1, WCODE::CRLF        },
+	SEolDefinition{ EEolType::line_feed,			IDS_EOLTYPENAME_2, { &WCODE::LF, 1 }  },
+	SEolDefinition{ EEolType::carriage_return,		IDS_EOLTYPENAME_3, { &WCODE::CR, 1 }  },
+	SEolDefinition{ EEolType::next_line,			IDS_EOLTYPENAME_4, { &WCODE::NEL, 1 } },
+	SEolDefinition{ EEolType::line_separator,		IDS_EOLTYPENAME_5, { &WCODE::PS_, 1 } },
+	SEolDefinition{ EEolType::paragraph_separator,	IDS_EOLTYPENAME_6, { &WCODE::LS_, 1 } },
 };
 
 //-----------------------------------------------
 //	実装補助
 //-----------------------------------------------
 
-/*!
-	行終端子の種類を調べる。
-	@param pszData 調査対象文字列へのポインタ
-	@param nDataLen 調査対象文字列の長さ
-	@return 改行コードの種類。終端子が見つからなかったときはEEolType::noneを返す。
-*/
-template <class T>
-EEolType GetEOLType( const T* pszData, size_t nDataLen )
+constexpr SEolDefinition::SEolDefinition(
+	EEolType eEolType,
+	int wName,
+	std::wstring_view data
+)
+	: m_eEolType(eEolType)
+	, m_wName(static_cast<WORD>(wName))
+	, m_Data(data)
 {
-	for( size_t i = 1; i < EOL_TYPE_NUM; ++i ){
-		if( g_aEolTable[i].StartsWith(pszData, nDataLen) ){
-			return static_cast<EEolType>(i);
-		}
-	}
-	return EEolType::none;
 }
 
-/*
-	ファイルを読み込むときに使用するもの
-*/
-
-EEolType _GetEOLType_uni( const char* pszData, size_t nDataLen )
+const std::wstring&	SEolDefinition::GetVal() const noexcept
 {
-	for( size_t i = 1; i < EOL_TYPE_NUM; ++i ){
-		if( g_aEolTable_uni_file[i].StartsWithW(pszData, nDataLen) ){
-			return static_cast<EEolType>(i);
-		}
-	}
-	return EEolType::none;
+	return m_Data;
 }
 
-EEolType _GetEOLType_unibe( const char* pszData, size_t nDataLen )
+std::string SEolDefinition::GetVal(ECodeType eCodeType) const
 {
-	for( size_t i = 1; i < EOL_TYPE_NUM; ++i ){
-		if( g_aEolTable_uni_file[i].StartsWithWB(pszData, nDataLen) ){
-			return static_cast<EEolType>(i);
-		}
-	}
-	return EEolType::none;
+	auto result = CCodeFactory::ConvertToCode(eCodeType, m_Data);
+	return std::move(result.destination);
+}
+
+bool SEolDefinition::StartsWith(LPCWSTR pData, size_t nLen) const noexcept
+{
+	const std::wstring_view text{ pData, nLen };
+	return !m_Data.empty() && text.starts_with(m_Data);
+}
+
+bool SEolDefinition::StartsWith(LPCSTR pData, size_t nLen, ECodeType eCodeType) const
+{
+	const auto eol = GetVal(eCodeType);
+	return nLen && !eol.empty() && std::size(eol) <= nLen && 0 == ::memcmp(pData, std::data(eol), std::size(eol));
 }
 
 //-----------------------------------------------
@@ -96,59 +77,72 @@ EEolType _GetEOLType_unibe( const char* pszData, size_t nDataLen )
 //-----------------------------------------------
 
 //! 現在のEOLの名称取得
-[[nodiscard]] LPCWSTR CEol::GetName() const noexcept
+LPCWSTR CEol::GetName() const
 {
-	return g_aEolTable[static_cast<size_t>(m_eEolType)].m_szName;
+	return LS(GetData().m_wName);
 }
 
 //! 現在のEOL文字列先頭へのポインタを取得
-[[nodiscard]] LPCWSTR CEol::GetValue2() const noexcept
+LPCWSTR CEol::GetValue2() const noexcept
 {
-	return g_aEolTable[static_cast<size_t>(m_eEolType)].m_szDataW;
+	return std::data(GetData().GetVal());
 }
 
 //! 現在のEOL文字列長を取得。文字単位。
-[[nodiscard]] CLogicInt	CEol::GetLen() const noexcept
+CLogicInt CEol::GetLen() const noexcept
 {
-	return CLogicInt(g_aEolTable[static_cast<size_t>(m_eEolType)].m_nLen);
+	return CLogicInt(int(std::size(GetData().GetVal())));
+}
+
+const SEolDefinition& CEol::GetData() const noexcept
+{
+	return *std::ranges::find_if(g_EolDefinitions, [this] (const SEolDefinition& eolDef) { return m_eEolType == eolDef.m_eEolType; });
 }
 
 void CEol::SetTypeByString( const wchar_t* pszData, size_t nDataLen )
 {
-	SetType( GetEOLType( pszData, nDataLen ) );
+	auto eEolType = EEolType::none;
+	for (const auto& eolDef : g_EolDefinitions) {
+		if (eolDef.StartsWith(pszData, nDataLen)) {
+			eEolType = eolDef.m_eEolType;
+			break;
+		}
+	}
+	SetType(eEolType);
 }
 
-void CEol::SetTypeByString( const char* pszData, size_t nDataLen )
+void CEol::SetTypeByString( const char* pszData, size_t nDataLen)
 {
-	SetType( GetEOLType( pszData, nDataLen ) );
+	auto eEolType = EEolType::none;
+	for (const auto& eolDef : g_EolDefinitions) {
+		if (eolDef.StartsWith(pszData, nDataLen, CODE_SJIS)) {
+			eEolType = eolDef.m_eEolType;
+			break;
+		}
+	}
+	SetType(eEolType);
 }
 
 void CEol::SetTypeByStringForFile_uni( const char* pszData, size_t nDataLen )
 {
-	SetType( _GetEOLType_uni( pszData, nDataLen ) );
+	auto eEolType = EEolType::none;
+	for (const auto& eolDef : g_EolDefinitions) {
+		if (eolDef.StartsWith(pszData, nDataLen, CODE_UTF16LE)) {
+			eEolType = eolDef.m_eEolType;
+			break;
+		}
+	}
+	SetType(eEolType);
 }
 
 void CEol::SetTypeByStringForFile_unibe( const char* pszData, size_t nDataLen )
 {
-	SetType( _GetEOLType_unibe( pszData, nDataLen ) );
-}
-
-bool operator == ( const CEol& lhs, const CEol& rhs ) noexcept
-{
-	return lhs.operator==(static_cast<EEolType>(rhs));
-}
-
-bool operator != ( const CEol& lhs, const CEol& rhs ) noexcept
-{
-	return !(lhs == rhs);
-}
-
-bool operator == ( EEolType lhs, const CEol& rhs ) noexcept
-{
-	return rhs.operator==(lhs);
-}
-
-bool operator != ( EEolType lhs, const CEol& rhs ) noexcept
-{
-	return !(lhs == rhs);
+	auto eEolType = EEolType::none;
+	for (const auto& eolDef : g_EolDefinitions) {
+		if (eolDef.StartsWith(pszData, nDataLen, CODE_UTF16BE)) {
+			eEolType = eolDef.m_eEolType;
+			break;
+		}
+	}
+	SetType(eEolType);
 }
