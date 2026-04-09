c072e055bf9d75127fd8ceb4c2f5da3f9330c9fe
berryzplus <berryzplus@gmail.com>
2026-04-09 19:41:42 +0900

    インクルードガード付与ツールを最新化する
    ・"*.hpp"も対象にする
    ・出力をUTF-8BOM付する

--- tools/includeGuard/includeGuard.ps1
+++ tools/includeGuard/includeGuard.ps1
@@ -21,22 +21,28 @@ PS C:\work\sakura-editor\sakura\sakura_core> ..\tools\includeGuard\includeGuard.
 https://github.com/sakura-editor/sakura/pull/1378
 https://sakura-editor.github.io/bbslog/sf/unicode/985.html
 #>
-$headers = Get-ChildItem -File -Recurse -Include *.h -Exclude StdAfx.h,sakura_rc.h,String_define.h
+$headers = Get-ChildItem -File -Recurse -Include "*.h","*.hpp" -Exclude "StdAfx.h","sakura_rc.h","String_define.h"
+$utf8Bom = [System.Text.UTF8Encoding]::new($true)
 foreach($header in $headers){
   $lines = Get-Content -Path $header
   $filepart = [System.IO.Path]::GetFileNameWithoutExtension($header.Name).ToUpper() -replace "-","_"
   $guardPattern = $lines | Select-String -Pattern "SAKURA_${filepart}_\w+_H_"
   $match = $lines | Select-String -Pattern "#pragma +once"
-  if (($match -ne $null) -And ($guardPattern -eq $null)) {
+  if (($null -ne $match) -And ($null -eq $guardPattern)) {
     $guidpart = (New-Guid).Guid.ToUpper() -replace "-","_"
     $guardkey = "SAKURA_${filepart}_${guidpart}_H_"
     $pragmaOnce = $match.LineNumber - 1
-    $lines[0..($pragmaOnce - 1)] | Out-File $header -Encoding UTF8
-    "#ifndef ${guardkey}" | Out-File $header -Encoding UTF8 -Append
-    "#define ${guardkey}" | Out-File $header -Encoding UTF8 -Append
-    $lines[$pragmaOnce..($lines.Length - 1)] | Out-File $header -Encoding UTF8 -Append
-    "#endif /* ${guardkey} */" | Out-File $header -Encoding UTF8 -Append
-  } elseif ($guardPattern -eq $null) {
+    $outputLines = [System.Collections.Generic.List[string]]::new()
+    if ($pragmaOnce -gt 0) {
+      $outputLines.AddRange([string[]]$lines[0..($pragmaOnce - 1)])
+    }
+    $outputLines.Add("#ifndef ${guardkey}")
+    $outputLines.Add("#define ${guardkey}")
+    $outputLines.AddRange([string[]]$lines[$pragmaOnce..($lines.Length - 1)])
+    $outputLines.Add("#endif /* ${guardkey} */")
+    $content = ($outputLines -join "`r`n") + "`r`n"
+    [System.IO.File]::WriteAllText($header.FullName, $content, $utf8Bom)
+  } elseif ($null -eq $guardPattern) {
     Write-Output $header.FullName
   }
 }
