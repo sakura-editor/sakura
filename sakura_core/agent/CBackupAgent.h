9c9ede2990fc24f92bc3c4a18138fcf70a4e4f7b
berryzplus <berryzplus@gmail.com>
2026-06-28 19:05:44 +0900

    CBackupAgent::FormatBackUpPathをテストできるようにする

--- sakura_core/agent/CBackupAgent.h
+++ sakura_core/agent/CBackupAgent.h
@@ -1,7 +1,7 @@
 ﻿/*! @file */
 /*
 	Copyright (C) 2008, kobake
-	Copyright (C) 2018-2022, Sakura Editor Organization
+	Copyright (C) 2018-2026, Sakura Editor Organization
 
 	SPDX-License-Identifier: Zlib
 */
@@ -15,8 +15,8 @@ class CBackupAgent : public CDocListenerEx{
 public:
 	ECallbackResult OnPreBeforeSave(SSaveInfo* pSaveInfo) override;
 
-protected:
 	int MakeBackUp( const WCHAR* target_file );								//!< バックアップの作成
 	bool FormatBackUpPath( WCHAR*, size_t, const WCHAR* );	//!< バックアップパスの作成 2005.11.21 aroka
 };
+
 #endif /* SAKURA_CBACKUPAGENT_26C06E20_DC8E_4AAD_BA63_F1872A2D7FFA_H_ */
