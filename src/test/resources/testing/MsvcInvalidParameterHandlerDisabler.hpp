ef6baca104168199a064b8d39d6cfa41ab195103
berryzplus <berryzplus@gmail.com>
2026-09-18 20:20:06 +0900

    MSVC専用テストツールを個別ファイルに分離する

---
+++ src/test/resources/testing/MsvcInvalidParameterHandlerDisabler.hpp
@@ -0,0 +1,45 @@
+﻿/*! @file */
+/*
+	Copyright (C) 2026, Sakura Editor Organization
+
+	SPDX-License-Identifier: Zlib
+ */
+
+#pragma once
+
+#if defined(_MSC_VER) && defined(_DEBUG)
+
+#include "cxx/ResourceHolder.hpp"
+
+#include <cstdlib>
+#include <cstdint>
+
+namespace testing {
+
+/*!
+ * @brief MSVCの無効なパラメーターハンドラーを無効化するためのクラス
+ */
+struct MsvcInvalidParameterHandlerDisabler
+{
+	using Holder = cxx::ResourceHolder<&::_set_invalid_parameter_handler>;
+	using Me = MsvcInvalidParameterHandlerDisabler;
+
+	static void __cdecl empty_handler(
+		wchar_t const*,
+		wchar_t const*,
+		wchar_t const*,
+		unsigned int,
+		uintptr_t
+	)
+	{
+		return;	// 何もしない
+	}
+
+	MsvcInvalidParameterHandlerDisabler() = default;
+
+	Holder prev{ ::_set_invalid_parameter_handler(&empty_handler) };
+};
+
+} // end of namespace testing
+
+#endif // if defined(_MSC_VER) && defined(_DEBUG)
---
+++ src/test/resources/testing/MsvcReportMode.hpp
@@ -0,0 +1,52 @@
+﻿/*! @file */
+/*
+	Copyright (C) 2026, Sakura Editor Organization
+
+	SPDX-License-Identifier: Zlib
+ */
+
+#pragma once
+
+#if defined(_MSC_VER) && defined(_DEBUG)
+
+#include "cxx/ResourceHolder.hpp"
+
+#include <crtdbg.h>
+
+#include <memory>
+
+namespace testing {
+
+/*!
+ * @brief MSVCのアサーションダイアログを抑制するためのクラス
+ */
+struct MsvcReportMode
+{
+	struct SReportMode
+	{
+		int prevMode = ::_CrtSetReportMode(_CRT_ASSERT, 0);
+	};
+
+	static void _Cleanup(const SReportMode* oldMode)
+	{
+		::_CrtSetReportMode(_CRT_ASSERT, oldMode->prevMode);
+	}
+
+	using Holder = cxx::ResourceHolder<&_Cleanup>;
+	using Me = MsvcReportMode;
+
+	MsvcReportMode() = default;
+
+	MsvcReportMode(const Me&) = delete;
+	Me& operator=(const Me&) = delete;
+
+	MsvcReportMode(Me&& other) noexcept = default;
+	Me& operator=(Me&& rhs) noexcept = default;
+
+private:
+	Holder m_old{ std::make_unique<SReportMode>().release() };
+};
+
+} // end of namespace testing
+
+#endif // if defined(_MSC_VER) && defined(_DEBUG)
